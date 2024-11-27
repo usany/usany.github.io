d32437bc51461eaa2d5e3062d165f87b2fb8d98c
usany <ckd_qja@naver.com>
2024-11-27 20:41:59 +0900

    fix

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -109,7 +109,13 @@ function Profile({ userObj }: Props) {
   //     });
   //   }
   // }, [])
-  
+  useEffect(() => {
+    document.documentElement.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "instant", // Optional if you want to skip the scrolling animation
+    });
+  }, [state]);
   useEffect(() => {
     handleBottomNavigation(5)
   })
