5973b7e6426dc61108ddfe520e656daac8a8d31e
usany <ckd_qja@naver.com>
2025-01-01 01:58:06 +0900

    Updates

--- assets/browser-C0XxP9Lc.js
+++
@@ -1,1 +0,0 @@
-import{aG as e}from"./index-CRk_zr9W.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
