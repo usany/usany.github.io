1ead2ddf4d5d6dc60aa0adc146d78e18a16cec7b
usany <ckd_qja@naver.com>
2025-01-14 21:59:08 +0900

    fix

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -89,8 +89,21 @@ function Menu({ userObj }: Props) {
     const accordionValues = ['카드', '메세지']
     const elementRef = useRef()
     useLongPress(elementRef, () => alert('practice'))
+    useEffect(() => {
+        function handleContextMenu(e) {
+          e.preventDefault(); // prevents the default right-click menu from appearing
+        }
+        // add the event listener to the component's root element
+        const rootElement = document.getElementById('sample');
+        rootElement.addEventListener('contextmenu', handleContextMenu);
+        // remove the event listener when the component is unmounted
+    
+        return () => {
+          rootElement.removeEventListener('contextmenu', handleContextMenu);
+        };
+      }, []);
     return (
-        <div className='flex justify-center flex-col pb-5'>
+        <div id='sample' className='flex justify-center flex-col pb-5'>
             <PageTitle title={'내 상태'}/>
             <Accordion 
                 value={[accordions.cards, accordions.messages]}
