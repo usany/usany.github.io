1212926b52c8ccfefd63a8f208567834f36b6650
usany <ckd_qja@naver.com>
2025-02-13 02:04:38 +0900

    fix

--- src/pages/main/BoardMap.tsx
+++ src/pages/main/BoardMap.tsx
@@ -36,19 +36,71 @@ function BoardMap() {
         apiKey: import.meta.env.VITE_MAPS_PLATFORM,
         version: "weekly",
     });
+    const tourStops = [
+        {
+          position: { lat: 34.8791806, lng: -111.8265049 },
+          title: "Boynton Pass",
+        },
+        {
+          position: { lat: 34.8559195, lng: -111.7988186 },
+          title: "Airport Mesa",
+        },
+        {
+          position: { lat: 34.832149, lng: -111.7695277 },
+          title: "Chapel of the Holy Cross",
+        },
+        {
+          position: { lat: 34.823736, lng: -111.8001857 },
+          title: "Red Rock Crossing",
+        },
+        {
+          position: { lat: 34.800326, lng: -111.7665047 },
+          title: "Bell Rock",
+        },
+    ];
     useEffect(() => {
-        loader
-        .importLibrary('maps')
-        .then(({Map}) => {
-            new Map(document.getElementById("map"), {
-                center: { lat: -34.397, lng: 150.644 },
-                zoom: 8,
+        if (mapAccordion) {
+            loader
+            .importLibrary('maps')
+            .then(({Map, InfoWindow}) => {
+                const map = new Map(document.getElementById("map"), {
+                    center: { lat: 34.84555, lng: -111.8035 },
+                    zoom: 10,
+                    mapId: '77db85c9c2270baa'
+                });
+                const infoWindow = new InfoWindow();
+
+                loader
+                .importLibrary('marker')
+                .then(({AdvancedMarkerElement, PinElement}) => {
+                    tourStops.forEach(({ position, title }, i) => {
+                        const pin = new PinElement({
+                          glyph: `${i + 1}`,
+                          scale: 1.5,
+                        });
+                        const marker = new AdvancedMarkerElement({
+                          position,
+                          map,
+                          title: `${i + 1}. ${title}`,
+                          content: pin.element,
+                          gmpClickable: true,
+                        });
+                        marker.addListener("click", () => {
+                            infoWindow.close();
+                            infoWindow.setContent(marker.title);
+                            infoWindow.open(marker.map, marker);
+                        });
+                    })
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            })
+            .catch((error) => {
+                console.log(error)
             });
-        })
-        .catch((error) => {
-            console.log(error)
-        });
-    })
+        }
+    }, [mapAccordion])
       
     return (  
         <div>
@@ -59,9 +111,10 @@ function BoardMap() {
             >
                 <AccordionItem value='item-1'>
                     <AccordionTrigger 
+                        onClick={() => setMapAccordion(!mapAccordion)}
                     >등록 지도</AccordionTrigger>
                     <AccordionContent>
-                        <div id="map">samples</div>
+                        <div id="map" className='w-full h-[300px]'>samples</div>
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
