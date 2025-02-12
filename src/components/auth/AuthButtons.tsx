07be0b86f1f85ce84bf0ab325e5a5f28b95723a7
usany <ckd_qja@naver.com>
2025-02-13 03:42:36 +0900

    fix

--- src/components/auth/AuthButtons.tsx
+++ src/components/auth/AuthButtons.tsx
@@ -8,9 +8,21 @@ import staticFacebook from 'src/assets/signFacebook.svg'
 const AuthButtons = () => {
   return (
     <div className='flex flex-col w-screen items-center justify-center px-20'>
-        <Button sx={{width: '50%'}} startIcon={<img src={staticMicrosoft} className='w-[20px]'/>} variant='outlined' onClick={onSocialClickMicrosoft}>마이크로소프트 로그인</Button>
-        <Button sx={{width: '50%'}} startIcon={<img src={staticGoogle} className='w-[20px]'/>} variant='outlined' name='g' onClick={onSocialClick}>구글 로그인</Button>
-        <Button sx={{width: '50%', backgroundColor: '#2196f3'}} startIcon={<img src={staticFacebook} className='w-[20px]'/>} variant='outlined' onClick={onSocialClick}>페이스북으로 로그인</Button>
+        <Button sx={{width: '50%'}} startIcon={<img src={staticMicrosoft} className='w-[20px]'/>} variant='outlined' onClick={onSocialClickMicrosoft}>
+          <div className='truncate'>
+            마이크로소프트 로그인
+          </div>
+        </Button>
+        <Button sx={{width: '50%'}} startIcon={<img src={staticGoogle} className='w-[20px]'/>} variant='outlined' name='g' onClick={onSocialClick}>
+          <div className='truncate'>
+          구글 로그인
+          </div>
+        </Button>
+        <Button sx={{width: '50%', backgroundColor: '#2196f3'}} startIcon={<img src={staticFacebook} className='w-[20px]'/>} variant='outlined' onClick={onSocialClick}>
+          <div className='truncate'>
+            페이스북으로 로그인
+          </div>
+        </Button>
         {/* <Button sx={{width: '50%'}} startIcon={<img src={staticMicrosoft} className='w-[20px]'/>} variant='outlined' name='g' onClick={onSocialClick}>구글로 로그인</Button> */}
         <Button sx={{width: '50%'}} variant='outlined' name='g' onClick={onSocialClick}>구글로 로그인</Button>
         <Button sx={{width: '50%'}} variant='outlined' name='h' onClick={onSocialClick}>애플로 로그인</Button>
