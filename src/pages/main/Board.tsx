166ee8238ce9dd061241bc77609d5f2f1b83d01e
usany <ckd_qja@naver.com>
2025-02-24 01:36:53 +0900

    fix

--- src/pages/main/Board.tsx
+++ src/pages/main/Board.tsx
@@ -21,7 +21,7 @@ import Cards from "src/components/card/Cards";
 import PageTitle from "src/pages/core/pageTitle/PageTitle";
 import BoardMap from "src/pages/main/BoardMap";
 import { SwipeableViews } from "src/navigate/SwipeableViews";
-import { AlarmCheck, AlertCircle, Building, Clock, DoorOpen, MessagesSquare, Pencil, Presentation, Search, SearchCheck, SearchCode, SearchSlash, Siren, TowerControl, Umbrella, UserCheck, UserRound, Watch } from "lucide-react";
+import { AlarmCheck, AlertCircle, Building, Clock, DoorOpen, MessagesSquare, Pen, PenBox, Pencil, PenSquare, PenTool, Presentation, Search, SearchCheck, SearchCode, SearchSlash, Siren, TowerControl, Umbrella, UserCheck, UserRound, Watch } from "lucide-react";
 
 interface Props {
   userObj: User | null;
@@ -121,6 +121,11 @@ function Notice({ userObj, borrow }: Props) {
       <SearchCheck />
       <SearchCode />
       <SearchSlash />
+      <Pen />
+      <PenBox />
+      <PenTool />
+      <PenSquare />
+      
       {/* <div>
         <div className="sticky top-20 p-5 bg-white">카드 목록</div>
         <div>
