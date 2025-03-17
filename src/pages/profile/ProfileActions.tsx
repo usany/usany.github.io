f0b21b14cb70916d06f3edccd5a7e04d2ca19246
usany <ckd_qja@naver.com>
2025-03-18 01:52:51 +0900

    cern

--- src/pages/profile/ProfileActions.tsx
+++ src/pages/profile/ProfileActions.tsx
@@ -9,12 +9,20 @@ const ProfileActions = ({
   handleFollowers,
   handleFollowings
 }) => {
+  // useEffect(() => {
+  //   onSnapshot(doc(dbservice, `members/${user.uid}`), (snapshot) => {
+  //     const points = snapshot.data().points
+  //     const doneCards = snapshot.data().done || []
+  //     setCards({point: points, done: doneCards})
+  //   })
+  // }, [])
+
   return (
     <div>
-      {userObj.uid === user.uid ?
+      {userObj.uid === user.uid ? 
         <ProfileForm userObj={userObj} />
         :
-        <ProfileConnects userObj={userObj} user={user} alliesCollection={alliesCollection} handleFollowers={handleFollowers} handleFollowings={handleFollowings}/>
+        <ProfileConnects userObj={userObj} user={user} alliesCollection={alliesCollection} handleFollowers={handleFollowers} handleFollowings={handleFollowings}/>      
       }
     </div>
   );
