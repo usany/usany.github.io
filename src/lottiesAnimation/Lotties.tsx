be91941ced5ea93676890dffafe4cf87434bb94d
usany <ckd_qja@naver.com>
2025-04-04 01:38:48 +0900

    cern

--- src/lottiesAnimation/Lotties.tsx
+++ src/lottiesAnimation/Lotties.tsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react'
 import Lottie from 'react-lottie'
 // import Lottie from 'lottie-react'
 import rain from 'src/assets/Animation.json'
+import { TextRoll } from 'src/components/components/motion-primitives/text-roll';
+import { MorphingText } from 'src/components/magicui/morphing-text';
+import { SparklesText } from 'src/components/magicui/sparkles-text';
 
 function Lotties() {
     const defaultOptions = {
@@ -12,9 +15,14 @@ function Lotties() {
           preserveAspectRatio: "xMidYMid slice"
         }
     };
-    
+    const texts = ['umbrella', 'umbrellas']
+
     return (
+      <div>
+        <SparklesText text={texts[0]}/>
+        <TextRoll className='flex flex-col items-center text-xl'>{texts[0]}</TextRoll>
         <Lottie options={defaultOptions} height={400} width={400} />
+      </div>
         // <Lottie animationData={rain} />
     )
 }
