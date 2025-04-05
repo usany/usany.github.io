3df5a87df9ffd8280500d0aa133ee6ee82c33d3f
usany <ckd_qja@naver.com>
2025-04-05 22:55:59 +0900

    cern

--- src/lottiesAnimation/Lotties.tsx
+++ src/lottiesAnimation/Lotties.tsx
@@ -1,22 +1,28 @@
-import { useState, useEffect } from 'react'
-import Lottie from 'react-lottie'
+import Lottie from 'react-lottie';
 // import Lottie from 'lottie-react'
-import rain from 'src/assets/Animation.json'
+import rain from 'src/assets/Animation.json';
+import { TextRoll } from 'src/components/motion-primitives/text-roll';
 
 function Lotties() {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: rain,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-    
-    return (
-        <Lottie options={defaultOptions} height={400} width={400} />
-        // <Lottie animationData={rain} />
-    )
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: rain,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  return (
+    <div>
+      {/* <SparklesText text={texts[0]}/> */}
+      <div className='flex flex-col items-center'>
+        <TextRoll className='text-5xl'>usan</TextRoll>
+      </div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </div>
+    // <Lottie animationData={rain} />
+  )
 }
 
 export default Lotties
