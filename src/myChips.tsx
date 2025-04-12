27d2047a04fbb3a95ca3d5d37a9668ff782d2fd4
usany <ckd_qja@naver.com>
2025-04-12 16:24:27 +0900

    cern

--- src/myChips.tsx
+++ src/myChips.tsx
@@ -1,6 +1,11 @@
+import { Chip } from "@mui/material"
+
 const Chips = ({ label, onClick, className }) => {
   return (
     // <div className={`${className} rounded-xl w-fit p-1 bg-light-2 dark:bg-dark-2 px-3`} onClick={onClick}>{label}</div>
+    <div onClick={onClick}>
+      <Chip label={label} />
+    </div>
   )
 }
 
