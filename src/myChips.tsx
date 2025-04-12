bcd9b9f299d80a0dc7655463af1fefc62c3cc405
usany <ckd_qja@naver.com>
2025-04-12 16:22:52 +0900

    cern

--- src/myChips.tsx
+++ src/myChips.tsx
@@ -1,6 +1,6 @@
 const Chips = ({ label, onClick, className }) => {
   return (
-    <div className={`${className} rounded-xl w-fit p-1 bg-light-2 dark:bg-dark-2 px-3`} onClick={onClick}>{label}</div>
+    // <div className={`${className} rounded-xl w-fit p-1 bg-light-2 dark:bg-dark-2 px-3`} onClick={onClick}>{label}</div>
   )
 }
 
