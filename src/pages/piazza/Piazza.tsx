501f3b72f29f807b8776cca6290a5d6ff05342aa
usany <ckd_qja@naver.com>
2025-04-29 15:01:41 +0900

    cern

--- src/pages/piazza/Piazza.tsx
+++ src/pages/piazza/Piazza.tsx
@@ -2,11 +2,12 @@ import { User } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import useKeyboard from "src/hooks/useKeyboard";
 import PiazzaForm from 'src/pages/piazza/piazzaForm/PiazzaForm';
 import PiazzaScreen from 'src/pages/piazza/piazzaScreen/PiazzaScreen';
 import PiazzaTitle from 'src/pages/piazza/piazzaTitle/PiazzaTitle';
 import { changeBottomNavigation } from 'src/stateSlices/bottomNavigationSlice';
-import { useKeyboardOffset } from 'virtual-keyboard-offset';
+// import { useKeyboardOffset } from 'virtual-keyboard-offset';
 
 interface Props {
   userObj: User
@@ -17,13 +18,23 @@ function Piazza({ userObj }: Props) {
   const dispatch = useDispatch()
   const { state } = useLocation()
   const [multiple, setMultiple] = useState(true)
+  const keyboard = useKeyboard()
+  console.log(keyboard)
   useEffect(() => {
     if (state?.multiple !== undefined) {
       setMultiple(state?.multiple)
     }
   }, [])
-  const { keyBoardOffset, windowHeight } = useKeyboardOffset();
-  console.log(keyBoardOffset, windowHeight);
+  // const { keyBoardOffset, windowHeight } = useKeyboardOffset();
+  // console.log(keyBoardOffset, windowHeight);
+  window.addEventListener('resize', () => {
+    // For the rare legacy browsers that don't support it
+    if (!window.visualViewport) {
+      return
+    }
+    const height = window.visualViewport.height - 200
+    console.log(window.visualViewport.height)
+  })
 
 
   useEffect(() => {
@@ -34,6 +45,7 @@ function Piazza({ userObj }: Props) {
   return (
     <>
       <PiazzaTitle multiple={multiple} displayName={displayName} />
+      {keyboard ? <div>open</div> : <div>closed</div>}
       <PiazzaScreen userObj={userObj} multiple={multiple} handleMultiple={(newValue) => setMultiple(newValue)} messagesList={messagesList} handleMessagesList={(newValue) => setMessagesList(newValue)} />
       <PiazzaForm userObj={userObj} multiple={multiple} messages={messages} handleMessages={(newValue) => setMessages(newValue)} messagesList={messagesList} handleMessagesList={(newValue) => setMessagesList(newValue)} />
     </>
