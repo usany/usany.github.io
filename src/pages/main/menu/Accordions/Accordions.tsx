bfba7a0f588023e73e29470571e4c3dfde9c8bc0
usany <ckd_qja@naver.com>
2025-05-19 01:31:13 +0900

    cern

---
+++ src/pages/main/menu/Accordions/Accordions.tsx
@@ -0,0 +1,120 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+import { User } from 'firebase/auth'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { getToken } from 'firebase/messaging'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { dbservice, messaging, storage } from 'src/baseApi/serverbase'
+import { useSelectors } from 'src/hooks/useSelectors'
+import CardsStacks from 'src/pages/core/card/CardsStacks'
+import MessageStacks from 'src/pages/core/chatting/MessageStacks'
+import PageTitle from 'src/pages/core/pageTitle/PageTitle'
+import { cardOff, cardOn } from 'src/stateSlices/cardAccordionSlice'
+import { messageOff, messageOn } from 'src/stateSlices/messageAccordionSlice'
+import { changeProfileColor } from 'src/stateSlices/profileColorSlice'
+import { changeProfileUrl } from 'src/stateSlices/profileUrlSlice'
+import useSetProfile from './useSetProfile'
+import useGetToken from './useGetToken'
+import useContextMenu from './useContextMenu'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+interface Props {
+  userObj: User
+}
+
+const titles = {
+  ko: '내 상태',
+  en: 'My Status',
+}
+const cards = {
+  ko: '카드',
+  en: 'Cards',
+}
+const messages = {
+  ko: '메세지',
+  en: 'Messages',
+}
+
+function Accordions({ userObj }: Props) {
+  const languages = useSelectors((state) => state.languages.value)
+  const index = languages === 'ko' || languages === 'en' ? languages : 'ko'
+  const cardAccordion = useSelector((state) => state.cardAccordion.value)
+  const messageAccordion = useSelector((state) => state.messageAccordion.value)
+  const dispatch = useDispatch()
+  return (
+    <Accordion value={[cardAccordion, messageAccordion]} type="multiple">
+      <AccordionItem value="item-1">
+        <div className="flex justify-center sticky top-16 z-30 px-5">
+          <div className="w-[1000px]">
+            <button
+              onClick={() => {
+                document.getElementById('cardAccordion')?.click()
+              }}
+              className="rounded shadow-md px-3 flex sticky top-16 z-30 w-full items-center justify-between bg-light-2/50 dark:bg-dark-2/50"
+            >
+              <div>{cards[index]}</div>
+              <AccordionTrigger
+                id="cardAccordion"
+                onClick={() => {
+                  if (cardAccordion) {
+                    dispatch(cardOff())
+                  } else {
+                    dispatch(cardOn())
+                  }
+                }}
+              ></AccordionTrigger>
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="w-[1000px]">
+            <AccordionContent className="text-sm">
+              <CardsStacks userObj={userObj} />
+            </AccordionContent>
+          </div>
+        </div>
+      </AccordionItem>
+      <AccordionItem value="item-2" className="px-5">
+        <div className="flex justify-center sticky top-16 z-30">
+          <div className="w-[1000px]">
+            <button
+              onClick={() => {
+                document.getElementById('messageAccordion')?.click()
+              }}
+              className="rounded shadow-md px-3 flex sticky top-16 z-30 w-full items-center justify-between bg-light-2/50 dark:bg-dark-2/50"
+            >
+              <div>{messages[index]}</div>
+              <AccordionTrigger
+                id="messageAccordion"
+                onClick={() => {
+                  if (messageAccordion) {
+                    dispatch(messageOff())
+                  } else {
+                    dispatch(messageOn())
+                  }
+                  // dispatch(changeMessageAccordion())
+                }}
+              ></AccordionTrigger>
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <AccordionContent className="text-sm max-w-[1000px]">
+            <MessageStacks userObj={userObj} />
+            {/* <Suspense fallback={<Skeleton />}>
+                <MessageStacks userObj={userObj} />
+              </Suspense> */}
+          </AccordionContent>
+        </div>
+      </AccordionItem>
+    </Accordion>
+  )
+}
+
+export default Accordions
