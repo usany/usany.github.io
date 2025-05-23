5a6eab20f8e16e8c6b7232bcebc40d942d94f2ff
usany <ckd_qja@naver.com>
2025-05-23 14:57:03 +0900

    cern

--- src/pages/contact/ContactFormDrawers.tsx
+++ src/pages/contact/ContactFormDrawers.tsx
@@ -1,8 +1,4 @@
 import Button from '@mui/material/Button';
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
-import { getDownloadURL, ref } from "firebase/storage";
-import { useEffect, useState } from 'react';
-import { dbservice, storage } from 'src/baseApi/serverbase';
 import { useSelectors } from "src/hooks/useSelectors";
 import Popups from "../core/Popups";
 import ContactFormDrawersContent from "./ContactFormDrawersContent";
@@ -15,38 +11,7 @@ interface Props {
 }
 
 function ContactFormDrawers({ violationUser, changeViolationUser }: Props) {
-  const [rank, setRank] = useState([])
-  const [loadedImage, setLoadedImage] = useState([])
-  const [userSearch, setUserSearch] = useState('')
-  const languages = useSelectors((state) => state.languages.value)
-  const onChangeUserSearch = (event) => {
-    const { target: { value } } = event
-    setUserSearch(value)
-  }
-
-  useEffect(() => {
-    const searchingMembersList = async () => {
-      const collectionQuery = query(
-        collection(dbservice, "members"),
-        orderBy("points", "desc"),
-      );
-      const docs = await getDocs(collectionQuery);
-      const newArray = docs.docs.map((document, index) => {
-        getDownloadURL(ref(storage, `${document.data()?.uid}`))
-          .then((url) => {
-            setLoadedImage([...loadedImage, { url: url, index: index }]);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-        return {
-          ...document.data(),
-        };
-      });
-      setRank(newArray);
-    };
-    searchingMembersList()
-  }, [])
+  const languages = useSelectors((state) => state.languages.value) 
 
   return (
     <div className='flex flex-col w-full'>
