54d962aeaa1eff2730beeda7ef46e1696abb0098
usany <ckd_qja@naver.com>
2025-05-23 14:50:14 +0900

    cern

--- src/pages/contact/ContactFormDrawers.tsx
+++ src/pages/contact/ContactFormDrawers.tsx
@@ -4,7 +4,6 @@ import { getDownloadURL, ref } from "firebase/storage";
 import { useEffect, useState } from 'react';
 import { dbservice, storage } from 'src/baseApi/serverbase';
 import { useSelectors } from "src/hooks/useSelectors";
-import useCardsBackground from '../../hooks/useCardsBackground';
 import Popups from "../core/Popups";
 import ContactFormDrawersContent from "./ContactFormDrawersContent";
 import ContactFormDrawersTitle from "./ContactFormDrawersTitle";
@@ -27,16 +26,12 @@ function ContactFormDrawers({ violationUser, changeViolationUser }: Props) {
 
   useEffect(() => {
     const searchingMembersList = async () => {
-      // const list = []
       const collectionQuery = query(
         collection(dbservice, "members"),
         orderBy("points", "desc"),
-        // limit(scrollNumber),
-        // startAfter(continuing ? continuing : "")
       );
       const docs = await getDocs(collectionQuery);
       const newArray = docs.docs.map((document, index) => {
-        // console.log(rank.indexOf(document));
         getDownloadURL(ref(storage, `${document.data()?.uid}`))
           .then((url) => {
             setLoadedImage([...loadedImage, { url: url, index: index }]);
@@ -44,91 +39,17 @@ function ContactFormDrawers({ violationUser, changeViolationUser }: Props) {
           .catch((error) => {
             console.log(error);
           });
-        // if (index + 1 === docs.docs.length) {
-        //   setContinuing(document);
-        // }
         return {
           ...document.data(),
         };
-        // if (rank.indexOf(document) === -1) {
-        // }
       });
       setRank(newArray);
-      // if (ranker.length === 0) {
-      //   const docRef = doc(dbservice, `members/${userObj.uid}`)
-      //   const myDocSnap = await getDoc(docRef)
-      //   const myDocSnapData = myDocSnap.data()
-      //   console.log(myDocSnapData)
-      //   newArray.map((document, index) => {
-      //     if (document.uid === userObj.uid) {
-      //       console.log(document?.ranking)
-      //       newArray[index].rank = index + 1;
-      //     }
-      //   });
-      //   setRanker([myDocSnapData]);
-      // }
-      // setIsLoading(false);
     };
     searchingMembersList()
-    // onSnapshot(query(collection(dbservice, 'members'), orderBy('points', 'desc')), (snapshot) => {
-    //   const newArray = snapshot.docs.map((document, index) => {
-    //     getDownloadURL(ref(storage, `${document.data()?.uid}`))
-    //     .then((url) => {
-    //       // console.log(url)
-    //       setLoadedImage([...loadedImage, {url: url, index: index}])
-    //     })
-    //     .catch((error) => {
-    //       console.log(error)
-    //     });
-    //     return ({
-    //       ...document.data(),
-    //     })
-    //   });
-    //   setRank(newArray)
-    // })
   }, [])
-  const { color } = useCardsBackground()
 
   return (
     <div className='flex flex-col w-full'>
-      {/* <Drawer>
-        <DrawerTrigger className='w-screen' onClick={() => setUserSearch('')}>
-          {violationUser ?
-            <Card sx={{
-              width: '100%',
-              bgcolor: color
-            }}>
-              <div className='flex'>
-                <div className='flex flex-col justify-center'>{languages === 'ko' ? '신고 유저:' : 'Reporting User'}</div>
-                <div className='px-5'>
-                  <Avatar className={`bg-${(violationUser?.profileColor || []).indexOf('#') === -1 ? violationUser?.profileColor : 'profile-blue'}`}>
-                    <AvatarImage src={violationUser?.profileImageUrl} />
-                    <AvatarFallback className='text-xl border-none'>{violationUser?.displayName[0]}</AvatarFallback>
-                  </Avatar>
-                </div>
-                <div className='flex flex-col justify-center'>{violationUser.displayName}</div>
-              </div>
-            </Card>
-            :
-            <Button sx={{ width: '100%' }} variant='outlined' form='auth'>{languages === 'ko' ? '신고 등록 유저' : 'Register reporting user'}</Button>
-          }
-        </DrawerTrigger>
-        <DrawerContent className="flex flex-col justify-center px-5 bg-light-2 dark:bg-dark-2 max-h-[60%]">
-          <ScrollArea className="overflow-y-scroll">
-            <DrawersBar />
-            <div className={`flex flex-col p-5 ${!userSearch && 'h-[60vh]'}`} >
-              <TextField label={languages === 'ko' ? '유저 이름' : 'User name'} onChange={onChangeUserSearch} />
-              {userSearch &&
-                <div className='flex flex-col'>
-                  <DrawerClose>
-                    <Lists elements={rank} multiple={true} userSearch={userSearch} ranking={false} handleUser={(newValue) => changeViolationUser(newValue)} />
-                  </DrawerClose>
-                </div>
-              }
-            </div>
-          </ScrollArea>
-        </DrawerContent>
-      </Drawer > */}
       <Popups trigger={<ContactFormDrawersTrigger violationUser={violationUser} />} title={<ContactFormDrawersTitle />} content={<ContactFormDrawersContent changeViolationUser={changeViolationUser} />} />
       {violationUser && <Button sx={{ width: '25%' }} variant='outlined' onClick={() => changeViolationUser(null)}>{languages === 'ko' ? '신고 등록 취소' : 'Cancel reporting'}</Button>
       }
