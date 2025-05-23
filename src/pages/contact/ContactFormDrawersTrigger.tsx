00e6f49389858256336ffb6ea8a6ce8d4516601e
usany <ckd_qja@naver.com>
2025-05-23 15:05:27 +0900

    cern

--- src/pages/contact/ContactFormDrawersTrigger.tsx
+++ src/pages/contact/ContactFormDrawersTrigger.tsx
@@ -18,73 +18,7 @@ function ContactFormDrawersTrigger({ violationUser, changeViolationUser }: Props
   const [loadedImage, setLoadedImage] = useState([])
   const [userSearch, setUserSearch] = useState('')
   const languages = useSelectors((state) => state.languages.value)
-  // const onChangeUserSearch = (event) => {
-  //   const { target: { value } } = event
-  //   setUserSearch(value)
-  // }
-
-  useEffect(() => {
-    const searchingMembersList = async () => {
-      // const list = []
-      const collectionQuery = query(
-        collection(dbservice, "members"),
-        orderBy("points", "desc"),
-        // limit(scrollNumber),
-        // startAfter(continuing ? continuing : "")
-      );
-      const docs = await getDocs(collectionQuery);
-      const newArray = docs.docs.map((document, index) => {
-        // console.log(rank.indexOf(document));
-        getDownloadURL(ref(storage, `${document.data()?.uid}`))
-          .then((url) => {
-            setLoadedImage([...loadedImage, { url: url, index: index }]);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-        // if (index + 1 === docs.docs.length) {
-        //   setContinuing(document);
-        // }
-        return {
-          ...document.data(),
-        };
-        // if (rank.indexOf(document) === -1) {
-        // }
-      });
-      setRank(newArray);
-      // if (ranker.length === 0) {
-      //   const docRef = doc(dbservice, `members/${userObj.uid}`)
-      //   const myDocSnap = await getDoc(docRef)
-      //   const myDocSnapData = myDocSnap.data()
-      //   console.log(myDocSnapData)
-      //   newArray.map((document, index) => {
-      //     if (document.uid === userObj.uid) {
-      //       console.log(document?.ranking)
-      //       newArray[index].rank = index + 1;
-      //     }
-      //   });
-      //   setRanker([myDocSnapData]);
-      // }
-      // setIsLoading(false);
-    };
-    searchingMembersList()
-    // onSnapshot(query(collection(dbservice, 'members'), orderBy('points', 'desc')), (snapshot) => {
-    //   const newArray = snapshot.docs.map((document, index) => {
-    //     getDownloadURL(ref(storage, `${document.data()?.uid}`))
-    //     .then((url) => {
-    //       // console.log(url)
-    //       setLoadedImage([...loadedImage, {url: url, index: index}])
-    //     })
-    //     .catch((error) => {
-    //       console.log(error)
-    //     });
-    //     return ({
-    //       ...document.data(),
-    //     })
-    //   });
-    //   setRank(newArray)
-    // })
-  }, [])
+  
   const { color, colorTwo } = useCardsBackground()
 
   return (
