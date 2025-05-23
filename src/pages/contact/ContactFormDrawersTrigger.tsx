6ad884793f26143a044a8353e30e5cdec8750208
usany <ckd_qja@naver.com>
2025-05-23 15:31:05 +0900

    cern

--- src/pages/contact/ContactFormDrawersTrigger.tsx
+++ src/pages/contact/ContactFormDrawersTrigger.tsx
@@ -4,6 +4,26 @@ import Card from '@mui/material/Card';
 import { useSelectors } from "src/hooks/useSelectors";
 import useCardsBackground from '../../hooks/useCardsBackground';
 
+const ContactUserSelected = ({ violationUser }) => {
+  const profile = violationUser.profileImage ? violationUser.profileImageUrl : violationUser.defaultProfile
+
+  return (
+    <Card sx={{
+      width: '100%',
+      bgcolor: color
+    }}>
+      <div className='flex'>
+        <div className='flex flex-col justify-center'>{languages === 'ko' ? '신고 유저:' : 'Reporting User'}</div>
+        <div className='px-5'>
+          <Avatar className={`bg-${(violationUser?.profileColor || []).indexOf('#') === -1 ? violationUser?.profileColor : 'profile-blue'}`}>
+            <AvatarImage src={profile} />
+          </Avatar>
+        </div>
+        <div className='flex flex-col justify-center'>{violationUser.displayName}</div>
+      </div>
+    </Card>
+  )
+}
 interface Props {
   violationUser: {} | null
   changeViolationUser: (newValue) => void
@@ -12,24 +32,10 @@ interface Props {
 function ContactFormDrawersTrigger({ violationUser }: Props) {
   const languages = useSelectors((state) => state.languages.value)
   const { color, colorTwo } = useCardsBackground()
-
   return (
     <div>
       {violationUser ?
-        <Card sx={{
-          width: '100%',
-          bgcolor: color
-        }}>
-          <div className='flex'>
-            <div className='flex flex-col justify-center'>{languages === 'ko' ? '신고 유저:' : 'Reporting User'}</div>
-            <div className='px-5'>
-              <Avatar className={`bg-${(violationUser?.profileColor || []).indexOf('#') === -1 ? violationUser?.profileColor : 'profile-blue'}`}>
-                <AvatarImage src={violationUser?.profileImageUrl} />
-              </Avatar>
-            </div>
-            <div className='flex flex-col justify-center'>{violationUser.displayName}</div>
-          </div>
-        </Card>
+        <ContactUserSelected violationUser={violationUser} />
         :
         <div className='flex justify-center'>
           <Button sx={{
