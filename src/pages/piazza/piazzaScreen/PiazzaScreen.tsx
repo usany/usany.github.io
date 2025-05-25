64cd04380806518aa93557e2fc731103195f2c3c
usany <ckd_qja@naver.com>
2025-05-25 22:47:38 +0900

    cern

--- src/pages/piazza/piazzaScreen/PiazzaScreen.tsx
+++ src/pages/piazza/piazzaScreen/PiazzaScreen.tsx
@@ -25,11 +25,11 @@ function PiazzaScreen({
     <>
       {isKeyboardOpen ?
         <div className='fixed bottom-[50px] w-screen h-full bg-light-3 dark:bg-dark-3 flex flex-col pt-[120px]'>
-          <PiazzaScreenView chattingUser={chattingUser} userObj={userObj} multiple={multiple} handleMultiple={handleMultiple} messagesList={messagesList} handleMessagesList={handleMessagesList} />
+          <PiazzaScreenView userObj={userObj} multiple={multiple} handleMultiple={handleMultiple} messagesList={messagesList} handleMessagesList={handleMessagesList} />
         </div>
         :
         <div className='fixed bottom-[110px] w-screen h-[60%] bg-light-3 dark:bg-dark-3 flex flex-col'>
-          <PiazzaScreenView chattingUser={chattingUser} userObj={userObj} multiple={multiple} handleMultiple={handleMultiple} messagesList={messagesList} handleMessagesList={handleMessagesList} />
+          <PiazzaScreenView userObj={userObj} multiple={multiple} handleMultiple={handleMultiple} messagesList={messagesList} handleMessagesList={handleMessagesList} />
         </div>
       }
     </>
