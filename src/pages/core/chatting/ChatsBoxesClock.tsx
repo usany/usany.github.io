475b864ab28aad89a9af653aebe498d785d2338d
usany <ckd_qja@naver.com>
2025-05-29 17:49:33 +0900

    cern

--- src/pages/core/chatting/ChatsBoxesClock.tsx
+++ src/pages/core/chatting/ChatsBoxesClock.tsx
@@ -1,9 +1,30 @@
+import { useSelectors } from "src/hooks/useSelectors"
+
 const ChatsBoxesClock = ({ message }) => {
+  const languages = useSelectors((state) => state.languages.value)
   const clock = new Date(message?.messageClock)
-  let clockValue
-  if (typeof clock.getFullYear() === 'number') {
-    clockValue =
-      clock.getFullYear().toString() +
+  let messageAmpm
+  let messageHours = clock.getHours()
+  let messageMonth = (clock.getMonth() + 1).toString()
+  let messageDate = clock.getDate().toString()
+  if (messageHours >= 13) {
+    messageAmpm = '오후'
+    if (messageHours !== 12) {
+      messageHours = messageHours - 12
+    }
+  } else {
+    messageAmpm = '오전'
+    if (messageHours === 0) {
+      messageHours = messageHours + 12
+    }
+  }
+  if (clock.getMonth() + 1 < 10) {
+    messageMonth = '0' + messageMonth
+  }
+  if (messageDate.length === 1) {
+    messageDate = '0' + messageDate
+  }
+  const clockValue = clock.getFullYear().toString() +
       '-' +
       messageMonth +
       '-' +
@@ -16,7 +37,6 @@ const ChatsBoxesClock = ({ message }) => {
       (clock.getMinutes() < 10 ? '0' : '') +
       clock.getMinutes() +
       (languages === 'en' ? (messageAmpm === '오전' ? 'am' : 'pm') : '')
-  }
   return (
     <>
       {clockValue.length > 10 && (
