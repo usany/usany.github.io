558be80be67c39a51a39785316344ca9e7818bd4
usany <ckd_qja@naver.com>
2025-06-04 17:02:58 +0900

    cern

--- src/pages/main/auth/AuthDialogsContentPassword.tsx
+++ src/pages/main/auth/AuthDialogsContentPassword.tsx
@@ -34,45 +34,47 @@ function AuthDialogsContentPassword({ userObj }) {
       });
   }
   return (
-    <div>
-      <div className="p-3">
-        {languages === 'ko' ?
-          <div>비밀번호 재설정 메일을 보내드릴게요</div>
-          :
-          <div>We will send you a password reset email</div>
-        }
+    <div className='flex justify-center p-5'>
+      <div className="flex flex-col border border-solid w-[470px] rounded-lg pt-5">
+        <div className="flex p-3">
+          {languages === 'ko' ?
+            <div>비밀번호 재설정 메일을 보내드릴게요</div>
+            :
+            <div>We will send you a password reset email</div>
+          }
+        </div>
+        <form
+          id={'password'}
+          className="pt-3"
+          onSubmit={passwordEmail}
+        >
+          <div className="flex justify-center px-3">
+            <TextField
+              label={languages === 'ko' ? '이메일' : 'email'}
+              value={email}
+              onChange={onChange}
+              variant="outlined"
+              name="email"
+              type="email"
+              fullWidth
+              required
+            />
+          </div>
+          <div className="flex flex-col justify-center p-3">
+            <Button
+              variant="outlined"
+              startIcon={<img src={staticMail} className="w-[20px]" />}
+              form={'password'}
+              type="submit"
+            >
+              {languages === 'ko'
+                ? '메일 전송'
+                : 'Send Mail'}
+            </Button>
+            {error && <span>{languages === 'ko' ? '전송에 실패했습니다' : 'Failed sending'}</span>}
+          </div>
+        </form>
       </div>
-      <form
-        id={'password'}
-        className="pt-3"
-        onSubmit={passwordEmail}
-      >
-        <div className="flex justify-center px-3">
-          <TextField
-            label={languages === 'ko' ? '이메일' : 'email'}
-            value={email}
-            onChange={onChange}
-            variant="outlined"
-            name="email"
-            type="email"
-            fullWidth
-            required
-          />
-        </div>
-        <div className="flex flex-col justify-center p-3">
-          <Button
-            variant="outlined"
-            startIcon={<img src={staticMail} className="w-[20px]" />}
-            form={'password'}
-            type="submit"
-          >
-            {languages === 'ko'
-              ? '메일 전송'
-              : 'Send Mail'}
-          </Button>
-          {error && <span>{languages === 'ko' ? '전송에 실패했습니다' : 'Failed sending'}</span>}
-        </div>
-      </form>
     </div>
   );
 }
