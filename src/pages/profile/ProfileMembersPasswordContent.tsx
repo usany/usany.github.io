ff99eaa61fdf24aaacf18a76fd5945087271fc18
usany <ckd_qja@naver.com>
2025-06-04 20:21:37 +0900

    cern

--- src/pages/profile/ProfileMembersPasswordContent.tsx
+++ src/pages/profile/ProfileMembersPasswordContent.tsx
@@ -1,4 +1,3 @@
-import { Chip } from "@mui/material";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { deleteUser } from "firebase/auth";
@@ -6,7 +5,7 @@ import {
   deleteDoc,
   doc
 } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   useNavigate
 } from "react-router-dom";
@@ -43,49 +42,37 @@ const ProfileMembersPasswordContent = ({ userObj, user }) => {
       });
     navigate("/");
   };
-  useEffect(() => {
-    const createdCards = user.userData?.createdCards
-    const connectedCards = user.userData?.connectedCards
-    const createdNumber = createdCards?.length || 0
-    const connectedNumber = connectedCards?.length || 0
-    if (
-      createdNumber === 0 &&
-      connectedNumber === 0
-    ) {
-      setProcess(true);
-    } else {
-      setProcess(false)
-    }
-  }, [user]);
+  // useEffect(() => {
+  //   const createdCards = user.userData?.createdCards
+  //   const connectedCards = user.userData?.connectedCards
+  //   const createdNumber = createdCards?.length || 0
+  //   const connectedNumber = connectedCards?.length || 0
+  //   if (
+  //     createdNumber === 0 &&
+  //     connectedNumber === 0
+  //   ) {
+  //     setProcess(true);
+  //   } else {
+  //     setProcess(false)
+  //   }
+  // }, [user]);
   return (
     <div>
       <div className='pt-5'>
         <div>
-          진행 중인 빌리기, 빌려주기가 없어야 회원 탈퇴를 할 수 있습니다.
+          새 비밀번호를 등록해 주세요.
         </div>
-        {process ? (
-          <div className='flex justify-center'>
-            <Chip label={'진행 카드가 없습니다'} sx={{ bgcolor: '#7fc4bc', color: 'white' }} />
-          </div>
-        ) : (
-          <div className='flex justify-center'>
-            <Chip label={'진행 카드가 있습니다'} sx={{
-              bgcolor: '#e76e50', color: 'white'
-            }} />
-          </div>
-        )}
-        <div>정말로 회원 탈퇴를 하시려면 이메일을 입력해 주세요</div>
       </div>
       <div className="flex flex-col justify-center p-5 gap-5">
-        <TextField name='newPassword' label="새 비밀번호" onChange={onChange} />
-        <TextField name='newPasswordConfirm' label="새 비밀번호 확인" onChange={onChange} />
+        <TextField type='password' name='newPassword' label="새 비밀번호" onChange={onChange} />
+        <TextField type='password' name='newPasswordConfirm' label="새 비밀번호 확인" onChange={onChange} />
         {password.newPassword && password.newPassword === password.newPasswordConfirm ? (
           <Button variant="outlined" onClick={delist}>
-            회원 탈퇴
+            비밀번호 변경
           </Button>
         ) : (
           <Button variant="outlined" disabled>
-            회원 탈퇴
+            비밀번호 변경
           </Button>
         )}
       </div>
