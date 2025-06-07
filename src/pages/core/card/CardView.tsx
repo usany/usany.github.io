9d32701a9e5c59815aa40b40b3ee162759021b0f
usany <ckd_qja@naver.com>
2025-06-07 16:08:27 +0900

    cern

--- src/pages/core/card/CardView.tsx
+++ src/pages/core/card/CardView.tsx
@@ -3,8 +3,9 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 // import staticImg from 'src/assets/pwa-512x512.png'
-import staticImg from 'src/assets/static_Cl.jpg'
 import useCardsBackground from 'src/hooks/useCardsBackground'
+import { staticArray } from 'src/pages/add/AddCards'
+import locationsBuildings from 'src/pages/add/locationsBuildings'
 import CardViewLocation from './CardViewLocation'
 import CardViewTime from './CardViewTime'
 import CardViewTop from './CardViewTop'
@@ -12,7 +13,11 @@ import CardViewTransfer from './CardViewTransfer'
 
 const CardView = ({ onTransfer, message, shadowColor }) => {
   const { color } = useCardsBackground()
-
+  const locationOne = locationsBuildings['en'][
+    locationsBuildings['ko'].indexOf(message.text.count)
+  ]
+  console.log(locationOne)
+  const staticImg = staticArray[message.text.count]
   return (
     <div className="flex flex-col gap-5">
       {onTransfer && <CardViewTransfer />}
