a7b119e1fcbec7bb45cdfbda5a3920c051ddcb1f
usany <ckd_qja@naver.com>
2025-06-12 20:44:03 +0900

    Updates

--- assets/ban-UV9LLvpm.js
+++ assets/ban-UV9LLvpm.js
@@ -1,4 +1,4 @@
-import{b1 as c}from"./index-Ds5utzVg.js";/**
+import{b1 as c}from"./index-d4NAFSAb.js";/**
  * @license lucide-react v0.381.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,62 @@
+
 self.addEventListener('push', event => {
+  console.log(event.data.json())
   const options = {
-    body: event.data.text(),
-    icon: '/path/to/your/icon.png', // Replace with your icon path
+    body: String(event.data.json().notification.body),
+    icon: '/path/to/your/icon.png',
+    badge: '/path/to/your/icon.png',
+    actions: [
+      {
+        action: 'yes',
+        type: 'button',
+        title: 'yes'
+      },
+      {
+        action: 'no',
+        type: 'text',
+        title: 'no',
+        placeholder: 'Type your explanation here',
+      }
+    ],
+    tag: 'renotify',
+    renotify: true,
+    vibrate: [
+      500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170,
+      40, 500,
+    ],
   };
   event.waitUntil(
-    self.registration.showNotification('Your App Name', options)
+    self.registration.showNotification('USANY', options)
   );
 });
+self.addEventListener('notificationclick', (event) => {
+  // clients.openWindow("https://jameshfisher.com/");
+  clients.openWindow("/");
+  event.notification.close();
+})
+
+
+// import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
+
+// const messaging = getMessaging();
+// self.addEventListener('push', event => {
+//   onBackgroundMessage(messaging, (payload) => {
+//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+// Customize notification here
+//     const notificationTitle = 'Background Message Title';
+//     const notificationOptions = {
+//       body: 'Background Message body.',
+//       icon: '/firebase-logo.png'
+//     };
+//     self.registration.showNotification(notificationTitle,
+//       notificationOptions);
+//   });
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     body: 'Background Message body.',
+//     icon: '/firebase-logo.png'
+//   };
+//   event.waitUntil(
+//     self.registration.showNotification(notificationTitle, notificationOptions)
+//   );
+// })
