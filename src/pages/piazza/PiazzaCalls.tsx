dc6af4544bf10c745f0bc729b5496fc86b40e1d7
usany <ckd_qja@naver.com>
2025-06-17 20:50:19 +0900

    cern

--- src/pages/piazza/PiazzaCalls.tsx
+++ src/pages/piazza/PiazzaCalls.tsx
@@ -1,7 +1,50 @@
 // import { useKeyboardOffset } from 'virtual-keyboard-offset';
 
 function PiazzaCalls() {
+// const myFace = document.getElementById('myFace')
+    // const muteButton = document.getElementById('mute')
+    // const streamButton = document.getElementById('stream')
+    // const videoSelect = document.getElementById('videoInput')
+    // let promise
+    // let muted = false
+    // let streamOff = false
+    // function handleMuteClick() {
+    //   if (!muted) {
+    //     muteButton.innerText = 'unmute'
+    //     muted = true
+    //     console.log(muteButton)
+    //   } else {
+    //     muteButton.innerText = 'mute'
+    //     muted = false
+    //   }
+    // }
+    // function handleStreamClick() {
+    //   if (!streamOff) {
+    //     streamButton.innerText = 'turn stream on'
+    //     streamOff = true
+    //   } else {
+    //     streamButton.innerText = 'turn stream off'
+    //     streamOff = false
+    //   }
+    // }
+    // muteButton.addEventListener('click', () => console.log('practices'))
+    // streamButton.addEventListener('click', handleStreamClick)
+    // async function getMedia() {
+    //   try {
+    //     const constraints = {
+    //       audio: true,
+    //       video: true
+    //     }
+    //     promise = await navigator.mediaDevices.getUserMedia(constraints);
+    //     const promises = await navigator.mediaDevices.enumerateDevices()
+    //     promise.getAudioTracks().forEach(track => track.enabled = !track.enabled)
 
+    // myFace.srcObject = promise
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // }
+    // getMedia()
   return (
     <div id='myStream'>
       <video id='myFace' width="320" height="240" controls autoPlay>
