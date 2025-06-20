d0e5c770468321ff82ddf5ecda4b93a71536d723
usany <ckd_qja@naver.com>
2025-06-20 20:21:11 +0900

    cern

--- src/pages/piazza/PiazzaCalls.tsx
+++ src/pages/piazza/PiazzaCalls.tsx
@@ -105,7 +105,30 @@ function PiazzaCalls() {
     yourScreen.srcObject = data.stream
   }
   function makeConnection() {
-    myPeerConnection = new RTCPeerConnection();
+    const iceServers = [
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun.l.google.com:5349" },
+      { urls: "stun:stun1.l.google.com:3478" },
+      { urls: "stun:stun1.l.google.com:5349" },
+      { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun2.l.google.com:5349" },
+      { urls: "stun:stun3.l.google.com:3478" },
+      { urls: "stun:stun3.l.google.com:5349" },
+      { urls: "stun:stun4.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:5349" }
+    ];
+
+    myPeerConnection = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: iceServers.map((value) => {
+            return (
+              value.urls
+            )
+          })
+        }
+      ]
+    });
     myPeerConnection.addEventListener('icecandidate', handleIce)
     myPeerConnection.addEventListener('addstream', handleAddStream)
     if (myStream) {
