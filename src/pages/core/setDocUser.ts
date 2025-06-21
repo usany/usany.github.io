9896af2b3bccd6cc352466da56f3ee13d84f932b
usany <ckd_qja@naver.com>
2025-06-22 03:08:01 +0900

    cern

--- src/pages/core/setDocUser.ts
+++ src/pages/core/setDocUser.ts
@@ -1,8 +1,8 @@
-import { doc, setDoc } from "firebase/firestore";
-import { dbservice } from "src/baseApi/serverbase";
+import { doc, setDoc } from 'firebase/firestore'
+import { dbservice } from 'src/baseApi/serverbase'
 
 const setDocUser = async ({ uid, email, ranking }) => {
-  await setDoc(doc(dbservice, "members", `${uid}`), {
+  await setDoc(doc(dbservice, 'members', `${uid}`), {
     uid: uid,
     displayName: email,
     points: 0,
@@ -19,11 +19,11 @@ const setDocUser = async ({ uid, email, ranking }) => {
     lendDoneCount: [],
     locationConfirmed: '',
     conversation: [],
-    chattings: []
+    chattings: {},
     // profileColor: "#2196f3",
     // followerNum: 0,
     // followingNum: 0,
-  });
-};
+  })
+}
 
-export default setDocUser;
+export default setDocUser
