d442a842aa1d02cc43ea2334a643da79c739717c
usany <ckd_qja@naver.com>
2025-06-27 17:15:33 +0900

    Updates

--- assets/ban-Bo557OE1.js
+++ assets/ban-Bo557OE1.js
@@ -1,4 +1,4 @@
-import{b2 as c}from"./index-DP_cf0Mx.js";/**
+import{b2 as c}from"./index-Da3v1BRc.js";/**
  * @license lucide-react v0.381.0 - ISC
  *
  * This source code is licensed under the ISC license.
