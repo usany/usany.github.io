04bc92c7e808fe2e48d42d262108a43ca4e01bad
usany <ckd_qja@naver.com>
2025-07-02 14:25:36 +0900

    cern

--- src/pages/core/navigationTop/sideNavigation/Modes.tsx
+++ src/pages/core/navigationTop/sideNavigation/Modes.tsx
@@ -1,11 +1,13 @@
+import { doc, updateDoc } from 'firebase/firestore'
 import { useDispatch, useSelector } from 'react-redux'
+import { dbservice } from 'src/baseApi/serverbase'
 import { useSelectors } from 'src/hooks/useSelectors'
 import Switches from 'src/pages/core/navigationTop/sideNavigation/Switches'
 import { changeEn, changeKo } from 'src/stateSlices/languagesSlice'
 import { changeDark, changeLight } from 'src/stateSlices/themeSlice'
 import SwitchesLanguages from './SwitchesLanguages'
 
-const Modes = () => {
+const Modes = ({ userObj }) => {
   const theme = useSelectors((state) => state.theme.value)
   const languages = useSelector((state) => state.languages.value)
   const dispatch = useDispatch()
@@ -25,13 +27,16 @@ const Modes = () => {
         }}
       />
       <SwitchesLanguages
-        onClick={() => {
+        onClick={async () => {
+          const docRef = doc(dbservice, `members/${userObj.uid}`)
           if (languages === 'ko') {
             localStorage.setItem('languages', 'en')
             dispatch(changeEn())
+            await updateDoc(docRef, { preferLanguage: 'en' });
           } else {
             localStorage.setItem('languages', 'ko')
             dispatch(changeKo())
+            await updateDoc(docRef, { preferLanguage: 'ko' });
           }
         }}
       />
--- src/pages/core/navigationTop/sideNavigation/navigationSignedIn/NavigationSignedIn.tsx
+++ src/pages/core/navigationTop/sideNavigation/navigationSignedIn/NavigationSignedIn.tsx
@@ -5,7 +5,7 @@ const NavigationSignedIn = ({ userObj, points }) => {
   return (
     <div className="flex justify-between border-b border-light-3 dark:border-dark-3 p-5 gap-5">
       <NavigationUser userObj={userObj} points={points} />
-      <Modes />
+      <Modes userObj={userObj} />
     </div>
   )
 }
