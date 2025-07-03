e01b026908e7832dfdf3b1bb5b009965d4684e30
usany <ckd_qja@naver.com>
2025-07-03 21:55:30 +0900

    cern

--- src/useUserObject.ts
+++ src/useUserObject.ts
@@ -1,15 +1,13 @@
 import { User } from 'firebase/auth'
 import { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
 import { auth } from 'src/baseApi/serverbase'
 import 'src/global.css'
-import { useSelectors } from './hooks/useSelectors'
-import { changeChangingUser } from './stateSlices/changingUserSlice'
 
 const useUserObject = () => {
   const [userObj, setUserObj] = useState<User | null | undefined>(undefined)
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
+      console.log(user)
       setUserObj(user)
     })
   }, [])
