ba779e161120cfc7311e32c6aca5bf13c0564af0
usany <ckd_qja@naver.com>
2025-07-05 20:47:38 +0900

    cern

--- src/pages/core/specifics/Specifics.tsx
+++ src/pages/core/specifics/Specifics.tsx
@@ -102,7 +102,7 @@ function Specifics({
       <div onClick={() => flipCards()}>flip card</div>
       <Tilt>
         <div className={`cards ${cardFlipped && 'rotatingCards'} z-50`}>
-          <PulsatingButton>
+          {onPulse ? <PulsatingButton>
             <div className={`truncate p-1 sides`}>
               <Card
                 className="colorTwo"
@@ -181,6 +181,85 @@ function Specifics({
               </Card>
             </div>
           </PulsatingButton>
+          :
+            <div className={`truncate p-1 sides`}>
+              <Card
+                className="colorTwo"
+                sx={{
+                  // position: 'absolute',
+                  // width: '100%',
+                  // height: '100%',
+                  boxShadow: `1.9px 1.9px 1.9px 1.9px ${shadowColor}`,
+                }}
+              >
+                <CardContent
+                  sx={{
+                    // position: 'absolute',
+                    // width: '100%',
+                    // height: '100%',
+                  }}
+                >
+                  <SpecificsActions
+                    drawerOpenTrue={drawerOpenTrue}
+                    userObj={userObj}
+                    message={message}
+                  />
+                  <div className="flex justify-center pt-1">
+                    <CardMedia
+                      sx={{
+                        width: 200 * 188 / 141 * 0.9,
+                        height: 188 * 0.9,
+                        borderRadius: '10px'
+                      }}
+                      image={staticImg}
+                    />
+                  </div>
+                  <SpecificsDimensions message={message} />
+                  <Divider />
+                  <SpecificsTrades
+                    drawerOpenTrue={drawerOpenTrue}
+                    userObj={userObj}
+                    message={message}
+                    round={round}
+                    connectedUser={connectedUser}
+                  />
+                  <Divider />
+                  <div>
+                    <SpecificsSteppers message={message} round={round} />
+                  </div>
+                  <Divider />
+                  <div className="flex justify-center pt-5">
+                    {!deleted ? (
+                      <div className="flex justify-center">
+                        <Btn
+                          messageObj={message}
+                          isOwner={message.creatorId === userObj.uid}
+                          uid={userObj.uid}
+                          displayName={userObj.displayName}
+                          userObj={userObj}
+                          num={num}
+                          points={points}
+                          deleteMessage={deleteMessage}
+                          round={round}
+                          increaseRound={increaseRound}
+                          decreaseRound={decreaseRound}
+                          changeOnPulse={changeOnPulse}
+                          changeConnectedUser={changeConnectedUser}
+                          toggleOnTransfer={toggleOnTransfer}
+                        />
+                      </div>
+                    ) : (
+                      <div className="flex justify-center">
+                        <Button variant="outlined" disabled>
+                          {languages === 'ko' ? '지워졌습니다' : 'Deleted'}
+                        </Button>
+                      </div>
+                    )}
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          }
           <div className='backSide'>
             practices
           </div>
