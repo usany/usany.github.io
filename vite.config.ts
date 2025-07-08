96033d4e9f3e9bb5c7d48901ce513995731f9c89
usany <ckd_qja@naver.com>
2025-07-09 04:42:06 +0900

    cern

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,8 @@ export default defineConfig({
       config: true,
     },
     includeAssets: [
+      'pwa-192x192.png',
+      'pwa-512x512.png',
       'screen-01.png',
       'screen.png'
     ],
@@ -32,25 +34,25 @@ export default defineConfig({
       orientation: "any",
       icons: [
         {
-          "src": "./pwa-192x192.png",
+          "src": "pwa-192x192.png",
           "sizes": "192x192",
           "type": "image/png",
           "purpose": "any"
         },
         {
-          "src": "./pwa-192x192.png",
+          "src": "pwa-192x192.png",
           "sizes": "192x192",
           "type": "image/png",
           "purpose": "maskable"
         },
         {
-          "src": "./pwa-512x512.png",
+          "src": "pwa-512x512.png",
           "sizes": "512x512",
           "type": "image/png",
           "purpose": "any"
         },
         {
-          "src": "./pwa-512x512.png",
+          "src": "pwa-512x512.png",
           "sizes": "512x512",
           "type": "image/png",
           "purpose": "maskable"
