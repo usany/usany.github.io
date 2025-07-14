e8aadf867751fb24e1e06737e9b61b6bf7482b8d
usany <ckd_qja@naver.com>
2025-07-15 02:24:57 +0900

    cern

--- src/components/ui/pulsating-button.tsx
+++ src/components/ui/pulsating-button.tsx
@@ -36,7 +36,7 @@ export const PulsatingButton = React.forwardRef<
         }
         {...props}
       >
-        <div className="relative z-10">{children}</div>
+        <div className="">{children}</div>
         <div className="absolute left-1/2 top-1/2 size-full -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-lg bg-inherit" />
       </button>
     );
