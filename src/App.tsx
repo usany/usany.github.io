b174a751625388fd46a0214d73d6559d62036538
usany <ckd_qja@naver.com>
2025-07-15 19:18:16 +0900

    cern

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { ThemeProvider } from '@mui/material/styles'
+import { doc, getDoc } from 'firebase/firestore'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useDispatch } from 'react-redux'
 import 'src/global.css'
 import Lotties from 'src/lottiesAnimation/Lotties'
 import Router from 'src/pages/core/router/Router'
+import { dbservice } from './baseApi/serverbase'
 import useColors from './hooks/useColors'
 import { useSelectors } from './hooks/useSelectors'
+import { changeDefaultProfile } from './stateSlices/defaultProfileSlice'
+import { changeProfileImage } from './stateSlices/profileImageSlice'
+import { changeProfileImageUrl } from './stateSlices/profileImageUrlSlice'
 import useUserObject from './useUserObject'
 
 function App() {
@@ -18,6 +25,20 @@ function App() {
   const theme = useSelectors((state) => state.theme.value)
   const userObj = useUserObject()
   const { lightTheme, darkTheme } = useColors()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (userObj) {
+      const initialProfile = async () => {
+        const docRef = doc(dbservice, `members/${userObj.uid}`)
+        const docSnap = await getDoc(docRef)
+        const userData = docSnap.data()
+        dispatch(changeProfileImage(userData?.profileImage))
+        dispatch(changeDefaultProfile(userData?.defaultProfile))
+        dispatch(changeProfileImageUrl(userData?.defaultProfile))
+      }
+      initialProfile()
+    }
+  }, [])
 
   function MyComponent() {
     const { t, i18n } = useTranslation();
