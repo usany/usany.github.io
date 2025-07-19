1dc255c721a67ed7fad16bbe51870095d5924aa0
usany <ckd_qja@naver.com>
2025-07-20 03:48:06 +0900

    cern

--- src/webSocket.tsx
+++ src/webSocket.tsx
@@ -4,7 +4,6 @@ const webSocket = io('https://service-ceni.onrender.com')
 // const webSocket = io('http://localhost:5000')
 
 // const onClick = () => {
-//   console.log('practice')
 //   webSocket.emit('button', { name: 'John Doe', age: 30 })
 // }
 
