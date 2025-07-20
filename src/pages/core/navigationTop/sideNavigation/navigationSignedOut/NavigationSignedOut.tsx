ca7a40a40e0357051e5cac2b87820aec2a2824fa
usany <ckd_qja@naver.com>
2025-07-20 17:33:41 +0900

    cern

--- src/pages/core/navigationTop/sideNavigation/navigationSignedOut/NavigationSignedOut.tsx
+++ src/pages/core/navigationTop/sideNavigation/navigationSignedOut/NavigationSignedOut.tsx
@@ -1,30 +1,12 @@
-import { useDispatch } from 'react-redux'
 import Modes from 'src/pages/core/navigationTop/sideNavigation/Modes'
 import NavigationUser from '../navigationUser/NavigationUser'
 
 const NavigationSignedOut = ({ userObj, points, checkbox }) => {
-  const dispatch = useDispatch()
 
   return (
     <div className="flex justify-between border-b border-light-3 dark:border-dark-3 p-5 gap-5 w-[350px]">
-      {/* <div className="flex border-b border-light-3 dark:border-dark-3 p-5">
-        <div className="max-w-[200px] max-h-[200px] overflow-hidden">
-          좋은 날씨네요
-        </div>
-        <div
-          className="flex justify-center"
-          onClick={() => {
-            dispatch(changeBottomNavigation(1));
-            checkbox();
-          }}
-        >
-          로그인을 해 주세요
-        </div>
-      </div> */}
       <NavigationUser userObj={userObj} points={points} />
       <Modes />
-      {/* <div className='flex items-center'>
-      </div> */}
     </div>
   )
 }
