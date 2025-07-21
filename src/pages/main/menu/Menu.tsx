9c45d866b4b78aab1519aa8dd9bde0dbc60918bb
usany <ckd_qja@naver.com>
2025-07-22 02:41:36 +0900

    cern

--- src/pages/main/menu/Menu.tsx
+++ src/pages/main/menu/Menu.tsx
@@ -1,4 +1,6 @@
 import { User } from 'firebase/auth'
+import { REGEXP_ONLY_DIGITS } from 'input-otp'
+import { InputOTP, InputOTPGroup, InputOTPSlot } from 'src/components/ui/input-otp'
 import { useSelectors } from 'src/hooks/useSelectors'
 import PageTitle from 'src/pages/core/pageTitle/PageTitle'
 import Accordions from './Accordions/Accordions'
@@ -26,6 +28,16 @@ function Menu({ userObj }: Props) {
     <div id="sample" className="flex justify-center flex-col pb-5">
       <PageTitle title={titles[index]} />
       <Accordions userObj={userObj} />
+      <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
       {/* <PushNotificationButton /> */}
       {/* <Avatar sx={{ bgcolor: blue[500] }} alt="Remy Sharp" src="./assets/groups.png" />
             <Avatar sx={{ bgcolor: blue[500] }} alt="Travis Howard" src="/static/images/avatar/2.jpg" />
