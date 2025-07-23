86e970989ce220e65a8189480c9d8e59995491c7
usany <ckd_qja@naver.com>
2025-07-23 17:11:23 +0900

    cern

--- src/pages/core/chatting/ChattingDialogs.tsx
+++ src/pages/core/chatting/ChattingDialogs.tsx
@@ -1,42 +1,42 @@
+import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const ChattingDialogs = ({ selectUser, user, handleClose }:
-    {
-        selectUser: boolean,
-        user: {displayName: string} | null,
-        handleClose: () => void,
-    }
+  {
+    selectUser: boolean,
+    user: { displayName: string } | null,
+    handleClose: () => void,
+  }
 ) => {
 
-    return (
-        <Dialog open={selectUser} onClose={handleClose}>
-            <DialogContent>
-                <div>
-                    {user?.displayName}
-                </div>
-            </DialogContent>
-            <DialogActions>
-            <Link to='/profile'
-            state={{element: user}}
-            >
-                <Button variant='outlined' onClick={() => {
-                    handleClose()
-                }}>
-                        프로필 확인
-                </Button>
-            </Link>
-            <Button variant='outlined' onClick={() => {
-                handleClose()
-            }}>
-                닫기
-            </Button>
-            </DialogActions>
-        </Dialog>
-    )
+  return (
+    <Dialog open={selectUser} onClose={handleClose}>
+      <DialogContent>
+        <div>
+          {user?.displayName}
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Link to={`/profile${user && `/?id:${user?.uid}`}`}
+          state={{ element: user }}
+        >
+          <Button variant='outlined' onClick={() => {
+            handleClose()
+          }}>
+            프로필 확인
+          </Button>
+        </Link>
+        <Button variant='outlined' onClick={() => {
+          handleClose()
+        }}>
+          닫기
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
 }
 
 export default ChattingDialogs
