97318aa4396c51e673f25447f10b903d47b330e8
usany <ckd_qja@naver.com>
2025-07-27 19:45:22 +0900

    cern

--- src/pages/add/Add.tsx
+++ src/pages/add/Add.tsx
@@ -58,7 +58,7 @@ function Add({ userObj, borrow }: Props) {
   const [addSteps, setAddSteps] = useState(0);
   const [display, setDisplay] = useState<DisplayCard | null>(null);
   const [item, setItem] = useState("");
-  // const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams()
   // const selectedItem = searchParams.get('item')
   const tabs = useSelector((state: TabsRootState) => state.tabs.value);
   const [fromTo, setFromTo] = useState<FromTo>({ from: null, to: null });
