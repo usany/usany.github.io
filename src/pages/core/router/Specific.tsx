4b8ae769aa3aa9a142164374b15c04bb92298af4
usany <ckd_qja@naver.com>
2025-07-30 03:58:34 +0900

    cern

--- src/pages/core/router/Specific.tsx
+++ src/pages/core/router/Specific.tsx
@@ -1,13 +1,58 @@
 import { Clock } from 'lucide-react'
 import { useState } from 'react'
 import PageTitle from '../pageTitle/PageTitle'
+import Popups from '../Popups'
 
 function Specific({ userObj }) {
   const [cardFlipped, setCardFlipped] = useState(false)
   const [onMove, setOnMove] = useState(false)
+  const [attachment, setAttachment] = useState(null)
+  const changeAttachment = (newValue) => setAttachment(newValue)
+  const [changedImage, setChangedImage] = useState({
+    attachment: false,
+    profileCharacter: '',
+    profileImage: false,
+    defaultProfile: '',
+    profileImageUrl: '',
+    profileColor: '',
+    initial: true,
+    changed: false,
+  })
+  const handleChangedImage = (newValue) => setChangedImage(newValue)
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event
+    const theFile = files[0]
+    console.log(files)
+    const reader = new FileReader()
+    reader.onloadend = (finishedEvent) => {
+      console.log(finishedEvent)
+      const {
+        currentTarget: { result },
+      } = finishedEvent
+      // console.log(result)
+      changeAttachment(result)
+    }
+    reader.readAsDataURL(theFile)
+  }
   return (
     <div>
       <PageTitle icon={<Clock />} title={'앨범'} />
+      <Popups
+        trigger={<div>추가</div>}
+        title={'추가'}
+        content={
+          <div className="flex px-5 justify-center p-5">
+            <label htmlFor="file" className="p-5 rounded border border-dashed">
+              내 파일 업로드
+            </label>
+            <input id="file" type="file" onChange={onFileChange} hidden />
+          </div>
+        }
+        close={<div>완료</div>}
+        attachment={changedImage}
+      />
     </div>
   )
 }
--- src/pages/profile/profileAvatar/profileDialogs/ProfileDialogs.tsx
+++ src/pages/profile/profileAvatar/profileDialogs/ProfileDialogs.tsx
@@ -1,25 +1,30 @@
 // import { useAvatarColorStore, useAvatarImageStore } from 'src/store'
-import { Button } from '@mui/material';
-import { Check } from 'lucide-react';
-import { useDispatch, useSelector } from 'react-redux';
-import staticBlue01 from "src/assets/blue02.png";
-import staticBlue02 from "src/assets/blue03.png";
-import staticGold01 from "src/assets/gold1.png";
-import staticGold02 from "src/assets/gold2.png";
-import staticRed01 from "src/assets/red1.png";
-import staticRed02 from "src/assets/red2.png";
-import Avatars from 'src/pages/core/Avatars';
-import { changeProfileColor } from 'src/stateSlices/profileColorSlice';
+import { Button } from '@mui/material'
+import { Check } from 'lucide-react'
+import { useDispatch, useSelector } from 'react-redux'
+import staticBlue01 from 'src/assets/blue02.png'
+import staticBlue02 from 'src/assets/blue03.png'
+import staticGold01 from 'src/assets/gold1.png'
+import staticGold02 from 'src/assets/gold2.png'
+import staticRed01 from 'src/assets/red1.png'
+import staticRed02 from 'src/assets/red2.png'
+import Avatars from 'src/pages/core/Avatars'
+import { changeProfileColor } from 'src/stateSlices/profileColorSlice'
 const images = {
   'profile-red': [staticRed01, staticRed02],
   '#2196f3': [staticBlue01, staticBlue02],
   'profile-amber': [staticGold01, staticGold02],
-  'profileRed': [staticRed01, staticRed02],
-  'profileBlue': [staticBlue01, staticBlue02],
-  'profileGold': [staticGold01, staticGold02],
+  profileRed: [staticRed01, staticRed02],
+  profileBlue: [staticBlue01, staticBlue02],
+  profileGold: [staticGold01, staticGold02],
   gold: [staticGold01, staticGold02],
 }
-const ProfileDialogs = ({ attachment, changeAttachment, changedImage, handleChangedImage }) => {
+const ProfileDialogs = ({
+  attachment,
+  changeAttachment,
+  changedImage,
+  handleChangedImage,
+}) => {
   // const [copyingProfile, setCopyingProfile] = useState({
   //   profileImage: false,
   //   defaultProfile: '',
@@ -33,25 +38,29 @@ const ProfileDialogs = ({ attachment, changeAttachment, changedImage, handleChan
   //   }
   // }, [])
   const profile = useSelector((state) => state.profile.value)
-  const profileColor = useSelector(state => state.profileColor.value)
+  const profileColor = useSelector((state) => state.profileColor.value)
   const dispatch = useDispatch()
   const switchColor = (newColor) => {
     dispatch(changeProfileColor(newColor))
-    handleChangedImage({ ...changedImage, profileColor: newColor, changed: true })
+    handleChangedImage({
+      ...changedImage,
+      profileColor: newColor,
+      changed: true,
+    })
   }
 
   const onFileChange = (event) => {
     const {
       target: { files },
-    } = event;
-    const theFile = files[0];
+    } = event
+    const theFile = files[0]
     console.log(files)
-    const reader = new FileReader();
+    const reader = new FileReader()
     reader.onloadend = (finishedEvent) => {
-      console.log(finishedEvent);
+      console.log(finishedEvent)
       const {
         currentTarget: { result },
-      } = finishedEvent;
+      } = finishedEvent
       // console.log(result)
       changeAttachment(result)
     }
@@ -61,19 +70,23 @@ const ProfileDialogs = ({ attachment, changeAttachment, changedImage, handleChan
   // console.log(attachment)
   return (
     <>
-      <div className='flex flex-col items-center gap-5 p-5'>
+      <div className="flex flex-col items-center gap-5 p-5">
         <Avatars
           element={changedImage.changed ? changedImage : profile}
           profile={true}
         />
-        <div className='flex-col px-5 content-center p-5'>
-          <label htmlFor='file' className='p-5 rounded border border-dashed'>내 파일 업로드</label>
-          <input id='file' type='file' onChange={onFileChange} hidden />
+        <div className="flex-col px-5 content-center p-5">
+          <label htmlFor="file" className="p-5 rounded border border-dashed">
+            내 파일 업로드
+          </label>
+          <input id="file" type="file" onChange={onFileChange} hidden />
         </div>
       </div>
-      <div className='flex flex-col gap-1'>
-        <div className='flex justify-center'>캐릭터 배경으로 저장하면 업로드 파일이 삭제됩니다.</div>
-        <div className='flex justify-center gap-5'>
+      <div className="flex flex-col gap-1">
+        <div className="flex justify-center">
+          캐릭터 배경으로 저장하면 업로드 파일이 삭제됩니다.
+        </div>
+        <div className="flex justify-center gap-5">
           {selectedImages.map((value, index) => {
             return (
               <div
@@ -86,9 +99,18 @@ const ProfileDialogs = ({ attachment, changeAttachment, changedImage, handleChan
                   //   console.log(url)
                   // })
                   const defaultProfile = images[profileColor][index]
-                  handleChangedImage({ ...changedImage, attachment: '', profileCharacter: index ? 'plant' : 'animal', profileImage: false, defaultProfile: defaultProfile, changed: true })
+                  handleChangedImage({
+                    ...changedImage,
+                    attachment: '',
+                    profileCharacter: index ? 'plant' : 'animal',
+                    profileImage: false,
+                    defaultProfile: defaultProfile,
+                    changed: true,
+                  })
                   changeAttachment(null)
-                  const fileInput = document.getElementById('file') || { value: null }
+                  const fileInput = document.getElementById('file') || {
+                    value: null,
+                  }
                   fileInput.value = null
                   // console.log(defaultProfile)
                   // if (index) {
@@ -96,26 +118,52 @@ const ProfileDialogs = ({ attachment, changeAttachment, changedImage, handleChan
                   // } else {
                   //   handleChangedImage({ ...profile, character: 'animal' })
                   // }
-                }}>
-                <Avatars element={{ profileImage: true, defaultProfile: value, profileImageUrl: value }} uid='' profile={false} profileColor={''} profileUrl={value} piazza={null} />
+                }}
+              >
+                <Avatars
+                  element={{
+                    profileImage: true,
+                    defaultProfile: value,
+                    profileImageUrl: value,
+                  }}
+                  uid=""
+                  profile={false}
+                  profileColor={''}
+                  profileUrl={value}
+                  piazza={null}
+                />
               </div>
             )
           })}
         </div>
-        <div className='flex justify-center h-5'>
-          <div className='flex justify-center rounded-xl w-10 bg-profile-red' onClick={() => switchColor('profileRed')}>{changedImage.profileColor === 'profileRed' &&
-            <Check />}</div>
-          <div className='flex justify-center rounded-xl w-10 bg-profile-blue' onClick={() => switchColor('profileBlue')}>{changedImage.profileColor === 'profileBlue' &&
-            <Check />}</div>
-          <div className='flex justify-center rounded-xl w-10 bg-profile-amber' onClick={() => switchColor('profileGold')}>{changedImage.profileColor === 'profileGold' &&
-            <Check />}</div>
+        <div className="flex justify-center h-5">
+          <div
+            className="flex justify-center rounded-xl w-10 bg-profile-red"
+            onClick={() => switchColor('profileRed')}
+          >
+            {changedImage.profileColor === 'profileRed' && <Check />}
+          </div>
+          <div
+            className="flex justify-center rounded-xl w-10 bg-profile-blue"
+            onClick={() => switchColor('profileBlue')}
+          >
+            {changedImage.profileColor === 'profileBlue' && <Check />}
+          </div>
+          <div
+            className="flex justify-center rounded-xl w-10 bg-profile-amber"
+            onClick={() => switchColor('profileGold')}
+          >
+            {changedImage.profileColor === 'profileGold' && <Check />}
+          </div>
         </div>
       </div>
-      {!changedImage.changed &&
-        <div className='flex justify-center p-5'>
-          <Button variant='outlined' disabled>저장</Button>
+      {!changedImage.changed && (
+        <div className="flex justify-center p-5">
+          <Button variant="outlined" disabled>
+            저장
+          </Button>
         </div>
-      }
+      )}
     </>
   )
 }
