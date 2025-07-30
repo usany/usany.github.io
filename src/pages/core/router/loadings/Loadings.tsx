b37b0281866fabb1a1871c437247313a99cc02d8
usany <ckd_qja@naver.com>
2025-07-31 04:18:02 +0900

    cern

--- src/pages/core/router/loadings/Loadings.tsx
+++ src/pages/core/router/loadings/Loadings.tsx
@@ -1,38 +1,26 @@
-import { useLocation } from 'react-router-dom';
-import LoadingsContact from "./loadingsContact/LoadingsContact";
-import LoadingsMain from "./loadingsMain/LoadingsMain";
-import LoadingsPiazza from "./loadingsPiazza/LoadingsPiazza";
-import LoadingsProfile from "./loadingsProfile/LoadingsProfile";
-import LoadingsRanking from "./loadingsRanking/LoadingsRanking";
-import LoadingsTitle from "./loadingsTitle/LoadingsTitle";
+import { useLocation } from 'react-router-dom'
+import LoadingsCollection from './loadingsCollection/LoadingsCollection'
+import LoadingsContact from './loadingsContact/LoadingsContact'
+import LoadingsMain from './loadingsMain/LoadingsMain'
+import LoadingsPiazza from './loadingsPiazza/LoadingsPiazza'
+import LoadingsProfile from './loadingsProfile/LoadingsProfile'
+import LoadingsRanking from './loadingsRanking/LoadingsRanking'
+import LoadingsTitle from './loadingsTitle/LoadingsTitle'
 
 function Loadings() {
-  const location = useLocation();
+  const location = useLocation()
 
   return (
-    <div className='flex justify-center flex-col pb-5'>
+    <div className="flex justify-center flex-col pb-5">
       <LoadingsTitle />
-      {location.pathname === '/' &&
-        <LoadingsMain />
-      }
-      {location.pathname === '/add' &&
-        <LoadingsMain />
-      }
-      {location.pathname === '/board' &&
-        <LoadingsMain />
-      }
-      {location.pathname === '/profile' &&
-        <LoadingsProfile />
-      }
-      {location.pathname === '/ranking' &&
-        <LoadingsRanking />
-      }
-      {location.pathname === '/piazza' &&
-        <LoadingsPiazza />
-      }
-      {location.pathname === '/contact' &&
-        <LoadingsContact />
-      }
+      {location.pathname === '/' && <LoadingsMain />}
+      {location.pathname === '/add' && <LoadingsMain />}
+      {location.pathname === '/board' && <LoadingsMain />}
+      {location.pathname === '/profile' && <LoadingsProfile />}
+      {location.pathname === '/ranking' && <LoadingsRanking />}
+      {location.pathname === '/piazza' && <LoadingsPiazza />}
+      {location.pathname === '/contact' && <LoadingsContact />}
+      {location.pathname === '/collection' && <LoadingsCollection />}
     </div>
   )
 }
