45f22b5e1f5eb929d5f8e7d3246baf64809d6894
usany <ckd_qja@naver.com>
2025-08-02 03:48:04 +0900

    cern

--- src/pages/contact/ContactForm.tsx
+++ src/pages/contact/ContactForm.tsx
@@ -1,20 +1,20 @@
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { User } from "firebase/auth";
-import { addDoc, collection } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import { dbservice } from 'src/baseApi/serverbase';
-import { useSelectors } from 'src/hooks/useSelectors';
-import ContactDrawers from 'src/pages/contact/ContactDrawers';
-import ContactFormDrawers from 'src/pages/contact/ContactFormDrawers';
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import { User } from 'firebase/auth'
+import { addDoc, collection } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { dbservice } from 'src/baseApi/serverbase'
+import { useSelectors } from 'src/hooks/useSelectors'
+import ContactDrawers from 'src/pages/contact/ContactDrawers'
+import ContactFormDrawers from 'src/pages/contact/ContactFormDrawers'
 
 const reportTitle = {
   ko: '신고하기 제목',
-  en: 'Report Title'
+  en: 'Report Title',
 }
 const reportContent = {
   ko: '신고하기 내용',
-  en: 'Report Content'
+  en: 'Report Content',
 }
 interface Props {
   userObj: User
@@ -41,8 +41,8 @@ function ContactForm({ userObj, user }: Props) {
   } | null>(null)
   const [initialViolationUser, setInitialViolationUser] = useState(true)
   const languages = useSelectors((state) => state.languages.value)
-  const index = (languages === 'ko' || languages === 'en') ? languages : 'ko'
-
+  const index = languages === 'ko' || languages === 'en' ? languages : 'ko'
+  const userCertificated = useSelectors((state) => state.userCertificated.value)
   useEffect(() => {
     if (user && initialViolationUser) {
       setViolationUser(user)
@@ -57,7 +57,7 @@ function ContactForm({ userObj, user }: Props) {
         userName: userObj?.displayName || '비로그인',
         messageTitle: messageTitle,
         message: messageContent,
-        violationUser: violationUser
+        violationUser: violationUser,
       })
       alert('등록되었습니다')
       setMessageTitle('')
@@ -76,37 +76,61 @@ function ContactForm({ userObj, user }: Props) {
   // }
   const onChangeMessageContent = (event: { target: { value: string } }) => {
     const {
-      target: { value }
+      target: { value },
     } = event
     setMessageContent(value)
   }
   const onChangeMessageTitle = (event: { target: { value: string } }) => {
     const {
-      target: { value }
+      target: { value },
     } = event
     setMessageTitle(value)
   }
 
   return (
-    <form id='auth'>
-      <div className='flex justify-center pt-5 px-5'>
-        <TextField name='title' label={reportTitle[index]} multiline value={messageTitle} onChange={onChangeMessageTitle} variant="outlined" fullWidth />
+    <form id="auth">
+      <div className="flex justify-center pt-5 px-5">
+        <TextField
+          name="title"
+          label={reportTitle[index]}
+          multiline
+          value={messageTitle}
+          onChange={onChangeMessageTitle}
+          variant="outlined"
+          fullWidth
+        />
       </div>
-      <div className='flex justify-center pt-5 px-5'>
-        <TextField name='content' label={reportContent[index]} multiline rows={5} value={messageContent} onChange={onChangeMessageContent} variant="outlined" fullWidth />
+      <div className="flex justify-center pt-5 px-5">
+        <TextField
+          name="content"
+          label={reportContent[index]}
+          multiline
+          rows={5}
+          value={messageContent}
+          onChange={onChangeMessageContent}
+          variant="outlined"
+          fullWidth
+        />
       </div>
-      {userObj &&
-        <div className='flex pt-3 px-5 gap-1'>
-          <ContactFormDrawers violationUser={violationUser} changeViolationUser={(newValue) => setViolationUser(newValue)} />
+      {userCertificated && (
+        <div className="flex pt-3 px-5 gap-1">
+          <ContactFormDrawers
+            violationUser={violationUser}
+            changeViolationUser={(newValue) => setViolationUser(newValue)}
+          />
         </div>
-      }
-      <div className='flex justify-center pt-2.5'>
-        {userObj && <ContactDrawers userObj={userObj} />}
-        {(messageTitle && messageContent) ?
-          <Button variant='outlined' form='auth' onClick={onSubmit}>{languages === 'ko' ? '전송' : 'send'}</Button>
-          :
-          <Button variant='outlined' form='auth' disabled>{languages === 'ko' ? '전송' : 'send'}</Button>
-        }
+      )}
+      <div className="flex justify-center pt-2.5">
+        {userCertificated && <ContactDrawers userObj={userObj} />}
+        {messageTitle && messageContent ? (
+          <Button variant="outlined" form="auth" onClick={onSubmit}>
+            {languages === 'ko' ? '전송' : 'send'}
+          </Button>
+        ) : (
+          <Button variant="outlined" form="auth" disabled>
+            {languages === 'ko' ? '전송' : 'send'}
+          </Button>
+        )}
       </div>
     </form>
   )
