eb57d2785eda7deecff22bd240301eff8dd37c8a
usany <ckd_qja@naver.com>
2025-08-03 04:39:35 +0900

    cern

--- src/pages/core/chatting/MessageStacks.tsx
+++ src/pages/core/chatting/MessageStacks.tsx
@@ -11,12 +11,13 @@ const MessageStacks = ({ userObj }: Props) => {
   const languages = useSelectors((state) => state.languages.value)
   const index = languages === 'ko' || languages === 'en' ? languages : 'ko'
   const { chattings, changeChattings, sorted, chattingNone, changeChattingNone } = useSortedChattings({ userObj })
+  const chattingsArray = navigator.onLine ? chattings : localStorage.getItem('chattings')
   return (
     <div className="flex flex-col gap-1 w-full">
       {navigator.onLine && chattingNone && <EmptyChattingStacks index={index} />}
       <ChattingStacks
         userObj={userObj}
-        chattings={chattings}
+        chattings={chattingsArray}
         changeChattings={changeChattings}
         sorted={sorted}
       />
