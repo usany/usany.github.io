392609953fb21165e9c0ad0a8f4fc53a97876985
user <user@userui-MacBookAir.local>
2025-08-05 21:02:30 +0900

    cern

--- src/pages/profile/ProfileDrawersEmptyCompanies.tsx
+++ src/pages/profile/ProfileDrawersEmptyCompanies.tsx
@@ -1,12 +1,15 @@
+import useTexts from 'src/useTexts'
+
 const ProfileDrawersEmptyCompanies = ({
   user,
   followers,
   alliesCollection,
 }) => {
+  const { noFollowers, noFollowings } = useTexts()
   return (
     <div className="flex justify-center">
       <div className="rounded shadow-md bg-light-1 dark:bg-dark-1 p-5">
-        {followers ? '팔로워가' : '팔로잉이'} 없습니다
+        {followers ? noFollowers : noFollowings}
       </div>
     </div>
   )
