4ef5f9b9c0f62be0efe9b65dd411e9cf9953223e
user <user@userui-MacBookAir.local>
2025-08-05 21:10:45 +0900

    cern

--- src/pages/profile/ProfileDrawersEmptyCompanies.tsx
+++ src/pages/profile/ProfileDrawersEmptyCompanies.tsx
@@ -2,14 +2,14 @@ import useTexts from 'src/useTexts'
 
 const ProfileDrawersEmptyCompanies = ({
   user,
-  followers,
+  followings,
   alliesCollection,
 }) => {
   const { noFollowers, noFollowings } = useTexts()
   return (
     <div className="flex justify-center">
       <div className="rounded shadow-md bg-light-1 dark:bg-dark-1 p-5">
-        {followers ? noFollowers : noFollowings}
+        {followings ? noFollowers : noFollowings}
       </div>
     </div>
   )
