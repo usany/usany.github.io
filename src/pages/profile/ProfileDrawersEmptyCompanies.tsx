5a92cf310c3fbb419fd03bd65018da5798a505ff
user <user@userui-MacBookAir.local>
2025-08-05 20:59:59 +0900

    cern

--- src/pages/profile/ProfileDrawersEmptyCompanies.tsx
+++ src/pages/profile/ProfileDrawersEmptyCompanies.tsx
@@ -1,13 +1,15 @@
-
-const ProfileDrawersEmptyCompanies = ({ user, followers, alliesCollection }) => {
-
+const ProfileDrawersEmptyCompanies = ({
+  user,
+  followers,
+  alliesCollection,
+}) => {
   return (
-    <div className='flex justify-center'>
-      <div className='rounded shadow-md bg-light-1 dark:bg-dark-1 p-5'>
+    <div className="flex justify-center">
+      <div className="rounded shadow-md bg-light-1 dark:bg-dark-1 p-5">
         {followers ? '팔로워가' : '팔로잉이'} 없습니다
       </div>
     </div>
-  );
+  )
 }
 
 export default ProfileDrawersEmptyCompanies
