7849fceec8eac6ecaa391ced04d12204a67aac89
usany <ckd_qja@naver.com>
2025-08-13 03:18:42 +0900

    cern

--- src/pages/core/navigationTop/sideNavigation/navigationSignedOut/NavigationSignedOut.tsx
+++ src/pages/core/navigationTop/sideNavigation/navigationSignedOut/NavigationSignedOut.tsx
@@ -1,7 +1,7 @@
 import Modes from 'src/pages/core/navigationTop/sideNavigation/Modes'
 import NavigationUser from '../navigationUser/NavigationUser'
 
-const NavigationSignedOut = ({ userObj, points, checkbox }) => {
+const NavigationSignedOut = ({ userObj, points }) => {
 
   return (
     <div className="flex justify-between border-b border-light-3 dark:border-dark-3 p-5 gap-5 w-[350px]">
