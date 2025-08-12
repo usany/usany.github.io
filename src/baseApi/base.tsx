e174e3520d0a6d86ce31cb12859b1735dbfbf019
usany <ckd_qja@naver.com>
2025-08-13 03:48:50 +0900

    cern

--- src/baseApi/base.tsx
+++ src/baseApi/base.tsx
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://rzltwgtrlomuybkqcpru.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6bHR3Z3RybG9tdXlia3FjcHJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ0MTQ0ODQsImV4cCI6MjAzOTk5MDQ4NH0.qo35dQrvDGnLTmjjE2zN55HhgRdNPhur8QIhphKq1lA'
-const supabase = createClient('https://ijsfbngiyhgvolsprxeh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqc2ZibmdpeWhndm9sc3ByeGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5ODA2MDksImV4cCI6MjA3MDU1NjYwOX0._tvdubZqog1Awb58KzYETJqCWuT7DbjaStPLnWdRvdk');
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 export default supabase
