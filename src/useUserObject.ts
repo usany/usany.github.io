504bd094c80250cae83aea4fd1eb265141bfe510
user <user@userui-MacBookAir.local>
2025-08-13 20:38:19 +0900

    cern

--- src/useUserObject.ts
+++ src/useUserObject.ts
@@ -3,9 +3,19 @@ import { useEffect, useState } from 'react'
 import { auth } from 'src/baseApi/serverbase'
 import 'src/global.css'
 import supabase from './baseApi/base'
+import { useDispatch } from 'react-redux'
+import { User } from 'firebase/auth'
+import { doc, getDoc } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { dbservice } from 'src/baseApi/serverbase'
+import { changeProfileColor } from 'src/stateSlices/profileColorSlice'
+import { changeProfile } from 'src/stateSlices/profileSlice'
+import { changeProfileUrl } from 'src/stateSlices/profileUrlSlice'
 
 const useUserObject = () => {
   const [userObj, setUserObj] = useState<User | null | undefined>(undefined)
+  const dispatch = useDispatch()
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       console.log(user)
@@ -16,9 +26,24 @@ const useUserObject = () => {
       }
       setUserObj(user)
     })
-    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+    const { data } = supabase.auth.onAuthStateChange(async (event, session) => {
       console.log(event, session)
       const user = { uid: data.subscription.id }
+      const docRef = doc(dbservice, `members/${userObj?.uid}`)
+      const docSnap = await getDoc(docRef)
+      const userData = docSnap.data()
+      dispatch(changeProfile(userData))
+      setUserObj(userData)
+      const userColor = docSnap.data()?.profileColor || '#2196f3'
+      const userImage = docSnap.data()?.profileImageUrl || 'null'
+      const userProfileImage = docSnap.data()?.profileImage || false
+      const userDefaultProfile = docSnap.data()?.defaultProfile || 'null'
+      dispatch(changeProfileColor(userColor))
+      if (userProfileImage) {
+        dispatch(changeProfileUrl(userImage))
+      } else {
+        dispatch(changeProfileUrl(userDefaultProfile))
+      }
       // setUserObj(user)
       console.log(data.subscription.id)
       if (event === 'INITIAL_SESSION') {
