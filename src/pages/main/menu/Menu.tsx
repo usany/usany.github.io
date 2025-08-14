5f53de1b54177c8a3b1bb55b457d0b80a1ae351d
usany <ckd_qja@naver.com>
2025-08-15 03:16:23 +0900

    cern

--- src/pages/main/menu/Menu.tsx
+++ src/pages/main/menu/Menu.tsx
@@ -6,9 +6,6 @@ import useContextMenu from './useContextMenu'
 import useGetToken from './useGetToken'
 import useSetProfile from './useSetProfile'
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState } from 'react'
-import staticImg from 'src/assets/blue.png'
-import supabase from 'src/baseApi/base'
 interface Props {
   userObj: User
 }
@@ -24,38 +21,38 @@ function Menu({ userObj }: Props) {
   useSetProfile(userObj)
   useGetToken(userObj)
   useContextMenu()
-  const [img, setImg] = useState(null)
-  const uploadImages = async () => {
-    const { data, error } = await supabase.storage
-      .from('remake')
-      .upload('publicImages.png', staticImg)
-    if (data) {
-      console.log(data)
-    }
-    if (error) {
-      console.log(error)
-    }
-  }
-  console.log(img)
-  const downloadImages = async () => {
-    const { data, error } = await supabase.storage
-      .from('remake')
-      .getPublicUrl('publicImages.png')
-    if (data) {
-      setImg(data.publicUrl)
-      console.log(data)
-    }
-    if (error) {
-      console.log(error)
-    }
-  }
+  // const [img, setImg] = useState(null)
+  // const uploadImages = async () => {
+  //   const { data, error } = await supabase.storage
+  //     .from('remake')
+  //     .upload('publicImages.png', staticImg)
+  //   if (data) {
+  //     console.log(data)
+  //   }
+  //   if (error) {
+  //     console.log(error)
+  //   }
+  // }
+  // console.log(img)
+  // const downloadImages = async () => {
+  //   const { data, error } = await supabase.storage
+  //     .from('remake')
+  //     .getPublicUrl('publicImages.png')
+  //   if (data) {
+  //     setImg(data.publicUrl)
+  //     console.log(data)
+  //   }
+  //   if (error) {
+  //     console.log(error)
+  //   }
+  // }
   return (
     <div id="sample" className="flex justify-center flex-col pb-5">
       <PageTitle title={titles[index]} />
       <Accordions userObj={userObj} />
-      <button onClick={uploadImages}>upload</button>
+      {/* <button onClick={uploadImages}>upload</button>
       <button onClick={downloadImages}>download</button>
-      {img && <img src={img} />}
+      {img && <img src={img} />} */}
       {/* <PushNotificationButton /> */}
       {/* <Avatar sx={{ bgcolor: blue[500] }} alt="Remy Sharp" src="./assets/groups.png" />
             <Avatar sx={{ bgcolor: blue[500] }} alt="Travis Howard" src="/static/images/avatar/2.jpg" />
