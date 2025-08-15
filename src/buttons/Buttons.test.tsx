246dab069333e49d0ecb4490ce12147beeb903e1
usany <ckd_qja@naver.com>
2025-08-16 00:15:22 +0900

    cern

--- src/buttons/Buttons.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Buttons from './Buttons'
-test('renders the component correctly', () => {
-  render(<Buttons />)
-  expect(screen.getByText('Send')).toBeInTheDocument()
-})
---
+++ src/buttons/SupportButton.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import SupportButton from './SupportButton'
+import { useSelectors } from 'src/hooks/useSelectors'
+test('renders the component correctly', () => {
+  const languages = useSelectors((state) => state.languages.value)
+  render(<SupportButton />)
+  expect(
+    screen.getByText(languages === 'ko' ? '승낙하기' : 'Confirm'),
+  ).toBeInTheDocument()
+})
