7f8c2ee3d13979bad41d3e865f88d0df8d1c813e
usany <ckd_qja@naver.com>
2025-08-15 23:55:52 +0900

    cern

---
+++ src/buttons/Buttons.test.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react'
+import Buttons from './Buttons'
+test('renders the component correctly', () => {
+  render(<Buttons />)
+  expect(screen.getByText('Hello, World!')).toBeInTheDocument()
+})
