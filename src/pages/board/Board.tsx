199b8f04ffb7055a2709ec899956cd8802fa0a91
usany <ckd_qja@naver.com>
2025-08-20 03:43:15 +0900

    cern

--- src/pages/board/Board.tsx
+++ src/pages/board/Board.tsx
@@ -180,7 +180,7 @@ function Board({ userObj }: Props) {
 
     const infowindow = new naver.maps.InfoWindow({
       content: contentString,
-      backgroundColor: '#357',
+      backgroundColor: '#777',
     })
     if (mapRef.current && naver) {
       const location = new naver.maps.LatLng(latitude, longitude)
