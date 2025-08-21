51263c6b2d0758bbfe7a762831957231d0435420
usany <ckd_qja@naver.com>
2025-08-22 01:09:03 +0900

    cern

--- src/pages/board/FilterDialogs/FilterDialogsContent.tsx
+++ src/pages/board/FilterDialogs/FilterDialogsContent.tsx
@@ -4,79 +4,42 @@ import {
   SelectGroup,
   SelectItem,
   SelectTrigger,
-  SelectValue
-} from "@/components/ui/select";
-import { useState } from "react";
-// import { Filter } from "lucide-react";
-import { useSelectors } from "src/hooks/useSelectors";
-import locationsBuildings from "src/pages/add/locationsBuildings";
+  SelectValue,
+} from '@/components/ui/select'
+import { useSelectors } from 'src/hooks/useSelectors'
+import locationsBuildings from 'src/pages/add/locationsBuildings'
 const itemsTitle = {
   ko: '우산 / 양산 선택',
-  en: 'Select Usan / Yangsan'
+  en: 'Select Usan / Yangsan',
 }
 const items = {
   ko: ['전체 아이템', '우산', '양산'],
-  en: ['All items', 'Usan', 'Yangsan']
+  en: ['All items', 'Usan', 'Yangsan'],
 }
 const locationsTitle = {
   ko: '장소 선택',
-  en: 'All locations'
+  en: 'Select Location',
 }
 const locations = {
   ko: ['전체 장소', ...locationsBuildings['ko']],
-  en: ['All locations', ...locationsBuildings['en']]
+  en: ['All locations', ...locationsBuildings['en']],
 }
 const timeTitle = {
   ko: '시간 정렬',
-  en: 'Time order'
+  en: 'Time order',
 }
 const time = {
   ko: ['최신순', '오래된'],
-  en: ['Recent', 'Older']
+  en: ['Recent', 'Older'],
 }
-const markers = [
-  {
-    label: '중도',
-    location: { lat: 37.5970966, lng: 127.0527314 }
-  },
-  {
-    label: '네오르네상스관',
-    location: { lat: 37.5948201, lng: 127.053091 }
-  },
-  {
-    label: '푸른솔',
-    location: { lat: 37.5941125, lng: 127.0557743 }
-  },
-  {
-    label: '간호이과대',
-    location: { lat: 37.5960528, lng: 127.0536951 }
-  },
-  {
-    label: '문과대',
-    location: { lat: 37.5971991, lng: 127.0539612 }
-  },
-  {
-    label: '청운',
-    location: { lat: 37.594732, lng: 127.0517775 }
-  },
-  {
-    label: '의과대',
-    location: { lat: 37.59390, lng: 127.0549 }
-  },
-  {
-    label: '경영대',
-    location: { lat: 37.5967052, lng: 127.0552861 }
-  },
-  {
-    label: '치과병원',
-    location: { lat: 37.594054, lng: 127.0531189 }
-  },
-]
-
-function FilterDialogsContent({ selectedValues, handleSelectedValues }) {
+interface Props {
+  selectedValues: object
+  handleSelectedValues: () => void
+}
+function FilterDialogsContent({ selectedValues, handleSelectedValues }: Props) {
   const languages = useSelectors((state) => state.languages.value)
-  const index = (languages === 'ko' || languages === 'en') ? languages : 'ko'
-  const [selected, setSelected] = useState(null);
+  const index = languages === 'ko' || languages === 'en' ? languages : 'ko'
+  // const [selected, setSelected] = useState(null)
   // const { filtering } = useTexts()
   // const onClick = ({ id }) => {
   //   setSelected(id);
@@ -84,20 +47,20 @@ function FilterDialogsContent({ selectedValues, handleSelectedValues }) {
   console.log(selectedValues)
 
   return (
-    <div className='p-5'>
+    <div className="p-5">
       <div className="flex justify-center">{itemsTitle[index]}</div>
       <Select
-        defaultValue={selectedValues[0].value || "전체 아이템"}
+        defaultValue={selectedValues[0].value || '전체 아이템'}
         onValueChange={(newValue) =>
           handleSelectedValues({
-            id: "selectedValueOne",
+            id: 'selectedValueOne',
             newValue: newValue,
           })
         }
       >
         <SelectTrigger
           className="bg-light-1 dark:bg-dark-1"
-          autoFocus={selected === selectedValues[0].id}
+          // autoFocus={selected === selectedValues[0].id}
         >
           <SelectValue />
         </SelectTrigger>
@@ -105,7 +68,10 @@ function FilterDialogsContent({ selectedValues, handleSelectedValues }) {
           <SelectGroup>
             {items[index].map((value, index) => {
               return (
-                <SelectItem key={index} value={items['ko'][index]} > {value}</SelectItem>
+                <SelectItem key={index} value={items['ko'][index]}>
+                  {' '}
+                  {value}
+                </SelectItem>
               )
             })}
             {/* <SelectItem value="전체 아이템">전체 아이템</SelectItem>
@@ -116,17 +82,17 @@ function FilterDialogsContent({ selectedValues, handleSelectedValues }) {
       </Select>
       <div className="flex justify-center">{locationsTitle[index]}</div>
       <Select
-        defaultValue={selectedValues[1].value || "전체 장소"}
+        defaultValue={selectedValues[1].value || '전체 장소'}
         onValueChange={(newValue) =>
           handleSelectedValues({
-            id: "selectedValueTwo",
+            id: 'selectedValueTwo',
             newValue: newValue,
           })
         }
       >
         <SelectTrigger
           className="bg-light-1 dark:bg-dark-1"
-          autoFocus={selected === selectedValues[1].id}
+          // autoFocus={selected === selectedValues[1].id}
         >
           <SelectValue />
         </SelectTrigger>
@@ -134,7 +100,9 @@ function FilterDialogsContent({ selectedValues, handleSelectedValues }) {
           <SelectGroup id="location">
             {locations[index].map((value, index) => {
               return (
-                <SelectItem key={index} value={locations['ko'][index]}>{value}</SelectItem>
+                <SelectItem key={index} value={locations['ko'][index]}>
+                  {value}
+                </SelectItem>
               )
             })}
             {/* <SelectItem value="전체 장소">전체 장소</SelectItem> */}
@@ -150,17 +118,17 @@ function FilterDialogsContent({ selectedValues, handleSelectedValues }) {
       </Select>
       <div className="flex justify-center">{timeTitle[index]}</div>
       <Select
-        defaultValue={selectedValues[2].value || "최신순"}
+        defaultValue={selectedValues[2].value || '최신순'}
         onValueChange={(newValue) =>
           handleSelectedValues({
-            id: "selectedValueThree",
+            id: 'selectedValueThree',
             newValue: newValue,
           })
         }
       >
         <SelectTrigger
           className="bg-light-1 dark:bg-dark-1"
-          autoFocus={selected === selectedValues[2].id}
+          // autoFocus={selected === selectedValues[2].id}
         >
           <SelectValue />
         </SelectTrigger>
@@ -168,7 +136,9 @@ function FilterDialogsContent({ selectedValues, handleSelectedValues }) {
           <SelectGroup>
             {time[index].map((value, index) => {
               return (
-                <SelectItem key={index} value={time['ko'][index]}>{value}</SelectItem>
+                <SelectItem key={index} value={time['ko'][index]}>
+                  {value}
+                </SelectItem>
               )
             })}
             {/* <SelectItem value="최신순">최신순</SelectItem>
@@ -177,7 +147,7 @@ function FilterDialogsContent({ selectedValues, handleSelectedValues }) {
         </SelectContent>
       </Select>
     </div>
-  );
+  )
 }
 
-export default FilterDialogsContent;
+export default FilterDialogsContent
