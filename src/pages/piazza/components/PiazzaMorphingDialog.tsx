d154c944c2a870e783436e0ea6187d3a57d8c116
usany <ckd_qja@naver.com>
2025-08-22 15:06:58 +0900

    cern

--- src/pages/piazza/components/PiazzaMorphingDialog.tsx
+++ src/pages/piazza/components/PiazzaMorphingDialog.tsx
@@ -1,17 +1,6 @@
 import { User } from 'firebase/auth'
-import { doc, getDoc } from 'firebase/firestore'
-import { useSearchParams } from 'react-router-dom'
-import { dbservice } from 'src/baseApi/serverbase'
-import {
-  MorphingDialog,
-  MorphingDialogClose,
-  MorphingDialogContainer,
-  MorphingDialogTrigger,
-} from 'src/components/ui/morphing-dialog'
-import useTexts from 'src/useTexts'
-import { webSocket } from 'src/webSocket'
-import PiazzaAudioCall from '../PiazzaAudioCall'
-import PiazzaCalls from '../PiazzaCalls'
+import PiazzaMorphingDialogAudioCall from './PiazzaMorphingDialogAudioCall'
+import PiazzaMorphingDialogVideoCall from './PiazzaMorphingDialogVideoCall'
 
 interface PiazzaMorphingDialogProps {
   userObj: User
@@ -20,75 +9,18 @@ interface PiazzaMorphingDialogProps {
 }
 
 function PiazzaMorphingDialog({ userObj, chattingUser, conversation }: PiazzaMorphingDialogProps) {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const { hangUp } = useTexts()
-
-  const stopCalls = async () => {
-    setSearchParams((searchParams) => {
-      searchParams.delete('call')
-      return searchParams
-    })
-    document
-      .getElementById('myScreen')
-      ?.srcObject.getTracks()
-      .forEach((track) => track.stop())
-    let toUserRef
-    let toUser
-    let messagingToken
-    let preferLanguage
-    if (chattingUser) {
-      toUserRef = doc(dbservice, `members/${chattingUser.uid}`)
-      toUser = await getDoc(toUserRef)
-      messagingToken = toUser.data()?.messagingToken
-      preferLanguage = toUser.data()?.preferLanguage
-    }
-    const passingObject = {
-      conversation: conversation,
-      isVideo: true,
-      sendingToken: messagingToken,
-      connectedUrl: `/piazza?id=${conversation}&call=video`,
-      preferLanguage: preferLanguage,
-      userUid: userObj.uid,
-      id: userObj.displayName,
-      conversationUid: chattingUser?.uid,
-      conversationName: chattingUser?.displayName,
-    }
-    console.log(passingObject)
-    webSocket.emit('quitCall', passingObject)
-  }
-
   return (
     <>
-      <MorphingDialog>
-        <MorphingDialogTrigger>
-          <div id="videoCall"></div>
-        </MorphingDialogTrigger>
-        <MorphingDialogContainer>
-          <div>
-            <div className="flex gap-5">
-              <PiazzaCalls />
-            </div>
-            <MorphingDialogClose>
-              <div onClick={stopCalls}>{hangUp}</div>
-            </MorphingDialogClose>
-          </div>
-        </MorphingDialogContainer>
-      </MorphingDialog>
-      <MorphingDialog>
-        <MorphingDialogTrigger>
-          <div id="audioCall"></div>
-        </MorphingDialogTrigger>
-        <MorphingDialogContainer>
-          <div>
-            <div className="flex gap-5">
-              <PiazzaAudioCall />
-            </div>
-            <MorphingDialogClose>
-              <div onClick={stopCalls}>{hangUp}</div>
-            </MorphingDialogClose>
-          </div>
-        </MorphingDialogContainer>
-      </MorphingDialog>
+      <PiazzaMorphingDialogVideoCall
+        userObj={userObj}
+        chattingUser={chattingUser}
+        conversation={conversation}
+      />
+      <PiazzaMorphingDialogAudioCall
+        userObj={userObj}
+        chattingUser={chattingUser}
+        conversation={conversation}
+      />
     </>
   )
 }
