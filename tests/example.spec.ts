0646f7993e7284c3ad82dacb2a0ab9b7cad96acd
usany <ckd_qja@naver.com>
2025-08-24 04:43:46 +0900

    cern

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://usany.github.io/')
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(page).toHaveTitle(/React/)
+})
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://usany.github.io/')
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(
+    page.getByRole('button', {
+      name: 'sign in',
+    }),
+  ).toBeVisible()
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  // await expect(
+  //   page.getByRole('heading', { name: 'Installation' }),
+  // ).toBeVisible()
+})
