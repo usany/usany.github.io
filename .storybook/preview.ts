5a27744980e8333dfe149f6d0ee802d7d37372db
usany <ckd_qja@naver.com>
2025-08-26 21:02:33 +0900

    cern

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,22 +1,17 @@
-import type { Preview } from "@storybook/react";
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
-// import '@fontsource/material-icons';
-// import { CssBaseline, ThemeProvider } from '@mui/material';
-// import { withThemeFromJSXProvider } from '@storybook/addon-themes';
-// import { lightTheme, darkTheme } from '../src/themes.js';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+import { darkTheme, lightTheme } from "../src/themes.js";
 
-const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-};
+/* snipped for brevity */
 
-export default preview;
+export const decorators = [
+	withThemeFromJSXProvider({
+		themes: {
+			light: lightTheme,
+			dark: darkTheme,
+		},
+		defaultTheme: "light",
+		Provider: ThemeProvider,
+		GlobalStyles: CssBaseline,
+	}),
+];
