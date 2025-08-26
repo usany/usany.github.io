908174d36ad70fd2c232863aa979187266d3818f
usany <ckd_qja@naver.com>
2025-08-26 21:07:56 +0900

    cern

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,10 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { withThemeFromJSXProvider } from "@storybook/addon-themes";
-import { darkTheme, lightTheme } from "../src/themes.js";
 
 /* snipped for brevity */
 
 export const decorators = [
 	withThemeFromJSXProvider({
-		themes: {
-			light: lightTheme,
-			dark: darkTheme,
-		},
 		defaultTheme: "light",
 		Provider: ThemeProvider,
 		GlobalStyles: CssBaseline,
