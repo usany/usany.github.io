59da11c830747ec4a608a46468ebcbfe09916dfb
usany <ckd_qja@naver.com>
2025-08-27 17:18:57 +0900

    cern

--- src/stories/Chip.stories.ts
+++ src/stories/Chip.stories.ts
@@ -2,12 +2,12 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { fn } from 'storybook/test';
 
-import { Chips } from './Button';
+import { Chips } from './Chip';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Components/Chip',
-  component: Buttons,
+  component: Chips,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
@@ -17,7 +17,7 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn(), label: 'Button', },
-} satisfies Meta<typeof Buttons>;
+} satisfies Meta<typeof Chips>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
