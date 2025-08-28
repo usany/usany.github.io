08d2ee4df7744542761f72644af53484b57ea869
user <user@userui-MacBookAir.local>
2025-08-28 19:57:49 +0900

    cern

--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -16,7 +16,7 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn(), label: 'Button' },
+  args: { onClick: fn(), label: 'Card' },
 } satisfies Meta<typeof Cards>
 
 export default meta
