c2f03aa0caf7c92b197b6f680a5540d621807c23
usany <ckd_qja@naver.com>
2025-08-28 23:19:41 +0900

    cern

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,19 +1,28 @@
-// .storybook/preview.js
+import { Preview, Renderer } from '@storybook/react';
 
-import { CssBaseline, ThemeProvider } from '@mui/material';
 import { withThemeFromJSXProvider } from '@storybook/addon-themes';
-import { lightTheme, darkTheme } from '../src/themes.js';
 
-/* snipped for brevity */
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from '../src/themes';
 
-export const decorators = [
-  withThemeFromJSXProvider({
-    themes: {
-      light: lightTheme,
-      dark: darkTheme,
-    },
-    defaultTheme: 'light',
-    Provider: ThemeProvider,
-    GlobalStyles: CssBaseline,
-  }),
-];
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+`;
+
+const preview: Preview = {
+  decorators: [
+    withThemeFromJSXProvider<Renderer>({
+      themes: {
+        light: lightTheme,
+        dark: darkTheme,
+      },
+      defaultTheme: 'light',
+      Provider: ThemeProvider,
+      GlobalStyles,
+    }),
+  ],
+};
+
+export default preview;
