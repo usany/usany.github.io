8dba2f5a3fb1255a4483603bb95cc3d3d21fcf87
usany <ckd_qja@naver.com>
2025-08-31 23:49:35 +0900

    cern

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,8 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import { fn } from 'storybook/test'
 
-import { Buttons } from './Button'
-
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Components/Button',
@@ -23,7 +21,7 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn(), label: 'Button' },
-} satisfies Meta<typeof Buttons>
+} satisfies Meta
 
 export default meta
 type Story = StoryObj<typeof meta>
