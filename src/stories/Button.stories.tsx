c4f63856365a01e3e64286a9b803c03c4654fd7e
usany <ckd_qja@naver.com>
2025-08-31 23:46:30 +0900

    cern

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -17,12 +17,6 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
-    backgrounds: {
-      options: {
-        light: { name: 'light', value: '#cbd5df' },
-        dark: { name: 'dark', value: '#1a202c' },
-      },
-    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
