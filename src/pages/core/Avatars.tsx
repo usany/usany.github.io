7c1250496fe21f140497493c9b1412155e067862
usany <ckd_qja@naver.com>
2025-09-01 18:12:28 +0900

    cern

--- src/pages/core/Avatars.tsx
+++ src/pages/core/Avatars.tsx
@@ -1,32 +1,13 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { DocumentData } from "firebase/firestore";
-import static01 from "src/assets/blue01.png";
-import static02 from "src/assets/blue02.png";
-import statics from "src/assets/blue03.png";
-import static05 from "src/assets/gold1.png";
-import static06 from "src/assets/gold2.png";
-import static03 from "src/assets/red1.png";
-import static04 from "src/assets/red2.png";
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { DocumentData } from 'firebase/firestore'
 
 interface Props {
   element: DocumentData | undefined
-  profile: boolean;
-  profileColor: string;
-  profileUrl: string;
-  defaultProfileUrl: string
+  profile: boolean
   piazza: () => void
 }
 
-const Avatars = ({
-  element,
-  profile,
-  piazza
-  // uid,
-  // profileColor,
-  // profileUrl,
-  // defaultProfileUrl,
-}: Props) => {
-  const profileImageArray = [static01, static02, static03, static04, static05, static06, statics]
+const Avatars = ({ element, profile, piazza }: Props) => {
   const profileImage = element?.profileImage
   let defaultProfile
   if (element?.defaultProfile) {
@@ -34,39 +15,26 @@ const Avatars = ({
   } else {
     defaultProfile = static05
   }
-  // console.log(element)
-  // console.log(element?.profileImageUrl)
-  // console.log(element?.defaultProfile)
   console.log(profile)
   return (
     <div>
       {profile ? (
-        <Avatar
-          className='w-48 h-48'
-        >
-          <AvatarImage src={profileImage ? element.profileImageUrl : defaultProfile} />
-          <AvatarFallback className='border border-none bg-light-1 dark:bg-dark-1'>
-          </AvatarFallback>
-          {/* {!profileImage &&
-            <AvatarFallback className="text-8xl border-none">
-              <img className='h-full' src={defaultProfile} />
-    </AvatarFallback>
-          } */}
-        </Avatar >
+        <Avatar className="w-48 h-48">
+          <AvatarImage
+            src={profileImage ? element.profileImageUrl : defaultProfile}
+          />
+          <AvatarFallback className="border border-none bg-light-1 dark:bg-dark-1"></AvatarFallback>
+        </Avatar>
       ) : (
-        <Avatar
-          onClick={piazza}
-        >
-          <AvatarImage src={profileImage ? element.profileImageUrl : defaultProfile} />
-          <AvatarFallback className='border border-none bg-light-1 dark:bg-dark-1'>
-          </AvatarFallback>
-          {/* <img className='h-full' src={defaultProfile} /> */}
-          {/* {!profileImage &&
-          } */}
+        <Avatar onClick={piazza}>
+          <AvatarImage
+            src={profileImage ? element.profileImageUrl : defaultProfile}
+          />
+          <AvatarFallback className="border border-none bg-light-1 dark:bg-dark-1"></AvatarFallback>
         </Avatar>
       )}
-    </div >
-  );
-};
+    </div>
+  )
+}
 
-export default Avatars;
+export default Avatars
