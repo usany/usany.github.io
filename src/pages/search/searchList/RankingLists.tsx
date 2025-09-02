a91bbc28eab6148896c17fd7143cebe6a8fa4163
user <user@userui-MacBookAir.local>
2025-09-02 21:19:40 +0900

    cern

--- src/pages/search/searchList/RankingLists.tsx
+++ src/pages/search/searchList/RankingLists.tsx
@@ -1,4 +1,4 @@
-import { User } from "firebase/auth";
+import { User } from 'firebase/auth'
 import {
   collection,
   doc,
@@ -7,83 +7,83 @@ import {
   limit,
   orderBy,
   query,
-  startAfter
-} from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import {
-  dbservice
-} from "src/baseApi/serverbase";
-import Lists from "src/pages/search/searchList/searchListViews/Lists";
+  startAfter,
+} from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { dbservice } from 'src/baseApi/serverbase'
+import { useSelectors } from 'src/hooks/useSelectors'
+import Lists from 'src/pages/search/searchList/searchListViews/Lists'
 
 interface Props {
-  userObj: User;
+  userObj: User
 }
 function RankingLists({ userObj }: Props) {
-  const [rank, setRank] = useState([]);
-  const [ranker, setRanker] = useState([]);
-  const [continuing, setContinuing] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [rank, setRank] = useState([])
+  const [ranker, setRanker] = useState([])
+  const [continuing, setContinuing] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   const [searchParams, setSearchParams] = useSearchParams()
+  const profile = useSelectors((state) => state.profile.value)
   const userSearch = searchParams.get('search')
-  const scrollNumber = 20;
+  const scrollNumber = 20
   useEffect(() => {
     const membersList = async () => {
       const collectionQuery = query(
-        collection(dbservice, "members"),
-        orderBy("points", "desc"),
+        collection(dbservice, 'members'),
+        orderBy('points', 'desc'),
         limit(scrollNumber),
-        startAfter(continuing ? continuing : "")
-      );
-      const docs = await getDocs(collectionQuery);
+        startAfter(continuing ? continuing : ''),
+      )
+      const docs = await getDocs(collectionQuery)
       const newArray = docs.docs.map((document, index) => {
         if (rank.indexOf(document) === -1) {
           if (index + 1 === docs.docs.length) {
-            setContinuing(document);
+            setContinuing(document)
           }
           return {
             ...document.data(),
-          };
+          }
         }
-      });
-      setRank([...rank, ...newArray]);
+      })
+      setRank([...rank, ...newArray])
       if (ranker.length === 0) {
-        const docRef = doc(dbservice, `members/${userObj.uid}`)
+        const docRef = doc(dbservice, `members/${profile?.uid}`)
         const myDocSnap = await getDoc(docRef)
         const myDocSnapData = myDocSnap.data()
         newArray.map((document, index) => {
-          if (document.uid === userObj.uid) {
-            newArray[index].rank = index + 1;
+          if (document.uid === profile?.uid) {
+            newArray[index].rank = index + 1
           }
-        });
-        setRanker([myDocSnapData]);
+        })
+        setRanker([myDocSnapData])
       }
-      setIsLoading(false);
-    };
+      setIsLoading(false)
+    }
     const searchingMembersList = async () => {
       const collectionQuery = query(
-        collection(dbservice, "members"),
-        orderBy("points", "desc"),
+        collection(dbservice, 'members'),
+        orderBy('points', 'desc'),
         // limit(scrollNumber),
-        startAfter(continuing ? continuing : "")
-      );
-      const docs = await getDocs(collectionQuery);
+        startAfter(continuing ? continuing : ''),
+      )
+      const docs = await getDocs(collectionQuery)
       const newArray = docs.docs.map((document, index) => {
-        console.log(rank.indexOf(document));
+        console.log(rank.indexOf(document))
         if (rank.indexOf(document) === -1) {
           if (index + 1 === docs.docs.length) {
-            setContinuing(document);
+            setContinuing(document)
           }
           return {
             ...document.data(),
-          };
+          }
         }
-      });
-      setRank([...rank, ...newArray]);
-      setIsLoading(false);
-    };
+      })
+      setRank([...rank, ...newArray])
+      setIsLoading(false)
+    }
     if (isLoading || rank.length === 0) {
-      membersList();
+      membersList()
     }
     if (userSearch) {
       searchingMembersList()
@@ -113,29 +113,31 @@ function RankingLists({ userObj }: Props) {
     //     }
     //   })
     // })
-  }, [isLoading, userSearch]);
+  }, [isLoading, userSearch])
   const handleScroll = () => {
     if (
-      document.documentElement.offsetHeight - (window.innerHeight + Math.round(document.documentElement.scrollTop)) > 250 ||
+      document.documentElement.offsetHeight -
+        (window.innerHeight + Math.round(document.documentElement.scrollTop)) >
+        250 ||
       isLoading
     ) {
-      console.log(document.documentElement.offsetHeight);
-      return;
+      console.log(document.documentElement.offsetHeight)
+      return
     } else {
-      console.log("scroll");
-      setIsLoading(true);
+      console.log('scroll')
+      setIsLoading(true)
     }
-  };
+  }
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [isLoading]);
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [isLoading])
   return (
-    <div className='flex truncate justify-center'>
+    <div className="flex truncate justify-center">
       {userSearch ? (
-        <div className='w-[1000px]'>
+        <div className="w-[1000px]">
           <Lists
-            userObj={userObj}
+            userObj={profile}
             elements={rank}
             multiple={true}
             userSearch={userSearch}
@@ -144,9 +146,9 @@ function RankingLists({ userObj }: Props) {
           />
         </div>
       ) : (
-        <div className='w-[1000px]'>
+        <div className="w-[1000px]">
           <Lists
-            userObj={userObj}
+            userObj={profile}
             elements={ranker}
             multiple={false}
             userSearch={null}
@@ -154,7 +156,7 @@ function RankingLists({ userObj }: Props) {
             handleUser={null}
           />
           <Lists
-            userObj={userObj}
+            userObj={profile}
             elements={rank}
             multiple={true}
             userSearch={null}
@@ -162,12 +164,14 @@ function RankingLists({ userObj }: Props) {
             handleUser={null}
           />
           {isLoading && (
-            <div className="flex justify-center text-2xl bg-light-2 dark:bg-dark-2 rounded">로딩</div>
+            <div className="flex justify-center text-2xl bg-light-2 dark:bg-dark-2 rounded">
+              로딩
+            </div>
           )}
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export default RankingLists;
+export default RankingLists
