79202cfaf82bf0f3b0689899116d8b1a42831ae9
usany <ckd_qja@naver.com>
2025-09-04 20:29:24 +0900

    cern

--- src/stateSlices/weather.ts
+++ src/stateSlices/weather.ts
@@ -1,10 +1,3 @@
-// import Typography from '@mui/material/Typography';
-// import Card from '@mui/material/Card';
-// import { CardActionArea, CardActions } from '@mui/material';
-// import { useSelector, useDispatch } from 'react-redux'
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-// import Badge from '@mui/material/Badge';
-// import Chip from '@mui/material/Chip';
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
 import axios from 'axios'
 
@@ -17,22 +10,6 @@ const getCurrentWeather = () => {
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${APIKEY}&units=metric`,
   )
   return response
-  // .then((response) => {
-  //     setWeatherInfo({
-  //         temperature: response.data.main.temp,
-  //         weather: response.data.weather[0].main,
-  //         icon: response.data.weather[0].icon,
-  //         loaded: true,
-  //     })
-  //     console.log(response)
-  // })
-  // .catch((error) => {
-  //     setWeatherInfo({
-  //         ...weatherInfo,
-  //         loaded: true,
-  //     })
-  //     showError('Failed to load weatherinfo')
-  // })
 }
 
 export const weather = createApi({
@@ -53,28 +30,6 @@ export const weather = createApi({
           return { error: error }
         }
       },
-      // query: (arg) => {
-      //   return arg
-      // }
-      // const piazzaRef = collection(dbservice, 'chats_group')
-      // const piazzaCollection = query(piazzaRef, orderBy('messageClockNumber', 'desc'), limit(1))
-      // console.log(arg)
-      // return piazzaCollection
-      // try {
-      //   const piazzaMessages = await getDocs(piazzaCollection)
-      // } catch (error) {
-      //   return { error: error }
-      // }
-      // async queryFn() {
-      //   try {
-      //     const piazzaRef = collection(dbservice, 'chats_group')
-      //     const piazzaCollection = query(piazzaRef, orderBy('messageClockNumber', 'desc'), limit(1))
-      //     const piazzaMessages = await getDocs(piazzaCollection)
-      //     return { data: piazzaMessages }
-      //   } catch (e) {
-      //     return { error: e }
-      //   }
-      // },
     }),
   }),
 })
