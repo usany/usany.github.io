d19e2d793c943084d22aaa070b370410c65efb37
usany <ckd_qja@naver.com>
2025-09-04 18:54:19 +0900

    cern

--- src/useUserObject.ts
+++ src/useUserObject.ts
@@ -1,15 +1,10 @@
-import { User } from 'firebase/auth'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { auth } from 'src/baseApi/serverbase'
 import 'src/global.css'
-// import supabase from './baseApi/base'
 import { useDispatch } from 'react-redux'
 import { doc, getDoc } from 'firebase/firestore'
 import { dbservice } from 'src/baseApi/serverbase'
-import { changeProfileColor } from 'src/stateSlices/profileColorSlice'
 import { changeProfile } from 'src/stateSlices/profileSlice'
-import { changeProfileUrl } from 'src/stateSlices/profileUrlSlice'
-import { useGetCurrentUserQuery } from './stateSlices/baseQuery'
 
 const useUserObject = () => {
   const dispatch = useDispatch()
@@ -27,20 +22,10 @@ const useUserObject = () => {
     } else {
       dispatch(changeProfile(null))
     }
-    // const userColor = docSnap.data()?.profileColor || '#2196f3'
-    // const userImage = docSnap.data()?.profileImageUrl || 'null'
-    // const userProfileImage = docSnap.data()?.profileImage || false
-    // const userDefaultProfile = docSnap.data()?.defaultProfile || 'null'
-    // dispatch(changeProfileColor(userColor))
-    // if (userProfileImage) {
-    //   dispatch(changeProfileUrl(userImage))
-    // } else {
-    //   dispatch(changeProfileUrl(userDefaultProfile))
-    // }
   }
   useEffect(() => {
-    // const reloading = sessionStorage.getItem('reloading')
     auth.onAuthStateChanged((user) => {
+      // const reloading = sessionStorage.getItem('reloading')
       // if (user === null && !reloading) {
       //   sessionStorage.setItem('reloading', 'true')
       //   location.reload()
