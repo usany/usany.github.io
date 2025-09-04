018200f46aea6b48bdb7763ee38d08fc9faa4574
usany <ckd_qja@naver.com>
2025-09-04 19:16:49 +0900

    cern

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,36 @@ import { Toaster } from './components/ui/toaster'
 import useColors from './hooks/useColors'
 import { useSelectors } from './hooks/useSelectors'
 import useUserObject from './useUserObject'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { changeOnLine } from './stateSlices/onLineSlice'
 
-// const usePreference = () => {
-//   const profile = useSelectors((state) => state.profile.value)
-//   const uid = profile?.uid
-//   const dispatch = useDispatch()
-// }
+const useNetwork = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    window.addEventListener('online', () => {
+      dispatch(changeOnLine(true))
+    })
+    window.addEventListener('offline', () => {
+      dispatch(changeOnLine(false))
+    })
+    return (() => {
+      window.removeEventListener('online', () => {
+        dispatch(changeOnLine(true))
+      })
+      window.removeEventListener('offline', () => {
+        dispatch(changeOnLine(false))
+      })
+    })
+  }, [])
+}
 
 function App() {
   // const [count, setCount] = useState(0)
   const theme = useSelectors((state) => state.theme.value)
   const profile = useSelectors((state) => state.profile.value)
   useUserObject()
+  useNetwork()
   const { lightTheme, darkTheme } = useColors()
   return (
     <>
