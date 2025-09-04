080f2ed75d05ae72b830326305c33038dab177ae
usany <ckd_qja@naver.com>
2025-09-04 19:57:01 +0900

    cern

--- src/pages/core/navigationTop/NavigationTop.tsx
+++ src/pages/core/navigationTop/NavigationTop.tsx
@@ -15,8 +15,6 @@ import NavigationTopMessages from './navigationTopMessages/NavigationTopMessages
 const NavigationTop = () => {
   const bottomNavigation = useSelectors((state) => state.bottomNavigation.value)
   const [sideNavigation, setSideNavigation] = useState(false)
-  const [renderDelayed, setRenderDelayed] = useState(false)
-  setTimeout(() => setRenderDelayed(true), 250)
   const handleSideNavigation = () => {
     setSideNavigation(!sideNavigation)
   }
@@ -28,34 +26,32 @@ const NavigationTop = () => {
     ['/', '/add', '/board'].indexOf(location.pathname) === -1
   return (
     <div className="shadow-md fixed z-50 bg-light-2 dark:bg-dark-2 rounded truncate">
-      {renderDelayed && (
-        <div className="flex justify-between w-screen items-center">
-          <Navigation handleSideNavigation={handleSideNavigation} />
-          <div className={`flex ${!largeMedia && 'flex-col'} items-center`}>
-            {scrollNavigation && scrollLocation && <NavigationScroll />}
-            <div>
-              {bottomNavigation % 2 === 0 && <ToggleTabs />}
-              {bottomNavigation === 1 && (
-                <>
-                  {profile?.certificated ? (
-                    <div className="flex gap-5">
-                      <NavigationTopCards />
-                      <NavigationTopMessages />
-                    </div>
-                  ) : (
-                    <NavigationTopLogOut />
-                  )}
-                </>
-              )}
-            </div>
+      <div className="flex justify-between w-screen items-center">
+        <Navigation handleSideNavigation={handleSideNavigation} />
+        <div className={`flex ${!largeMedia && 'flex-col'} items-center`}>
+          {scrollNavigation && scrollLocation && <NavigationScroll />}
+          <div>
+            {bottomNavigation % 2 === 0 && <ToggleTabs />}
+            {bottomNavigation === 1 && (
+              <>
+                {profile?.certificated ? (
+                  <div className="flex gap-5">
+                    <NavigationTopCards />
+                    <NavigationTopMessages />
+                  </div>
+                ) : (
+                  <NavigationTopLogOut />
+                )}
+              </>
+            )}
           </div>
-          {navigator.onLine ? (
-            <WeatherView />
-          ) : (
-            <div className="w-[145px] h-[64px] px-5"></div>
-          )}
         </div>
-      )}
+        {navigator.onLine ? (
+          <WeatherView />
+        ) : (
+          <div className="w-[145px] h-[64px] px-5"></div>
+        )}
+      </div>
     </div>
   )
 }
