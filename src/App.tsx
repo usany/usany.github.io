599050a528c47cd3b7fd19f9f6f7fa0ce14abb11
usany <ckd_qja@naver.com>
2025-09-04 18:56:40 +0900

    cern

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import { ThemeProvider } from '@mui/material/styles'
-import { doc, updateDoc } from 'firebase/firestore'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
 import 'src/global.css'
 import Lotties from 'src/lottiesAnimation/Lotties'
 import Router from 'src/pages/core/router/Router'
-import { dbservice } from './baseApi/serverbase'
 import { Toaster } from './components/ui/toaster'
 import useColors from './hooks/useColors'
 import { useSelectors } from './hooks/useSelectors'
-import { changeEn } from './stateSlices/languagesSlice'
-import { changeDark } from './stateSlices/themeSlice'
 import useUserObject from './useUserObject'
-import { useGetCurrentUserQuery } from './stateSlices/baseQuery'
 
 // const usePreference = () => {
 //   const profile = useSelectors((state) => state.profile.value)
