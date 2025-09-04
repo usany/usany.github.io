0c257b7b97a402514da690b2c733563a07e5701d
usany <ckd_qja@naver.com>
2025-09-04 19:26:36 +0900

    cern

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,7 @@ import { Toaster } from './components/ui/toaster'
 import useColors from './hooks/useColors'
 import { useSelectors } from './hooks/useSelectors'
 import useUserObject from './useUserObject'
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { changeOnLine } from './stateSlices/onLineSlice'
-
-const useNetwork = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    window.addEventListener('online', () => {
-      dispatch(changeOnLine(true))
-    })
-    window.addEventListener('offline', () => {
-      dispatch(changeOnLine(false))
-    })
-    return (() => {
-      window.removeEventListener('online', () => {
-        dispatch(changeOnLine(true))
-      })
-      window.removeEventListener('offline', () => {
-        dispatch(changeOnLine(false))
-      })
-    })
-  }, [])
-}
+import useNetwork from './useUserObject'
 
 function App() {
   // const [count, setCount] = useState(0)
---
+++ src/useNetwork.ts
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { changeOnLine } from './stateSlices/onLineSlice'
+
+const useNetwork = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    window.addEventListener('online', () => {
+      dispatch(changeOnLine(true))
+    })
+    window.addEventListener('offline', () => {
+      dispatch(changeOnLine(false))
+    })
+    return (() => {
+      window.removeEventListener('online', () => {
+        dispatch(changeOnLine(true))
+      })
+      window.removeEventListener('offline', () => {
+        dispatch(changeOnLine(false))
+      })
+    })
+  }, [])
+}
+
+export default useNetwork
