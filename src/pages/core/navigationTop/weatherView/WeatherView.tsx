0a9cd5aa656ca7372b30ff865e16ba779a8cc7ee
usany <ckd_qja@naver.com>
2025-09-04 20:36:50 +0900

    cern

--- src/pages/core/navigationTop/weatherView/WeatherView.tsx
+++ src/pages/core/navigationTop/weatherView/WeatherView.tsx
@@ -14,7 +14,7 @@ import { useGetWeatherQuery } from 'src/stateSlices/weather'
 
 const WeatherView = () => {
   const languages = useSelectors((state) => state.languages.value)
-  const { data, error, isLoading } = useGetWeatherQuery()
+  const { data, error, isLoading } = useGetWeatherQuery('')
   if (isLoading) return <div className='flex items-center px-5 w-[148px] h-[64px]'>waiting</div>
   if (error) return <div className='flex items-center px-5 w-[148px] h-[64px]'>failed</div>
   return (
