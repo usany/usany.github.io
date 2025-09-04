92ad85aa41c12b78941f385047abf9c29b30ab14
usany <ckd_qja@naver.com>
2025-09-04 20:38:33 +0900

    cern

--- src/pages/core/navigationTop/weatherView/WeatherView.tsx
+++ src/pages/core/navigationTop/weatherView/WeatherView.tsx
@@ -1,16 +1,5 @@
-import axios from 'axios'
-import { useSelector } from 'react-redux'
 import { useSelectors } from 'src/hooks'
 import { useGetWeatherQuery } from 'src/stateSlices/weather'
-
-// const getCurrentWeather = () => {
-//   const APIKEY = 'e9f8a415cef0c0bb87f7da5e167bdaf1'
-//   const latitude = 37.5948
-//   const longitude = 127.0531
-
-//   const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${APIKEY}&units=metric`)
-//   return response
-// }
 
 const WeatherView = () => {
   const languages = useSelectors((state) => state.languages.value)
