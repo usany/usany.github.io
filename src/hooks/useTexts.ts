74e7de76af802e8483eb51fa5f73424a61bc13c5
usany <ckd_qja@naver.com>
2025-09-04 20:39:46 +0900

    cern

---
+++ src/hooks/useTexts.ts
@@ -0,0 +1,11 @@
+import { useSelectors } from './hooks'
+import texts from './texts.json'
+
+const useTexts = () => {
+  const languages = useSelectors((state) => state.languages.value)
+  const selectedText = texts[languages]
+
+  return selectedText
+}
+
+export default useTexts
