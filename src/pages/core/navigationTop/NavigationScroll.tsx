dbca8f5dd01c1cfafb18337506131194b2508b26
usany <ckd_qja@naver.com>
2025-09-05 20:31:14 +0900

    cern

--- src/pages/core/navigationTop/NavigationScroll.tsx
+++ src/pages/core/navigationTop/NavigationScroll.tsx
@@ -1,6 +1,6 @@
 import { MessagesSquare, SearchCheck, Siren, UserRound } from "lucide-react";
 import { useSelectors } from "src/hooks";
-
+import texts from '../../../texts.json'
 type RoutePath = '/profile' | '/ranking' | '/piazza' | '/contact';
 
 interface Props {
@@ -17,38 +17,36 @@ interface Props {
     '/contact': string,
   },
 }
-const icons: Record<RoutePath, JSX.Element> = {
+const icons = {
   '/profile': <UserRound />,
   '/ranking': <SearchCheck />,
   '/piazza': <MessagesSquare />,
   '/contact': <Siren />,
 }
-const texts: Props = {
-  ko: {
-    '/profile': '프로필',
-    '/ranking': '랭킹',
-    '/piazza': '채팅',
-    '/contact': '신고'
-  },
-  en: {
-    '/profile': 'User Profile',
-    '/ranking': 'User Ranking',
-    '/piazza': 'Chats',
-    '/contact': 'Report'
-  }
-}
-// Type guard to check if a string is a valid RoutePath
-const isRoutePath = (path: string): path is RoutePath => {
+// const texts: Props = {
+//   ko: {
+//     '/profile': '프로필',
+//     '/ranking': '랭킹',
+//     '/piazza': '채팅',
+//     '/contact': '신고'
+//   },
+//   en: {
+//     '/profile': 'User Profile',
+//     '/ranking': 'User Ranking',
+//     '/piazza': 'Chats',
+//     '/contact': 'Report'
+//   }
+// }
+
+const isRoutePath = (path: string) => {
   return ['/profile', '/ranking', '/piazza', '/contact'].includes(path);
 };
 
 const NavigationScroll = () => {
   const languages = useSelectors(state => state.languages.value) as 'ko' | 'en';
   const currentPath = location.pathname as RoutePath;
-
-  // Only render if the current path is a valid RoutePath
   if (!isRoutePath(currentPath)) {
-    return null; // or some fallback UI
+    return null
   }
 
   return (
