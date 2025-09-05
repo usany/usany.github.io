90d938a7d4e3d5a4ab8f2d0e11edaac37503257e
usany <ckd_qja@naver.com>
2025-09-05 18:52:03 +0900

    cern

--- src/pages/core/Avatars.tsx
+++ src/pages/core/Avatars.tsx
@@ -11,14 +11,12 @@ const Avatars = ({ element, profile, piazza }: Props) => {
   const profileImage = element?.profileImage
   const defaultProfile = element?.defaultProfile
   return (
-    <div>
-      <Avatar className={profile ? "w-48 h-48":''} onClick={profile ? piazza : undefined}>
-        <AvatarImage
-          src={profileImage ? element.profileImageUrl : defaultProfile}
-        />
-        <AvatarFallback className="border border-none bg-light-1 dark:bg-dark-1"></AvatarFallback>
-      </Avatar>
-    </div>
+    <Avatar className={profile ? "w-48 h-48":''} onClick={profile ? piazza : undefined}>
+      <AvatarImage
+        src={profileImage ? element.profileImageUrl : defaultProfile}
+      />
+      <AvatarFallback className="border border-none bg-light-1 dark:bg-dark-1"></AvatarFallback>
+    </Avatar>
   )
 }
 
