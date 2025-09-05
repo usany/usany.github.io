6ed685cb83198ee813b7fdbbed958ca241053e7a
usany <ckd_qja@naver.com>
2025-09-05 17:55:29 +0900

    cern

--- src/pages/core/navigationTop/weatherView/WeatherView.tsx
+++ src/pages/core/navigationTop/weatherView/WeatherView.tsx
@@ -1,13 +1,14 @@
-import { useSelectors } from 'src/hooks'
+import { useSelectors, useTexts } from 'src/hooks'
 import { useGetWeatherQuery } from 'src/stateSlices/weather'
 
 const WeatherView = () => {
   const languages = useSelectors((state) => state.languages.value)
+  const { loading, error } = useTexts()
   const LATITUDE = 37.5948
   const LONGITUDE = 127.0531
   const { data, error, isLoading } = useGetWeatherQuery({latitude: LATITUDE, longitude: LONGITUDE})
-  if (isLoading) return <div className='flex items-center px-5 w-[148px] h-[64px]'>waiting</div>
-  if (error) return <div className='flex items-center px-5 w-[148px] h-[64px]'>failed</div>
+  if (isLoading) return <div className='flex items-center px-5 w-[148px] h-[64px]'>{loading}</div>
+  if (error) return <div className='flex items-center px-5 w-[148px] h-[64px]'>{error}</div>
   return (
     <div>
       <div className='flex flex-col px-5'>
