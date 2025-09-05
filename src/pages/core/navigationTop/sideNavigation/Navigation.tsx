e093fc2f59fa77aab34ac2286ed8c75821f61b8b
usany <ckd_qja@naver.com>
2025-09-05 19:52:23 +0900

    cern

--- src/pages/core/navigationTop/sideNavigation/Navigation.tsx
+++ src/pages/core/navigationTop/sideNavigation/Navigation.tsx
@@ -122,7 +122,7 @@ function Navigation({ handleSideNavigation }: Props) {
                     value.href === '/contact') {
                   return drawerLinks;
                 }
-                return null; // Explicitly return null for other cases
+                return null
               })}
             </div>
           ) : (
