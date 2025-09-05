f70814e2d0c4bc2d2c3bac0c02cc723ace03aef1
usany <ckd_qja@naver.com>
2025-09-05 20:31:51 +0900

    cern

--- src/pages/core/navigationTop/NavigationScroll.tsx
+++ src/pages/core/navigationTop/NavigationScroll.tsx
@@ -3,40 +3,12 @@ import { useSelectors } from "src/hooks";
 import texts from '../../../texts.json'
 type RoutePath = '/profile' | '/ranking' | '/piazza' | '/contact';
 
-interface Props {
-  ko: {
-    '/profile': string,
-    '/ranking': string,
-    '/piazza': string,
-    '/contact': string,
-  },
-  en: {
-    '/profile': string,
-    '/ranking': string,
-    '/piazza': string,
-    '/contact': string,
-  },
-}
 const icons = {
   '/profile': <UserRound />,
   '/ranking': <SearchCheck />,
   '/piazza': <MessagesSquare />,
   '/contact': <Siren />,
 }
-// const texts: Props = {
-//   ko: {
-//     '/profile': '프로필',
-//     '/ranking': '랭킹',
-//     '/piazza': '채팅',
-//     '/contact': '신고'
-//   },
-//   en: {
-//     '/profile': 'User Profile',
-//     '/ranking': 'User Ranking',
-//     '/piazza': 'Chats',
-//     '/contact': 'Report'
-//   }
-// }
 
 const isRoutePath = (path: string) => {
   return ['/profile', '/ranking', '/piazza', '/contact'].includes(path);
