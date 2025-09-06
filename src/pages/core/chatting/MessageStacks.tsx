8450bbba7aadd38488ef27d66e38f28e2ebc111e
usany <ckd_qja@naver.com>
2025-09-06 15:47:46 +0900

    cern

--- src/pages/core/chatting/MessageStacks.tsx
+++ src/pages/core/chatting/MessageStacks.tsx
@@ -1,4 +1,3 @@
-import { User } from 'firebase/auth'
 import { useSelectors } from 'src/hooks'
 import ChattingStacks from './ChattingStacks'
 import EmptyChattingStacks from './EmptyChattingStacks'
