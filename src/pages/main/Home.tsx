b8198719c89ac374ccb868d48323f5cc5c60a15f
usany <ckd_qja@naver.com>
2025-09-06 14:21:30 +0900

    cern

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -1,33 +1,9 @@
 import { useSelectors } from 'src/hooks'
 import Auth from 'src/pages/main/auth/Auth'
 import Menu from 'src/pages/main/menu/Menu'
-// import { doc, getDoc } from 'firebase/firestore'
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { dbservice } from 'src/baseApi/serverbase'
-// import { changeBottomNavigation } from 'src/stateSlices/bottomNavigationSlice'
-// import { changeUserCertificated } from 'src/stateSlices/userCertificatedSlice'
 
 function Home() {
   const profile = useSelectors((state) => state.profile.value)
-  // const bottomNavigation = useSelectors((state) => state.bottomNavigation.value)
-  // const dispatch = useDispatch()
-  // useEffect(() => {
-  //   if (bottomNavigation === 5) {
-  //     dispatch(changeBottomNavigation(1))
-  //   }
-  // }, [profile])
-  // useEffect(() => {
-  //   const userCertification = async () => {
-  //     const userRef = doc(dbservice, `members/${profile?.uid}`)
-  //     const userDoc = await getDoc(userRef)
-  //     const { certificated } = userDoc.data()
-  //     dispatch(changeUserCertificated(certificated))
-  //   }
-  //   if (profile) {
-  //     userCertification()
-  //   }
-  // }, [profile])
   return (
     <>
       {profile?.certificated ? (
