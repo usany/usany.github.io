e8f89c9cd1fc6677d81449e65500fe7f469c12bb
usany <ckd_qja@naver.com>
2025-09-08 00:49:39 +0900

    cern

--- src/pages/contact/ContactDrawersTrigger.tsx
+++ src/pages/contact/ContactDrawersTrigger.tsx
@@ -1,20 +1,11 @@
-import { useEffect, useState } from 'react'
 import Button from '@mui/material/Button'
-import { collection, deleteDoc, doc, getDocs, query } from 'firebase/firestore'
-import { dbservice } from 'src/baseApi/serverbase'
-import { useSelectors } from 'src/hooks'
+import { useSelectors, useTexts } from 'src/hooks'
 
-const reportList = {
-  ko: '신고하기 내역',
-  en: 'Report list',
-}
 const ContactDrawersTrigger = () => {
-  const languages = useSelectors((state) => state.languages.value)
-  const index = languages === 'ko' || languages === 'en' ? languages : 'ko'
-
+  const {reportList} = useTexts()
   return (
     <Button variant="outlined" form="auth">
-      {reportList[index]}
+      {reportList}
     </Button>
   )
 }
