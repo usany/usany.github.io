9a57ffbd20bc7fbb88a7c7d83f281a17e9938e8f
usany <ckd_qja@naver.com>
2025-09-08 02:23:56 +0900

    cern

--- src/pages/contact/ContactFormDrawersContent.tsx
+++ src/pages/contact/ContactFormDrawersContent.tsx
@@ -1,9 +1,8 @@
 import TextField from '@mui/material/TextField'
 import { collection, DocumentData, getDocs, orderBy, query } from 'firebase/firestore'
-import { getDownloadURL, ref } from 'firebase/storage'
 import { useEffect, useState } from 'react'
-import { dbservice, storage } from 'src/baseApi/serverbase'
-import { useSelectors, useTexts } from 'src/hooks'
+import { dbservice } from 'src/baseApi/serverbase'
+import { useTexts } from 'src/hooks'
 import Lists from 'src/pages/search/searchList/searchListViews/Lists'
 
 interface Props {
@@ -14,9 +13,6 @@ interface Props {
 
 function ContactFormDrawersContent({ changeViolationUser }: Props) {
   const [users, setUsers] = useState<DocumentData[]>([])
-  // const [loadedImage, setLoadedImage] = useState<
-  //   { url: string; index: number }[]
-  // >([])
   const [userSearch, setUserSearch] = useState('')
   const {userName} = useTexts()
   const onChangeUserSearch = (event: { target: { value: string } }) => {
@@ -33,15 +29,7 @@ function ContactFormDrawersContent({ changeViolationUser }: Props) {
         orderBy('points', 'desc'),
       )
       const docs = await getDocs(collectionQuery)
-      const newArray = docs.docs.map((document, index) => {
-        // getDownloadURL(ref(storage, `${document.data()?.uid}`))
-        //   .then((url) => {
-        //     setLoadedImage([...loadedImage, { url: url, index: index }])
-        //   })
-        //   .catch((error) => {
-        //     console.log(error)
-        //   })
-
+      const newArray = docs.docs.map((document) => {
         return {
           ...document.data(),
         }
