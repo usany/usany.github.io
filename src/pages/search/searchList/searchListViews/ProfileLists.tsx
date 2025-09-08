1b6779e6ff1469c68f7ade1faa670e58897b9752
usany <ckd_qja@naver.com>
2025-09-09 03:29:35 +0900

    cern

--- src/pages/search/searchList/searchListViews/ProfileLists.tsx
+++ src/pages/search/searchList/searchListViews/ProfileLists.tsx
@@ -1,7 +1,16 @@
 // import Avatar from '@mui/material/Avatar';
 import Lists from './Lists'
 
-function ProfileLists({ userObj, elements }) {
+function ProfileLists({
+  userObj,
+  elements,
+  changeProfileDialog,
+  changeSelectedUser,
+  multiple,
+  userSearch,
+  ranking,
+  handleUser,
+}) {
   return (
     <div>
       <Lists
@@ -11,39 +20,6 @@ function ProfileLists({ userObj, elements }) {
         ranking={false}
         handleUser={null}
       />
-      {/* {elements.map((element, index) => {
-        const userNameConfirm = true;
-        if (userNameConfirm) {
-          let displayName;
-          if (element.displayName.length > 10) {
-            displayName = element.displayName.slice(0, 10) + "......";
-          } else {
-            displayName = element.displayName.slice(0, 10) + "......";
-          }
-          if (largeMedia) {
-            return (
-              <Link
-                to={'/profile'}
-                state={{ element: element }}
-              >
-                <DialogClose>
-                  <ProfileListsView element={element} displayName={displayName} changeSelectedUser={changeSelectedUser} />
-                </DialogClose>
-              </Link>
-            )
-          }
-          return (
-            <Link
-              to={'/profile'}
-              state={{ element: element }}
-            >
-              <DrawerClose>
-                <ProfileListsView element={element} displayName={displayName} changeSelectedUser={changeSelectedUser} />
-              </DrawerClose>
-            </Link>
-          );
-        }
-      })} */}
     </div>
   )
 }
