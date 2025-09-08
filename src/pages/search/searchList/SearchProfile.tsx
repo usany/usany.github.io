95a9371a6cc3130fd91801afcb75444ba82e4680
usany <ckd_qja@naver.com>
2025-09-08 20:27:21 +0900

    cern

--- src/pages/search/searchList/SearchProfile.tsx
+++ src/pages/search/searchList/SearchProfile.tsx
@@ -1,30 +1,10 @@
-import {
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  limit,
-  orderBy,
-  query,
-  startAfter,
-} from 'firebase/firestore'
-import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import { dbservice } from 'src/baseApi/serverbase'
 import { useSelectors, useTexts } from 'src/hooks'
 import Lists from 'src/pages/search/searchList/searchListViews/Lists'
-import RankingListsTitle from './searchListViews/searchListViewsTitle/RankingListsTitle'
 
 function SearchProfile() {
-  const [rank, setRank] = useState([])
-  const [ranker, setRanker] = useState([])
-  const [continuing, setContinuing] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
   const [searchParams, setSearchParams] = useSearchParams()
   const profile = useSelectors((state) => state.profile.value)
-  const userSearch = searchParams.get('search')
-  const scrollNumber = 20
-  const {loading} = useTexts()
 
   if (searchParams.get('search')) return null
   return (
