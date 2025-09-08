d6e2be9d8bd2eae5dfa337bbdd12c2290614d85b
usany <ckd_qja@naver.com>
2025-09-08 19:02:30 +0900

    cern

--- src/pages/search/searchList/searchListViews/ListsView.tsx
+++ src/pages/search/searchList/searchListViews/ListsView.tsx
@@ -65,10 +65,8 @@ const ListsView = ({ elements, userSearch, multiple, link, handleUser }) => {
               onClick={onClick}
             >
               <div
-                className={`flex truncate justify-around gap-1 p-3 rounded ranking-${
-                  location.pathname === '/ranking' &&
-                  (multiple ? index + 1 : element.rank)
-                } ${
+                className={`flex truncate justify-around gap-1 p-3 rounded
+                ${
                   location.pathname === '/ranking' && multiple && index <3 &&
                   `bg-[#e2e8f0] dark:bg-[#2d3848]`
                 }`}
