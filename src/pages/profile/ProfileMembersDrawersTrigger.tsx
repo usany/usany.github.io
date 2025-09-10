c60a47c6e06034613459187734adfc49ca26dcb1
usany <ckd_qja@naver.com>
2025-09-10 20:06:28 +0900

    cern

--- src/pages/profile/ProfileMembersDrawersTrigger.tsx
+++ src/pages/profile/ProfileMembersDrawersTrigger.tsx
@@ -1,21 +1,17 @@
 import { Card } from "@mui/material";
 import useCardsBackground from "src/hooks/useCardsBackground";
-import { useSelectors } from "src/hooks";
+import { useSelectors, useTexts } from "src/hooks";
 
-const ProfileMembersDrawersTrigger = () => {
+const ProfileMembersDrawersTrigger = ({isPassword}) => {
   const { colorTwo } = useCardsBackground()
   const languages = useSelectors((state) => state.languages.value)
-
+  const {changePassword, deleteAccount} = useTexts()
   return (
     <Card sx={{ width: "100%", bgcolor: colorTwo }}>
       <div
         className="flex justify-center p-5"
       >
-        {languages === 'ko' ?
-          '회원 탈퇴'
-          :
-          'Delete Account'
-        }
+        {isPassword ? changePassword : deleteAccount}
       </div>
     </Card>
   );
