18eb50c3301110e2391484f63f101cb1e6cee8f1
usany <ckd_qja@naver.com>
2025-09-12 19:49:56 +0900

    cern

--- src/pages/profile/ProfileCompaniesTrigger.tsx
+++ src/pages/profile/ProfileCompaniesTrigger.tsx
@@ -5,15 +5,12 @@ const ProfileCompaniesTrigger = ({
   alliesCollection,
   onClick,
 }) => {
-  const languages = useSelectors((state) => state.languages.value)
-  const index = languages === 'ko' || languages === 'en' ? languages : 'ko'
   const {follower, following} = useTexts()
   return (
     <div className="p-5" onClick={onClick}>
       <div>{followers ? follower : following}</div>
       <div className="flex justify-center">
         {alliesCollection.length}
-        {languages === 'ko' && '명'}
       </div>
     </div>
   )
