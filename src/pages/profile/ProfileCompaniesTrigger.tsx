9ff46339070b4df7b3b1527fe9ca4d58940ec0fe
usany <ckd_qja@naver.com>
2025-09-12 20:24:35 +0900

    cern

--- src/pages/profile/ProfileCompaniesTrigger.tsx
+++ src/pages/profile/ProfileCompaniesTrigger.tsx
@@ -1,11 +1,24 @@
+import { collection, getDocs, query } from 'firebase/firestore'
+import { dbservice } from 'src/baseApi/serverbase'
 import { useTexts } from 'src/hooks'
 
 const ProfileCompaniesTrigger = ({
   followers,
   alliesCollection,
-  usersCollection,
+  handleCompanies,
 }) => {
   const {follower, following} = useTexts()
+  const usersCollection = async (index) => {
+    const elementsCollection = []
+    const collectionRef = collection(dbservice, 'members')
+    const docs = await getDocs(query(collectionRef))
+    docs.forEach((element) => {
+      if (alliesCollection[index].list.indexOf(element.data().uid) !== -1) {
+        elementsCollection.push(element.data())
+      }
+    })
+    handleCompanies(elementsCollection)
+  }
 
   return (
     <div className="p-5" onClick={usersCollection}>
