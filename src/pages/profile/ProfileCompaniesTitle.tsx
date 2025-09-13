d433516333907201016c74d2d90066aaf0b01c33
usany <ckd_qja@naver.com>
2025-09-14 02:42:39 +0900

    cern

--- src/pages/profile/ProfileCompaniesTitle.tsx
+++ src/pages/profile/ProfileCompaniesTitle.tsx
@@ -1,9 +1,10 @@
-import { useTexts } from 'src/hooks'
+import { useSelectors, useTexts } from 'src/hooks'
 import { useLocation } from 'react-router-dom'
 
 const ProfileCompaniesTitle = ({
   followers,
 }) => {
+  const profile = useSelectors((state) => state.profile.value)
   const { follower, following } = useTexts()
   const { state } = useLocation()
   const user = state?.element || profile
