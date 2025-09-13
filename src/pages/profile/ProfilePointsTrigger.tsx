acf12437f21a11e81d52007e19e7a57bd784e8eb
usany <ckd_qja@naver.com>
2025-09-14 02:25:24 +0900

    cern

--- src/pages/profile/ProfilePointsTrigger.tsx
+++ src/pages/profile/ProfilePointsTrigger.tsx
@@ -1,12 +1,22 @@
+import { Card } from "@mui/material"
 import { useTexts } from "src/hooks"
+import useCardsBackground from "src/hooks/useCardsBackground"
 
 const ProfilePointsTrigger = ({ cards }) => {
   const {points} = useTexts()
+  const { colorTwo } = useCardsBackground()
+
   return (
-    <div className='p-5'>
-      <div>{points}</div>
-      <div className='flex justify-center'>{cards.point}</div>
-    </div>
+    <Card
+      sx={{
+        bgcolor: colorTwo,
+      }}
+    >
+      <div className='p-5'>
+        <div>{points}</div>
+        <div className='flex justify-center'>{cards.point}</div>
+      </div>
+    </Card>
   )
 }
 
