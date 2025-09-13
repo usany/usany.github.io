9cfbb70d1c6baef7595c606a53d06d5db1a5f54d
usany <ckd_qja@naver.com>
2025-09-14 02:52:29 +0900

    cern

--- src/pages/profile/ProfilePointsTitle.tsx
+++ src/pages/profile/ProfilePointsTitle.tsx
@@ -2,8 +2,6 @@ import { useLocation } from 'react-router-dom'
 import { useSelectors, useTexts } from 'src/hooks'
 
 const ProfilePointsTitle = () => {
-  const profile = useSelectors((state) => state.profile.value)
-  const { state } = useLocation()
   const { pointReceipt } = useTexts()
   return (
     <div className="flex justify-center">{`${pointReceipt}`}</div>
