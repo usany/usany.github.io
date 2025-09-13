78de8a5d022af7c16ff55431e11aefda6f1ef416
usany <ckd_qja@naver.com>
2025-09-14 02:41:58 +0900

    cern

--- src/pages/profile/ProfileCompaniesTitle.tsx
+++ src/pages/profile/ProfileCompaniesTitle.tsx
@@ -1,10 +1,13 @@
 import { useTexts } from 'src/hooks'
+import { useLocation } from 'react-router-dom'
 
 const ProfileCompaniesTitle = ({
-  user,
   followers,
 }) => {
   const { follower, following } = useTexts()
+  const { state } = useLocation()
+  const user = state?.element || profile
+
   return (
     <div className="flex justify-center">
       {user.displayName} {followers ? follower : following}
