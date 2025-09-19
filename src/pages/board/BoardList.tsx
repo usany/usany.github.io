d100ef3e8e2bfe5e9089eedc5f677cb80a72e8a7
usany <ckd_qja@naver.com>
2025-09-19 18:12:09 +0900

    cern

--- src/pages/board/BoardList.tsx
+++ src/pages/board/BoardList.tsx
@@ -1,22 +1,16 @@
 import { ScrollText } from "lucide-react";
-import { useSelectors } from "src/hooks";
+import { useSelectors, useTexts } from "src/hooks";
 import FilterDialogsTrigger from "./FilterDialogs/FilterDialogsTrigger";
 
-const cardList = {
-  ko: '카드 목록',
-  en: 'Card list'
-}
 
 function BoardList() {
-
-  const languages = useSelectors((state) => state.languages.value)
-  const index = (languages === 'ko' || languages === 'en') ? languages : 'ko'
+  const {cardList} = useTexts()
 
   return (
     <div className="rounded shadow-md flex p-3 justify-between bg-light-2/50 dark:bg-dark-2/50">
       <div className="truncate pt-1">
         <div className='flex gap-5'>
-          <ScrollText />{cardList[index]}
+          <ScrollText />{cardList}
         </div>
       </div>
       <div className="truncate flex gap-1">
