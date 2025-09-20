aa6d5057781c09d282d25ccc05bc0e31aa13ca4d
usany <ckd_qja@naver.com>
2025-09-21 02:43:21 +0900

    cern

--- src/pages/add/AddCards.tsx
+++ src/pages/add/AddCards.tsx
@@ -69,116 +69,114 @@ const AddCards = ({ borrow, item, fromTo, locationState, display }: Props) => {
           }}
         >
           <CardContent sx={{ padding: '5px' }}>
-            <div>
-              <div className="flex justify-between gap-1">
-                <Avatars element={profile} profile={false} piazza={null} />
-                {item && (
-                  <div className="flex items-center">
-                    <Chip
-                      label={
-                        <div className="text-xs">
-                          {languages === 'ko'
-                            ? item
-                            : item === '우산'
-                            ? 'Umbrella'
-                            : 'Yangsan'}{' '}
-                          {languages === 'ko'
-                            ? borrow
-                              ? ' 빌리기'
-                              : ' 빌려주기'
-                            : borrow
-                            ? ' borrowing'
-                            : ' lending'}
-                        </div>
-                      }
+            <div className="flex justify-between gap-1">
+              <Avatars element={profile} profile={false} piazza={null} />
+              {item && (
+                <div className="flex items-center">
+                  <Chip
+                    label={
+                      <div className="text-xs">
+                        {languages === 'ko'
+                          ? item
+                          : item === '우산'
+                          ? 'Umbrella'
+                          : 'Yangsan'}{' '}
+                        {languages === 'ko'
+                          ? borrow
+                            ? ' 빌리기'
+                            : ' 빌려주기'
+                          : borrow
+                          ? ' borrowing'
+                          : ' lending'}
+                      </div>
+                    }
+                  />
+                </div>
+              )}
+            </div>
+            {!item ? (
+              <div className="flex justify-center pt-5">
+                {languages === 'ko' ? '빈 카드입니다' : 'Empty card'}
+              </div>
+            ) : (
+              <div>
+                {locationState.locationOne && (
+                  <div className="flex justify-center pt-1">
+                    <CardMedia
+                      sx={{
+                        width: 159 * 0.9,
+                        height: 141 * 0.9,
+                      }}
+                      image={staticImg}
                     />
                   </div>
                 )}
-              </div>
-              {!item ? (
-                <div className="flex justify-center pt-5">
-                  {languages === 'ko' ? '빈 카드입니다' : 'Empty card'}
-                </div>
-              ) : (
-                <div>
-                  {locationState.locationOne && (
-                    <div className="flex justify-center pt-1">
-                      <CardMedia
-                        sx={{
-                          width: 159 * 0.9,
-                          height: 141 * 0.9,
-                        }}
-                        image={staticImg}
-                      />
-                    </div>
-                  )}
-                  <div className="flex flex-col pt-1 gap-1 text-xs">
-                    {locationState && (
-                      <div className="flex gap-1 items-center">
-                        {locationState?.locationOne && <Building />}
-                        <div className="flex items-center">
-                          {languages === 'ko'
-                            ? locationState?.locationOne
-                            : locationsBuildings['en'][
-                                locationsBuildings['ko'].indexOf(
+                <div className="flex flex-col pt-1 gap-1 text-xs">
+                  {locationState && (
+                    <div className="flex gap-1 items-center">
+                      {locationState?.locationOne && <Building />}
+                      <div className="flex items-center">
+                        {languages === 'ko'
+                          ? locationState?.locationOne
+                          : locationsBuildings['en'][
+                              locationsBuildings['ko'].indexOf(
+                                locationState?.locationOne,
+                              )
+                            ]}{' '}
+                        {languages === 'ko'
+                          ? locationState?.locationTwo
+                          : locationState?.locationOne &&
+                            locationsCollection['en'][
+                              Object.keys(locationsCollectionLetters).find(
+                                (key) =>
+                                  locationsCollectionLetters[key] ===
                                   locationState?.locationOne,
-                                )
-                              ]}{' '}
-                          {languages === 'ko'
-                            ? locationState?.locationTwo
-                            : locationState?.locationOne &&
-                              locationsCollection['en'][
+                              )
+                            ][
+                              locationsCollection['ko'][
                                 Object.keys(locationsCollectionLetters).find(
                                   (key) =>
                                     locationsCollectionLetters[key] ===
                                     locationState?.locationOne,
                                 )
-                              ][
-                                locationsCollection['ko'][
-                                  Object.keys(locationsCollectionLetters).find(
-                                    (key) =>
-                                      locationsCollectionLetters[key] ===
-                                      locationState?.locationOne,
-                                  )
-                                ].indexOf(locationState?.locationTwo)
-                              ]}{' '}
-                          {locationState?.locationThree}
-                        </div>
+                              ].indexOf(locationState?.locationTwo)
+                            ]}{' '}
+                        {locationState?.locationThree}
                       </div>
-                    )}
-                    {fromTo.from && (
-                      <div className="flex gap-1 items-center">
-                        <Watch />
-                        <div className="flex flex-col justify-center">
+                    </div>
+                  )}
+                  {fromTo.from && (
+                    <div className="flex gap-1 items-center">
+                      <Watch />
+                      <div className="flex flex-col justify-center">
+                        <div className="flex">
+                          {languages === 'en' && (
+                            <div className="w-[40px]">From</div>
+                          )}
+                          {fromTo.from.year}.{fromTo.from.month < 10 && '0'}
+                          {fromTo.from.month}.{fromTo.from.day < 10 && '0'}
+                          {fromTo.from.day} {fromTo.from.hour < 10 && '0'}
+                          {fromTo.from.hour}:{fromTo.from.minute < 10 && '0'}
+                          {fromTo.from.minute} {languages === 'ko' && '부터'}
+                        </div>
+                        {fromTo.to && (
                           <div className="flex">
                             {languages === 'en' && (
-                              <div className="w-[40px]">From</div>
+                              <div className="w-[40px]">To</div>
                             )}
-                            {fromTo.from.year}.{fromTo.from.month < 10 && '0'}
-                            {fromTo.from.month}.{fromTo.from.day < 10 && '0'}
-                            {fromTo.from.day} {fromTo.from.hour < 10 && '0'}
-                            {fromTo.from.hour}:{fromTo.from.minute < 10 && '0'}
-                            {fromTo.from.minute} {languages === 'ko' && '부터'}
+                            {fromTo.to.year}.{fromTo.to.month < 10 && '0'}
+                            {fromTo.to.month}.{fromTo.from.day < 10 && '0'}
+                            {fromTo.to.day} {fromTo.to.hour < 10 && '0'}
+                            {fromTo.to.hour}:{fromTo.to.minute < 10 && '0'}
+                            {fromTo.to.minute} {languages === 'ko' && '까지'}
                           </div>
-                          {fromTo.to && (
-                            <div className="flex">
-                              {languages === 'en' && (
-                                <div className="w-[40px]">To</div>
-                              )}
-                              {fromTo.to.year}.{fromTo.to.month < 10 && '0'}
-                              {fromTo.to.month}.{fromTo.from.day < 10 && '0'}
-                              {fromTo.to.day} {fromTo.to.hour < 10 && '0'}
-                              {fromTo.to.hour}:{fromTo.to.minute < 10 && '0'}
-                              {fromTo.to.minute} {languages === 'ko' && '까지'}
-                            </div>
-                          )}
-                        </div>
+                        )}
                       </div>
-                    )}
-                  </div>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
+              </div>
+            )}
           </CardContent>
         </Card>
       </AnimatedList>
