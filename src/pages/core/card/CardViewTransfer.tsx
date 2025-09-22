3e33532ade89822ee6813189f74edaa731563cb5
usany <ckd_qja@naver.com>
2025-09-22 20:12:50 +0900

    cern

--- src/pages/core/card/CardViewTransfer.tsx
+++ src/pages/core/card/CardViewTransfer.tsx
@@ -1,27 +1,26 @@
 import East from '@mui/icons-material/East'
 import West from '@mui/icons-material/West'
 import { useLocation } from 'react-router-dom'
-import { useSelectors } from 'src/hooks'
+import { useSelectors, useTexts } from 'src/hooks'
 
 const CardViewTransfer = () => {
   const languages = useSelectors((state) => state.languages.value)
   const locations = useLocation()
+  const {haveBeenMovedToMyStatus, haveBeenMovedToMyBoard} = useTexts()
   return (
     <div className="flex justify-center items-center z-30 rounded bg-black/50 text-white w-full h-full absolute">
       {locations.pathname === '/' ? (
         <div className="flex justify-center items-center gap-1 h-full">
-          <East />
+          {locations.pathname === '/' ? <East /> : <West />}
           <div>
-            {languages === 'ko' ? '게시판으로 카드 전송됨' : 'Moved to board'}
+            {haveBeenMovedToMyBoard}
           </div>
         </div>
       ) : (
         <div className="flex justify-center items-center gap-1 h-full">
           <West />
           <div>
-            {languages === 'ko'
-              ? '내 상태로 카드 전송됨'
-              : 'Moved to my status'}
+            {haveBeenMovedToMyStatus}
           </div>
         </div>
       )}
