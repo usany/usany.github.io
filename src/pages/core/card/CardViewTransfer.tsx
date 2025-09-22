19045f0805142c7716cb0f0dba8be9dc3bb3f544
usany <ckd_qja@naver.com>
2025-09-22 20:19:47 +0900

    cern

--- src/pages/core/card/CardViewTransfer.tsx
+++ src/pages/core/card/CardViewTransfer.tsx
@@ -1,10 +1,9 @@
 import East from '@mui/icons-material/East'
 import West from '@mui/icons-material/West'
 import { useLocation } from 'react-router-dom'
-import { useSelectors, useTexts } from 'src/hooks'
+import { useTexts } from 'src/hooks'
 
 const CardViewTransfer = () => {
-  // const languages = useSelectors((state) => state.languages.value)
   const locations = useLocation()
   const {haveBeenMovedToMyStatus, haveBeenMovedToMyBoard} = useTexts()
   return (
@@ -15,19 +14,6 @@ const CardViewTransfer = () => {
           {locations.pathname === '/' ? haveBeenMovedToMyBoard : haveBeenMovedToMyStatus}
         </div>
       </div>
-      {/* {locations.pathname === '/' ? (
-        <div className="flex justify-center items-center gap-1 h-full">
-          {locations.pathname === '/' ? <East /> : <West />}
-          {locations.pathname === '/' ? haveBeenMovedToMyBoard : haveBeenMovedToMyStatus}
-        </div>
-      ) : (
-        <div className="flex justify-center items-center gap-1 h-full">
-          <West />
-          <div>
-            {haveBeenMovedToMyStatus}
-          </div>
-        </div>
-      )} */}
     </div>
   )
 }
