31da0f0c52a877fb97d4bd2a3841302235e4bd28
usany <ckd_qja@naver.com>
2025-09-26 04:13:32 +0900

    cern

--- src/pages/core/specifics/SpecificsSteppers.tsx
+++ src/pages/core/specifics/SpecificsSteppers.tsx
@@ -1,17 +1,16 @@
-import { useSelectors } from "src/hooks";
+import { useSelectors, useTexts } from "src/hooks";
 import Steppers from "src/pages/add/Steppers";
-// import { CardActionArea, CardActions } from '@mui/material';
-// import { useBottomNavigationStore } from 'src/store'
 
 interface Props {
   message: {};
+  round: number
 }
 
 function SpecificsSteppers({ message, round }: Props) {
-  const languages = useSelectors((state) => state.languages.value)
+  const {process} = useTexts()
   return (
     <div className="flex flex-col gap-1">
-      <div className="flex pt-5">{languages === 'ko' ? '진행 단계:' : 'Process:'} {round}</div>
+      <div className="flex pt-5">{process} {round}</div>
       <Steppers message={message} round={round} />
     </div>
   );
