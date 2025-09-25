7df3af6f2aa3a876598586e18c5274c7ab59a9fb
usany <ckd_qja@naver.com>
2025-09-26 05:37:12 +0900

    cern

---
+++ src/pages/core/specifics/SpecificsTradesPopups.tsx
@@ -0,0 +1,79 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import BeachAccess from '@mui/icons-material/BeachAccess'
+import EastIcon from '@mui/icons-material/East'
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule'
+import WestIcon from '@mui/icons-material/West'
+import { Chip } from '@mui/material'
+import { useSelectors, useTexts } from 'src/hooks'
+import Popups from '../Popups'
+import SpecificsTradesContent from './SpecificsTradesContent'
+import SpecificsTradesTitle from './SpecificsTradesTitle'
+import SpecificsTradesTrigger from './SpecificsTradeTrigger'
+
+interface Props {
+  drawerOpenTrue: () => void
+  message: {}
+  connectedUser: {}
+}
+function SpecificsTradesPopups({ isCreator }: Props) {
+  const messageDisplayName = message.displayName
+  const connectedDisplayName = connectedUser.displayName
+  const {borrowing, lending, noOneYet, pointsReward} = useTexts()
+  const messageName =
+    messageDisplayName.length > 10
+      ? messageDisplayName.slice(0, 10) + '......'
+      : messageDisplayName
+  const connectedMessageName = connectedDisplayName?.length > 10 ? connectedDisplayName.slice(0, 10) + '......' : connectedDisplayName
+  if (isCreator) {
+    return (
+      <Popups
+        trigger={
+          <SpecificsTradesTrigger
+            onClick={drawerOpenTrue}
+            isCreator={true}
+            message={message}
+          />
+        }
+        title={<SpecificsTradesTitle message={message} connectedUser={connectedUser} isCreator={true} />}
+        content={
+          <SpecificsTradesContent
+            isCreator={true}
+            message={message}
+            connectedUser={connectedUser}
+          />
+        }
+      />
+    )
+  }
+  return (
+    <Popups
+      trigger={
+        <SpecificsTradesTrigger
+          onClick={drawerOpenTrue}
+          isCreator={false}
+          message={{
+            ...message,
+            connectedProfileImage: true,
+            connectedProfileImageUrl: connectedUser.url,
+            connectedDefaultProfile: connectedUser.url,
+          }}
+        />
+      }
+      title={<SpecificsTradesTitle message={message} connectedUser={connectedUser} isCreator={false} />}
+      content={
+        <SpecificsTradesContent
+          isCreator={false}
+          message={{
+            ...message,
+            connectedProfileImage: true,
+            connectedProfileImageUrl: connectedUser.url,
+            connectedDefaultProfile: connectedUser.url,
+          }}
+          connectedUser={connectedUser}
+        />
+      }
+    />
+  )
+}
+
+export default SpecificsTradesPopups
