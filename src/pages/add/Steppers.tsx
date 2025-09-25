370af663cc0d55245b7bd454cc50df0cc7aaf0e4
usany <ckd_qja@naver.com>
2025-09-26 04:46:08 +0900

    cern

--- src/pages/add/Steppers.tsx
+++ src/pages/add/Steppers.tsx
@@ -37,26 +37,6 @@ function Steppers({ message }) {
               </StepLabel>
             </Step>
           ))}
-        {/* {message.text.choose === 1 &&
-          steps[index].map((label) => (
-            <Step key={label}>
-              <StepLabel>
-                <div className='text-xs'>
-                  {label}
-                </div>
-              </StepLabel>
-            </Step>
-          ))}
-        {message.text.choose === 2 &&
-          steps[index].map((label) => (
-            <Step key={label}>
-              <StepLabel>
-                <div className='text-xs'>
-                  {label}
-                </div>
-              </StepLabel>
-            </Step>
-          ))} */}
       </Stepper>
     </div>
   )
