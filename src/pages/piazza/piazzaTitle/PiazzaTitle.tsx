c68d14315cdd177b24dd827d1f7ec678c12afa45
usany <ckd_qja@naver.com>
2025-09-30 02:34:51 +0900

    cern

--- src/pages/piazza/piazzaTitle/PiazzaTitle.tsx
+++ src/pages/piazza/piazzaTitle/PiazzaTitle.tsx
@@ -1,19 +1,13 @@
 import { MessagesSquare } from 'lucide-react'
-import { useSelectors, useTexts } from 'src/hooks'
+import { useTexts } from 'src/hooks'
 import PageTitle from 'src/pages/core/pageTitle/PageTitle'
 import PiazzaSwitch from 'src/pages/piazza/piazzaTitle/piazzaSwitch/PiazzaSwitch'
 
-const piazzaTitles = {
-  ko: ['단체 대화', '개인 대화'],
-  en: ['Group Messaging', 'Messaging']
-}
 interface Props {
   displayName: string
 }
 
 const PiazzaTitle = ({ displayName }: Props) => {
-  const languages = useSelectors((state) => state.languages.value)
-  const index = (languages === 'ko' || languages === 'en') ? languages : 'ko'
   const {groupMessaging, privateMessaging} = useTexts()
   const conversation = location.search ? location.search.slice(location.search.indexOf('=') + 1) : 'piazza'
   return (
