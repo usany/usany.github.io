771e2ab8807a12c4402f26732798113434bcf71a
usany <ckd_qja@naver.com>
2025-10-03 21:12:13 +0900

    cern

--- src/pages/piazza/piazzaScreen/PiazzaScreenView.tsx
+++ src/pages/piazza/piazzaScreen/PiazzaScreenView.tsx
@@ -16,26 +16,8 @@ import Avatars from 'src/pages/core/Avatars'
 import Popups from 'src/pages/core/Popups'
 import { webSocket } from 'src/webSocket.tsx'
 import PiazzaDialogsContent from './piazzaDialogs/PiazzaDialogsContent'
+import PiazzaDialogsTitle from './PiazzaDialogsTitle'
 
-const PiazzaDialogsTitle = ({user, displayedName}) => {
-  const languages = useSelectors((state) => state.languages.value)
-  return (
-    <>
-      <div className="flex justify-center">
-        {user?.displayName}
-      </div>
-      {user?.displayName !== displayedName && (
-        <>
-          {languages === 'ko' ? (
-            `(${displayedName}에서 개명)`
-          ) : (
-            `(Changed name from ${displayedName})`
-          )}
-        </>
-      )}
-    </>
-  )
-}
 const PiazzaScreenViewClock = ({ value }) => {
   const languages = useSelectors((state) => state.languages.value)
   const clock = new Date(value.messageClock)
