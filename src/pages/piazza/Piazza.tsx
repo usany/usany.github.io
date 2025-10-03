e8188f23770ec15fc91855ec62c20dbfce222cfb
usany <ckd_qja@naver.com>
2025-10-03 17:56:06 +0900

    cern

--- src/pages/piazza/Piazza.tsx
+++ src/pages/piazza/Piazza.tsx
@@ -1,21 +1,21 @@
 import { doc, getDoc } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
 import { useLocation, useSearchParams } from 'react-router-dom'
 import { dbservice } from 'src/baseApi/serverbase'
 import { useSelectors } from 'src/hooks'
 import PiazzaForm from 'src/pages/piazza/piazzaForm/PiazzaForm'
 import PiazzaScreen from 'src/pages/piazza/piazzaScreen/PiazzaScreen'
 import PiazzaTitle from 'src/pages/piazza/piazzaTitle/PiazzaTitle'
-import { changeBottomNavigation } from 'src/stateSlices/bottomNavigationSlice'
 import type { RootState } from 'src/store'
 import PiazzaMorphingDialogAudioCall from './components/PiazzaMorphingDialogAudioCall'
 import PiazzaMorphingDialogVideoCall from './components/PiazzaMorphingDialogVideoCall'
+// import { useDispatch } from 'react-redux'
+// import { changeBottomNavigation } from 'src/stateSlices/bottomNavigationSlice'
 
 function Piazza() {
   const [messages, setMessages] = useState('')
   const [messagesList, setMessagesList] = useState<[]>([])
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
   const { state } = useLocation() as any
   const [isKeyboardOpen, setIsKeyboardOpen] = useState(false)
   const [chattingUser, setChattingUser] = useState<any>(null)
