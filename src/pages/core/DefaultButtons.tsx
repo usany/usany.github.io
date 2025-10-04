e2861be4097a064f3f436b700800e3e6a511a8a0
usany <ckd_qja@naver.com>
2025-10-04 18:12:43 +0900

    cern

--- src/pages/core/DefaultButtons.tsx
+++ src/pages/core/DefaultButtons.tsx
@@ -13,19 +13,17 @@ export const DefaultButtons = ({ label, ...props }: ButtonProps) => {
   const theme = useTheme()
   const bgcolor = theme.palette.mode === 'light' ? '#e2e8f0' : '#2d3848'
   return (
-    <>
-      <Button
-        variant="outlined"
-        sx={{
+    <Button
+      variant="outlined"
+      sx={{
+        bgcolor: bgcolor,
+        ':hover': {
           bgcolor: bgcolor,
-          ':hover': {
-            bgcolor: bgcolor,
-          },
-        }}
-        {...props}
-      >
-        {label}
-      </Button>
-    </>
+        },
+      }}
+      {...props}
+    >
+      {label}
+    </Button>
   )
 }
