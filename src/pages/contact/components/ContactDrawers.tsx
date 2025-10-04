30715ba3b24e9fa82b2bb42034ba0d59021c4932
usany <ckd_qja@naver.com>
2025-10-05 03:15:19 +0900

    cern

--- src/pages/contact/components/ContactDrawers.tsx
+++ src/pages/contact/components/ContactDrawers.tsx
@@ -1,6 +1,8 @@
 import Popups from "../../core/Popups";
 import { useSelectors } from 'src/hooks';
-import {ContactDrawersContent, ContactDrawersTitle, ContactDrawersTrigger} from "./";
+import ContactDrawersContent from "./ContactDrawersContent";
+import ContactDrawersTitle from "./ContactDrawersTitle";
+import ContactDrawersTrigger from "./ContactDrawersTrigger";
 
 
 function ContactFormDrawers() {
