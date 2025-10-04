86136c6a5c516d706fd2c48e51a8692a722329c8
usany <ckd_qja@naver.com>
2025-10-04 18:09:04 +0900

    cern

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,17 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
-
+import { DefaultButtons } from 'src/pages/core/DefaultButtons'
 import { fn } from 'storybook/test'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Components/Button',
-  decorators: [
-    (Story) => (
-      <div>
-        <Story />
-      </div>
-    ),
-  ],
+  component: DefaultButtons,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
@@ -27,5 +21,7 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-
-export { DefaultButtons } from 'src/pages/core/DefaultButtons'
+export const defaultButtons: Story = {
+  args: {}
+}
+// export { DefaultButtons } from 'src/pages/core/DefaultButtons'
