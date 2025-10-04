a06c84e64cde5ecb5a90524162ae33ed8e2fcea6
usany <ckd_qja@naver.com>
2025-10-04 18:11:46 +0900

    cern

--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import { fn } from 'storybook/test'
 
-import { Cards } from 'src/pages/core/Card'
+import { Cards } from 'src/pages/core/Cards'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
