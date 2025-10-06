4a9d7e3c73991258cb4c370981d2ec907e513c5f
usany <ckd_qja@naver.com>
2025-10-06 23:21:11 +0900

    cern

--- src/hooks/useCardsBackground.ts
+++ src/hooks/useCardsBackground.ts
@@ -2,12 +2,12 @@ import useSelectors from './useSelectors';
 
 const useCardsBackground = () => {
   const theme = useSelectors((state) => state.theme.value)
-  // const colorOne = theme === 'dark' ? '#5c6778' : '#f7fafb'
-  // const colorTwo = theme === 'dark' ? '#2d3848' : '#e2e8f0'
-  // const colorThree = theme === 'dark' ? '#1a202c' : '#cbd5df'
-  const colorOne = theme === 'dark' ? '#2c3e50' : '#a3c4dc'
-  const colorTwo = theme === 'dark' ? '#6c7a89' : '#c6d0e1'
-  const colorThree = theme === 'dark' ? '#4a5a6a' : '#dde3e8'
+  const colorOne = theme === 'dark' ? '#5c6778' : '#f7fafb'
+  const colorTwo = theme === 'dark' ? '#2d3848' : '#e2e8f0'
+  const colorThree = theme === 'dark' ? '#1a202c' : '#cbd5df'
+  // const colorOne = theme === 'dark' ? '#4a5a6a' : '#dde3e8'
+  // const colorTwo = theme === 'dark' ? '#6c7a89' : '#c6d0e1'
+  // const colorThree = theme === 'dark' ? '#2c3e50' : '#a3c4dc'
   return ({ colorOne, colorTwo, colorThree })
 }
 export default useCardsBackground
