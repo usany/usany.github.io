d79944e2a288ec591daa185cc517d76d57c425b8
usany <ckd_qja@naver.com>
2025-10-07 03:05:48 +0900

    cern

--- src/pages/add/Selects.tsx
+++ src/pages/add/Selects.tsx
@@ -219,7 +219,7 @@ function Selects({
       {(locationState.locationOne === "직접 입력" || locationState.locationOne === "Self input") && (
         <div className="flex pt-7">
           <TextField
-            inputProps={{ maxLength: languages === 'ko' ? 12 : 21 }}
+            inputProps={{ maxLength: 11 }}
             onChange={changeLocationInput} required autoFocus />
         </div>
       )}
