6b150c873f8bdfe4316c1daf47ffe86bdf287bd0
usany <ckd_qja@naver.com>
2025-10-16 19:45:54 +0900

    cern

--- src/pages/add/locationsBuildings.ts
+++ src/pages/add/locationsBuildings.ts
@@ -219,9 +219,18 @@ export const markers = Object.keys(buildingsObject).map((value) => {
   }
 })
 
+const keysArray = Object.keys(buildingsObject)
 const locationsBuildings = {
-  ko: ["중도", "청운", "푸른솔", "간호이과대", "경영대", "문과대", "의과대", "치과병원", "네오르네상스관"],
-  en: ['Central library', 'Cheongwoon', 'Pureunsol', 'Nursing Science & Science', 'Business', 'Humanities', 'Medicine', 'Dental Hospital', 'Neo-Renaissance']
+  ko: keysArray.map((value) => {
+    return (buildingsObject[value].ko.name)
+  }),
+  en : keysArray.map((value) => {
+    return (buildingsObject[value].en.name)
+  }),
 }
+// const locationsBuildings = {
+//   ko: ["중도", "청운", "푸른솔", "간호이과대", "경영대", "문과대", "의과대", "치과병원", "네오르네상스관"],
+//   en: ['Central library', 'Cheongwoon', 'Pureunsol', 'Nursing Science & Science', 'Business', 'Humanities', 'Medicine', 'Dental Hospital', 'Neo-Renaissance']
+// }
 
 export default locationsBuildings
