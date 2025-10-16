c276a5594b0532c8024ad20bacbbd2e5596fa774
usany <ckd_qja@naver.com>
2025-10-16 19:47:28 +0900

    cern

--- src/pages/add/locationsBuildings.ts
+++ src/pages/add/locationsBuildings.ts
@@ -228,9 +228,5 @@ const locationsBuildings = {
     return (buildingsObject[value].en.name)
   }),
 }
-// const locationsBuildings = {
-//   ko: ["중도", "청운", "푸른솔", "간호이과대", "경영대", "문과대", "의과대", "치과병원", "네오르네상스관"],
-//   en: ['Central library', 'Cheongwoon', 'Pureunsol', 'Nursing Science & Science', 'Business', 'Humanities', 'Medicine', 'Dental Hospital', 'Neo-Renaissance']
-// }
 
 export default locationsBuildings
