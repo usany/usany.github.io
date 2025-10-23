c73bf89c2187f82bc9ff2560c9960e83325f7738
usany <ckd_qja@naver.com>
2025-10-24 05:16:39 +0900

    cern

--- src/pages/add/AddCards.tsx
+++ src/pages/add/AddCards.tsx
@@ -60,7 +60,7 @@ const AddCards = ({ borrow, item, fromTo, locationState, display }: Props) => {
     if (value === 'input') return buildingsObj[value].ko.name === locationOne
     return buildingsObj[value.slice(0, 2)][value].ko.name === locationOne
   }) : ''
-  const staticImg = locationOne ? (locationOne === 'input' ? buildingsObj[key?.slice(0, 2)][key]?.image : buildingsObj[key]?.image) : staticArray['building']
+  const staticImg = (locationOne && locationOne !== '직접 입력') ? buildingsObj[key?.slice(0, 2)][key]?.image : staticArray['building']
   // const staticImg = staticArray[locationOne] || staticArray['building']
   const {borrowing, lending, emptyCard} = useTexts()
   return (
