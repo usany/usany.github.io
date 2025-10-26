b6c46c54fd40c8e5afbe6e02465e8a4adeeb032c
usany <ckd_qja@naver.com>
2025-10-27 06:34:25 +0900

    cern

--- src/pages/profile/ProfileLocations.tsx
+++ src/pages/profile/ProfileLocations.tsx
@@ -18,7 +18,10 @@ import useTexts from 'src/hooks/useTexts'
 import { changeProfile } from 'src/stateSlices/profileSlice'
 import ProfileLocationsChip from './ProfileLocationsChip'
 
-const campuses = {ko: ['서울캠퍼스', '광릉캠퍼스', '광릉캠퍼스'], en: ['Seoul Campus', 'Global Campus', 'Gwangneung Campus']}
+const campuses = {
+  ko: ['서울캠퍼스', '광릉캠퍼스', '광릉캠퍼스'],
+  en: ['Seoul Campus', 'Global Campus', 'Gwangneung Campus']
+}
 const area = {
   westSouth: { lat: 37.5927551, lng: 127.047462 },
   westNorth: { lat: 37.6010743, lng: 127.047462 },
