d41c9d029be8cd4485893b847315e9ad6e55fd82
usany <ckd_qja@naver.com>
2025-11-11 02:26:21 +0900

    cern

--- src/pages/core/buildings/seh.ts
+++ src/pages/core/buildings/seh.ts
@@ -1,6 +1,12 @@
 export const sehKo = [
-  
+  "B101	행정실",
+  "B102	세미나실",
+  "B106	세탁실",
+  "B107	체력단련실",
 ];
 export const sehEn = [
-  
+  "B101	Administration Office",
+  "B102	Seminar Room",
+  "B106	Laundry Room",
+  "B107	Fitness Room",
 ];
