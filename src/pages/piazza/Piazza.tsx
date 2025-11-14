1492a5469df26d20939caf47d1e3386a616bb975
usany <ckd_qja@naver.com>
2025-11-15 03:42:11 +0900

    cern

--- src/pages/piazza/Piazza.tsx
+++ src/pages/piazza/Piazza.tsx
@@ -49,32 +49,32 @@ function Piazza() {
     }
   }, [conversation, chatUid])
   const piazzaForm = useSelectors((state: RootState) => state.piazzaForm.value)
-  useEffect(() => {
-    const listener = () => {
-      const minKeyboardHeight = 300
-      const newState = window.screen.height - minKeyboardHeight > (window.visualViewport?.height || window.screen.height)
-      // const newState =
-      //   window.screen.height - 300 >
-      //   (window.visualViewport?.height || window.screen.height)
-      if (isKeyboardOpen !== newState) {
-        setIsKeyboardOpen(newState)
-        dispatch(changePiazzaForm(newState))
-      }
-    }
-    window.addEventListener('resize', listener)
-    if (typeof visualViewport !== 'undefined') {
-      window.visualViewport?.addEventListener('resize', listener)
-    }
-    visualViewport?.addEventListener('resize', listener)
-    if (typeof visualViewport !== 'undefined') {
-      visualViewport?.addEventListener('resize', listener)
-    }
-    return () => {
-      if (typeof visualViewport !== 'undefined') {
-        window.visualViewport?.removeEventListener('resize', listener)
-      }
-    }
-  }, [isKeyboardOpen])
+  // useEffect(() => {
+  //   const listener = () => {
+  //     const minKeyboardHeight = 300
+  //     const newState = window.screen.height - minKeyboardHeight > (window.visualViewport?.height || window.screen.height)
+  //     const newState =
+  //       window.screen.height - 300 >
+  //       (window.visualViewport?.height || window.screen.height)
+  //     if (isKeyboardOpen !== newState) {
+  //       setIsKeyboardOpen(newState)
+  //       dispatch(changePiazzaForm(newState))
+  //     }
+  //   }
+  //   window.addEventListener('resize', listener)
+  //   if (typeof visualViewport !== 'undefined') {
+  //     window.visualViewport?.addEventListener('resize', listener)
+  //   }
+  //   visualViewport?.addEventListener('resize', listener)
+  //   if (typeof visualViewport !== 'undefined') {
+  //     visualViewport?.addEventListener('resize', listener)
+  //   }
+  //   return () => {
+  //     if (typeof visualViewport !== 'undefined') {
+  //       window.visualViewport?.removeEventListener('resize', listener)
+  //     }
+  //   }
+  // }, [isKeyboardOpen])
 
   // useEffect(() => {
   //   dispatch(changeBottomNavigation(5))
