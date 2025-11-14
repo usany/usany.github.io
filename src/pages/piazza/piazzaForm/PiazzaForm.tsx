a3443b02e9334ecba96403f7044ab2142f10fc4b
usany <ckd_qja@naver.com>
2025-11-15 05:12:25 +0900

    cern

--- src/pages/piazza/piazzaForm/PiazzaForm.tsx
+++ src/pages/piazza/piazzaForm/PiazzaForm.tsx
@@ -282,7 +282,7 @@ function PiazzaForm({
           trigger={
             <div className="flex items-center px-1 h-full rounded bg-light-2 dark:bg-dark-2">
               <PlusCircle />
-              samples
+              sample
             </div>
           }
           title={selectCall}
@@ -303,8 +303,8 @@ function PiazzaForm({
           //   top: document.body.scrollHeight,
           //   behavior: 'smooth', // Optional if you want to skip the scrolling animation
           // })
-          // inputRef.current?.scrollIntoView()
-          setTimeout(() => inputRef.current?.scrollIntoView(), 100)
+          inputRef.current?.scrollIntoView()
+          setTimeout(() => inputRef.current?.scrollIntoView(), 500)
         }}
       />
       <button className="w-1/6 rounded bg-light-2 dark:bg-dark-2" type="submit">
