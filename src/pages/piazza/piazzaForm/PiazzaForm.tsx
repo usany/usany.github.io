02d170aca9afc51d8a704f3ed62214d2802a46f3
usany <ckd_qja@naver.com>
2025-11-15 03:37:44 +0900

    cern

--- src/pages/piazza/piazzaForm/PiazzaForm.tsx
+++ src/pages/piazza/piazzaForm/PiazzaForm.tsx
@@ -273,6 +273,7 @@ function PiazzaForm({
           trigger={
             <div className="flex items-center px-1 h-full rounded bg-light-2 dark:bg-dark-2">
               <PlusCircle />
+              samples
             </div>
           }
           title={selectCall}
