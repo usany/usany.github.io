4b946b8f74b3994f3674341dee5a5868066243b8
usany <ckd_qja@naver.com>
2025-11-22 18:13:38 +0900

    cern

--- src/pages/collection/Collection.tsx
+++ src/pages/collection/Collection.tsx
@@ -271,8 +271,8 @@ function Collection() {
                 </div>
               }
             </div>
-            {/* {error && <div className='flex justify-center'>인공지능이 바쁩니다. 다시 시도해주세요.</div>} */}
             {error && <div className='flex justify-center'>AI is busy. Please try again.</div>}
+            {/* {error && <div className='flex justify-center'>인공지능이 바쁩니다. 다시 시도해주세요.</div>} */}
             {/* {!loading && ['n', 'N'].indexOf(isUmbrella ? isUmbrella[0] : isUmbrella) !==
                 -1 && <div className='flex justify-center pt-5'>{cannotFindAnUmbrella}</div>} */}
             {!loading && <div className='flex flex-col'>
