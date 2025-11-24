938745e74256b6ad5c5c35184d828fe2d272b93e
usany <ckd_qja@naver.com>
2025-11-24 20:43:28 +0900

    cern

--- src/lottiesAnimation/Lotties.tsx
+++ src/lottiesAnimation/Lotties.tsx
@@ -1,7 +1,7 @@
 import Lottie from 'react-lottie';
 // import Lottie from 'lottie-react'
 import rain from 'src/assets/Animation.json';
-import { TextRoll } from 'src/components/motion-primitives/text-roll';
+// import { TextRoll } from 'src/components/motion-primitives/text-roll';
 function Lotties() {
   const defaultOptions = {
     loop: true,
@@ -16,12 +16,6 @@ function Lotties() {
     <div className='flex flex-col justify-center items-center pt-[250px]'>
       <div className='flex flex-col text-5xl'>{language < 0.5 ? '쿠우산' : 'KHUSAN'}</div>
       <Lottie options={defaultOptions} height={200} width={200} />
-      {/* <TextRoll className='text-5xl'>KHUSAN</TextRoll> */}
-      {/* <div className='flex flex-col items-center'>
-      </div> */}
-      {/* <div className='flex flex-col items-center'>
-        <TextRoll className='text-5xl'>KHUSAN</TextRoll>
-      </div> */}
     </div>
   )
 }
