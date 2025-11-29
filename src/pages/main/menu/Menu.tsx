c4463be421575edf1dc18e6363af4bda0b4d3e4f
usany <ckd_qja@naver.com>
2025-11-30 03:08:29 +0900

    cern

--- src/pages/main/menu/Menu.tsx
+++ src/pages/main/menu/Menu.tsx
@@ -3,8 +3,8 @@ import PageTitle from 'src/pages/core/pageTitle/PageTitle'
 import Accordions from './Accordions/Accordions'
 
 function Menu() {
+
   const { myStatus } = useTexts()
-  
   return (
     <div className="flex justify-center flex-col pb-5">
       <PageTitle title={myStatus} />
