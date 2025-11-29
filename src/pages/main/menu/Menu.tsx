eabc3991c0b5eb5c09a95bf751f02f6c91778d80
usany <ckd_qja@naver.com>
2025-11-30 03:15:06 +0900

    cern

--- src/pages/main/menu/Menu.tsx
+++ src/pages/main/menu/Menu.tsx
@@ -3,7 +3,6 @@ import PageTitle from 'src/pages/core/pageTitle/PageTitle'
 import Accordions from './Accordions/Accordions'
 
 function Menu() {
-
   const { myStatus } = useTexts()
   return (
     <div className="flex justify-center flex-col pb-5">
