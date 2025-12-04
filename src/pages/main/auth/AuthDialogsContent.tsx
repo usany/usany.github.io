7619aa484a7b496eac231215b509103e15aa1a68
usany <ckd_qja@naver.com>
2025-12-04 19:11:35 +0900

    cern

--- src/pages/main/auth/AuthDialogsContent.tsx
+++ src/pages/main/auth/AuthDialogsContent.tsx
@@ -3,15 +3,17 @@ import useTexts from "src/hooks/useTexts";
 import AuthForm from "src/pages/main/auth/AuthForm";
 import AuthMethods from "./AuthMethods";
 import Playlist from "src/pages/core/Playlist";
+import useLargeMedia from "src/hooks/useLargeMedia";
 
 function AuthDialogsContent({changeProgress}) {
   const [agreed, setAgreed] = useState(false)
   const changeAgreed = () => {
     setAgreed(!agreed)
   }
+  const largeMedia = useLargeMedia()
   // const {onlyTakesOneMinuteToRegisterAccount, playlistReadyForYouToGetRidOfBoredom} = useTexts()
   return (
-    <>
+    <div className={`${!largeMedia && 'w-screen'}`}>
       {/* <div className="flex flex-col p-3">
         {onlyTakesOneMinuteToRegisterAccount}
         {playlistReadyForYouToGetRidOfBoredom}
@@ -21,7 +23,7 @@ function AuthDialogsContent({changeProgress}) {
       </div> */}
       <AuthMethods agreed={agreed} changeAgreed={changeAgreed} changeProgress={changeProgress}/>
       <AuthForm signIn={false} agreed={agreed} />
-    </>
+    </div>
   );
 }
 
