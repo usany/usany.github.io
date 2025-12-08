efcf9cd41c25ac5d5ca9cda3fc7ac318854af769
usany <ckd_qja@naver.com>
2025-12-08 23:50:45 +0900

    cern

--- src/hooks/useUserObject.ts
+++ src/hooks/useUserObject.ts
@@ -54,11 +54,11 @@ const useUserObject = () => {
     }
     handleRedirectResult()
     auth.onAuthStateChanged((user) => {
-      const reloading = sessionStorage.getItem('reloading')
-      if (user === null && !reloading) {
-        sessionStorage.setItem('reloading', 'true')
-        location.reload()
-      }
+      // const reloading = sessionStorage.getItem('reloading')
+      // if (user === null && !reloading) {
+      //   sessionStorage.setItem('reloading', 'true')
+      //   location.reload()
+      // }
       if (user?.uid) {
         setProfile(user?.uid)
       } else {
