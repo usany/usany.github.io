bc3e9f2b97b0fb42ee13a3391a3036bdbf1ee1b6
usany <ckd_qja@naver.com>
2025-12-10 20:19:55 +0900

    cern

--- src/hooks/useUserObject.ts
+++ src/hooks/useUserObject.ts
@@ -23,7 +23,7 @@ const useUserObject = () => {
     if (newProfile) {
       setTimeout(() => {
         dispatch(changeProfile(newProfile))
-      }, 500)
+      }, 1000)
     }
   }
   const onLine = useSelectors((state) => state.onLine.value)
