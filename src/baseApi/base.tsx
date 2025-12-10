5d307823fe6b3b56665a296fe8bd9fc8411e0872
usany <ckd_qja@naver.com>
2025-12-10 18:38:39 +0900

    cern

--- src/baseApi/base.tsx
+++ src/baseApi/base.tsx
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const url = import.meta.env.VITE_SUPABASE_URL
-const supabase = createClient(import.meta.env.VITE_SUPABASE_BASIC_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+const url = import.meta.env.VITE_SUPABASE_ANON_KEY.slice(0, import.meta.env.VITE_SUPABASE_ANON_KEY.indexOf('.'))+'.supabase.co'
+const supabase = createClient(url, import.meta.env.VITE_SUPABASE_ANON_KEY);
 export default supabase
