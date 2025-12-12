fd88e35ad2a74927215cb1026fe67debbec334bf
usany <ckd_qja@naver.com>
2025-12-12 18:27:17 +0900

    cern

--- src/pages/core/navigationTop/sideNavigation/Navigation.tsx
+++ src/pages/core/navigationTop/sideNavigation/Navigation.tsx
@@ -73,14 +73,6 @@ function Navigation() {
     <>
       {showPlaylist && createPortal(
         <Playlist open={open} />,
-        // <div
-        //   className={`fixed bottom-0 left-0 z-[120] flex justify-start transition-all ${
-        //     open ? 'opacity-100 pointer-events-auto duration-250' : 'opacity-0 pointer-events-none duration-0'
-        //   }`}
-        // >
-        //   <div className="flex w-[350px] justify-center">
-        //   </div>
-        // </div>,
         document.body
       )}
       <Drawer direction="left" onOpenChange={(value) => value ? setTimeout(() => setOpen(value), 250) : setOpen(value)}>
