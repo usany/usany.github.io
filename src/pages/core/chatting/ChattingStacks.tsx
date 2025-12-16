948b9592bf7d576f0d036bd89c25eb1b348159c5
usany <ckd_qja@naver.com>
2025-12-16 20:34:49 +0900

    cern

--- src/pages/core/chatting/ChattingStacks.tsx
+++ src/pages/core/chatting/ChattingStacks.tsx
@@ -127,42 +127,46 @@ const ChattingStacks = ({
         conversationName,
         profileUrl
       } = message
-      let userOne
-      let userTwo
-      let userOneDisplayName
-      let userTwoDisplayName
-      let userOneProfileUrl
-      let userTwoProfileUrl
-      const messageCount = chattings[conversation]?.messageCount || 1
-      if (userUid < conversationUid) {
-        userOne = userUid
-        userTwo = conversationUid
-        userOneDisplayName = id
-        userTwoDisplayName = conversationName
-        userOneProfileUrl = profileUrl
-      } else {
-        userOne = conversationUid
-        userTwo = userUid
-        userOneDisplayName = conversationName
-        userTwoDisplayName = id
-        userTwoProfileUrl = profileUrl
+      if (conversationUid === profile.uid) {
+        let userOne
+        let userTwo
+        let userOneDisplayName
+        let userTwoDisplayName
+        let userOneProfileUrl
+        let userTwoProfileUrl
+        const messageCount = chattings[conversation]?.messageCount || 1
+        if (userUid < conversationUid) {
+          userOne = userUid
+          userTwo = conversationUid
+          userOneDisplayName = id
+          userTwoDisplayName = conversationName
+          userOneProfileUrl = profileUrl
+        } else {
+          userOne = conversationUid
+          userTwo = userUid
+          userOneDisplayName = conversationName
+          userTwoDisplayName = id
+          userTwoProfileUrl = profileUrl
+        }
+        const replaceObj = {
+          userUid: userUid,
+          userName: id,
+          userOne: userOne,
+          userOneDisplayName: userOneDisplayName,
+          userTwo: userTwo,
+          userTwoDisplayName: userTwoDisplayName,
+          message: msg,
+          messageClock: messageClock,
+          messageClockNumber: messageClockNumber,
+          userOneProfileUrl: userOneProfileUrl,
+          userTwoProfileUrl: userTwoProfileUrl,
+          messageCount: messageCount,
+        }
+        const newChattings = { ...chattings, [conversation]: replaceObj }
+        changeChattings(newChattings)
       }
-      const replaceObj = {
-        userUid: userUid,
-        userName: id,
-        userOne: userOne,
-        userOneDisplayName: userOneDisplayName,
-        userTwo: userTwo,
-        userTwoDisplayName: userTwoDisplayName,
-        message: msg,
-        messageClock: messageClock,
-        messageClockNumber: messageClockNumber,
-        userOneProfileUrl: userOneProfileUrl,
-        userTwoProfileUrl: userTwoProfileUrl,
-        messageCount: messageCount,
-      }
-      const newChattings = { ...chattings, [conversation]: replaceObj }
-      changeChattings(newChattings)
+      console.log(conversationUid)
+      console.log(profile.uid)
     }
     webSocket.on(`sNewMessage`, sNewMessageCallback)
     return () => {
