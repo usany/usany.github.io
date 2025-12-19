3868e7180b4f7f691de6975e06751fbe39f3f241
usany <ckd_qja@naver.com>
2025-12-19 20:27:58 +0900

    cern

--- src/pages/core/chatting/MessageStacks.tsx
+++ src/pages/core/chatting/MessageStacks.tsx
@@ -4,13 +4,10 @@ import EmptyChattingStacks from './EmptyChattingStacks'
 import { useSortedChattings } from './useSortedChattings'
 
 const MessageStacks = () => {
-  const languages = useSelectors((state) => state.languages.value)
-  const index = languages === 'ko' || languages === 'en' ? languages : 'ko'
-  const { chattings, changeChattings, sorted, chattingNone } = useSortedChattings()
+  const { chattings, changeChattings, sorted } = useSortedChattings()
   const chattingsArray = navigator.onLine ? chattings : JSON.parse(localStorage.getItem('chattings') || '[]')
   return (
     <div className="flex flex-col gap-1 w-full">
-      {/* {navigator.onLine && chattingNone && <EmptyChattingStacks index={index} />} */}
       <ChattingStacks
         chattings={chattingsArray}
         changeChattings={changeChattings}
