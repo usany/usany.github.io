f4e00a2f582863ee35358993c9d4185c4269f6cc
usany <ckd_qja@naver.com>
2025-12-23 21:58:19 +0900

    cern

--- src/pages/core/card/Cards.tsx
+++ src/pages/core/card/Cards.tsx
@@ -37,6 +37,32 @@ const Cards = ({
       changeLongPressCard('')
     }
   }, [longPressCard])
+  const increaseRound = () => {
+    if (messageValue.round === 1) {
+      setMessageValue((prev) => {
+        return {...prev, connectedId: profile.uid, connectedProfileImage: profile.profileImage, connectedProfileImageUrl: profile.profileImageUrl, connectedDefaultProfile: profile.defaultProfile, round: prev.round+1}
+      })
+    } else {
+      setMessageValue((prev) => {
+        return (
+          {...prev, round: prev.round+1}
+        )
+      })
+    }
+  }
+  const decreaseRound = () => {
+    if (messageValue.round === 2) {
+      setMessageValue((prev) => {
+        return {...prev, connectedId: '', connectedProfileImage: false, connectedProfileImageUrl: '', connectedDefaultProfile: '', round: prev.round-1}
+      })
+    } else {
+      setMessageValue((prev) => {
+        return (
+          {...prev, round: prev.round-1}
+        )
+      })
+    }
+  }
   return (
     <div className="max-w-60 min-w-20 text-sm p-1" ref={cardsRef}>
       {longPressCard ? (
