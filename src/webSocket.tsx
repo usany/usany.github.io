f150858ac56037fd2aafc1006bc1131e98873933
usany <ckd_qja@naver.com>
2026-01-09 21:23:10 +0900

    cern

--- src/webSocket.tsx
+++ src/webSocket.tsx
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
-export const webSocket = io('https://service-ceni.onrender.com')
-// export const webSocket = io('http://localhost:5000')
+// export const webSocket = io('https://service-ceni.onrender.com')
+export const webSocket = io('http://localhost:5000')
 
 // const onClick = () => {
 //   webSocket.emit('button', { name: 'John Doe', age: 30 })
